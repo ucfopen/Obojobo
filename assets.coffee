07f28199c00a9b740fc4d8a2c255672c2f93f054
Zachary Berry <zachberry@gmail.com>
2016-05-20 14:34:54 -0400

    Get webpack code to work with rails

---
+++ assets.coffee
@@ -0,0 +1,16 @@
+module.exports =
+	TOOLBAR_BOLD: require 'svg-url?noquotes!./img/editor/toolbar/bold.svg'
+	TOOLBAR_ITALIC: require 'svg-url?noquotes!./img/editor/toolbar/italic.svg'
+	TOOLBAR_IMAGE: require 'svg-url?noquotes!./img/editor/toolbar/image.svg'
+	TOOLBAR_INDENT: require 'svg-url?noquotes!./img/editor/toolbar/indent.svg'
+	TOOLBAR_INSERT: require 'svg-url?noquotes!./img/editor/toolbar/insert.svg'
+	TOOLBAR_OL: require 'svg-url?noquotes!./img/editor/toolbar/ol.svg'
+	TOOLBAR_TABLE: require 'svg-url?noquotes!./img/editor/toolbar/table.svg'
+	TOOLBAR_UL: require 'svg-url?noquotes!./img/editor/toolbar/ul.svg'
+	TOOLBAR_UNINDENT: require 'svg-url?noquotes!./img/editor/toolbar/unindent.svg'
+
+	TEXTMENU_BOLD: require 'svg-url?noquotes!./img/editor/textmenu/bold.svg'
+	TEXTMENU_ITALIC: require 'svg-url?noquotes!./img/editor/textmenu/italic.svg'
+	TEXTMENU_LINK: require 'svg-url?noquotes!./img/editor/textmenu/link.svg'
+	TEXTMENU_SUB: require 'svg-url?noquotes!./img/editor/textmenu/sub.svg'
+	TEXTMENU_SUP: require 'svg-url?noquotes!./img/editor/textmenu/sup.svg'
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,33 @@
+var path = require('path');
 var webpack = require('webpack')
+var StatsPlugin = require('stats-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
+// must match config.webpack.dev_server.port
+var devServerPort = 3808;
+
+// set TARGET=production on the environment to add asset fingerprints
+var production = process.env.TARGET === 'production';
+
+var config = {
 	entry: {
 		// 'obodraft': ['./src/scripts/node_modules/app/OboDraft.coffee'],
-		'editor': ['./src/scripts/node_modules/editor.coffee'],
+		'application': ['./app/assets/webpack/src/scripts/node_modules/editor.coffee'],
 		// 'thing': ['./src/scripts/thing/something.coffee'],
 	},
+	// output: {
+	// 	filename: '[name].js',
+	// 	publicPath: 'http://localhost:8090/assets'
+	// },
 	output: {
-		filename: '[name].js',
-		publicPath: 'http://localhost:8090/assets'
+		// Build assets directly in to public/webpack/, let webpack know
+		// that all webpacked assets start with webpack/
+
+		// must match config.webpack.output_dir
+		path: path.join(__dirname, '..', 'public', 'webpack'),
+		publicPath: '/webpack',
+
+		filename: production ? '[name]-[chunkhash].js' : '[name].js'
 	},
 	module: {
 		loaders: [
@@ -21,7 +39,11 @@ module.exports = {
 			{
 				test: /\.scss$/,
 				loader: ExtractTextPlugin.extract(['css', 'sass'])
-			}
+			},
+			// {
+			// 	test: /\.svg$/,
+			// 	loader: 'svg-url-loader'
+			// }
 		]
 	},
 	externals: {
@@ -32,7 +54,17 @@ module.exports = {
 	plugins: [
 		new ExtractTextPlugin('style.css', {
 			allChunks: true
-		})//,
+		}),
+		//must match config.webpack.manifest_filename
+		new StatsPlugin('manifest.json', {
+			// We only need assetsByChunkName
+			chunkModules: false,
+			source: false,
+			chunks: false,
+			modules: false,
+			assets: true
+		})
+		//,
 		// new webpack.DefinePlugin({
 		// 	"process.env": {
 		// 		NODE_ENV: JSON.stringify('production')
@@ -43,3 +75,28 @@ module.exports = {
 		extensions: ['', '.js', '.coffee']
 	}
 }
+
+if (production) {
+	config.plugins.push(
+		new webpack.NoErrorsPlugin(),
+		new webpack.optimize.UglifyJsPlugin({
+			compressor: { warnings: false },
+			sourceMap: false
+		}),
+		new webpack.DefinePlugin({
+			'process.env': { NODE_ENV: JSON.stringify('production') }
+		}),
+		new webpack.optimize.DedupePlugin(),
+		new webpack.optimize.OccurenceOrderPlugin()
+	);
+} else {
+	config.devServer = {
+		port: devServerPort,
+		headers: { 'Access-Control-Allow-Origin': '*' }
+	};
+	config.output.publicPath = '//localhost:' + devServerPort + '/webpack/';
+	// Source maps
+	config.devtool = 'cheap-module-eval-source-map';
+}
+
+module.exports = config;
