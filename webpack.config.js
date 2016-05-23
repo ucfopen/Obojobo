2a1a9c6bced64c398cc923d34b7aed331ebf6bdd
Zachary Berry <zachberry@gmail.com>
2016-05-23 08:59:07 -0400

    update webpack config to work with webpack --watch inside rails project

--- webpack.config.js
+++ webpack.config.js
@@ -4,30 +4,19 @@ var StatsPlugin = require('stats-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 // must match config.webpack.dev_server.port
-var devServerPort = 3808;
-
+// var devServerPort = 3808;
 
 // set TARGET=production on the environment to add asset fingerprints
 var production = process.env.TARGET === 'production';
 
 var config = {
 	entry: {
-		// 'obodraft': ['./src/scripts/node_modules/app/OboDraft.coffee'],
-		'application': ['./app/assets/editor/src/scripts/node_modules/editor.coffee'],
-		// 'thing': ['./src/scripts/thing/something.coffee'],
+		'editor': ['./app/assets/editor/src/scripts/node_modules/editor.coffee']
 	},
-	// output: {
-	// 	filename: '[name].js',
-	// 	publicPath: 'http://localhost:8090/assets'
-	// },
 	output: {
-		// Build assets directly in to public/webpack/, let webpack know
-		// that all webpacked assets start with webpack/
-
 		// must match config.webpack.output_dir
-		path: path.join(__dirname, '..', 'public', 'webpack'),
-		publicPath: '/webpack',
-
+		path: path.join(__dirname, '..', '..', '..', 'public', 'webpack'),
+		publicPath: 'webpack/',
 		filename: production ? '[name]-[chunkhash].js' : '[name].js'
 	},
 	module: {
@@ -40,11 +29,7 @@ var config = {
 			{
 				test: /\.scss$/,
 				loader: ExtractTextPlugin.extract(['css', 'sass'])
-			},
-			// {
-			// 	test: /\.svg$/,
-			// 	loader: 'svg-url-loader'
-			// }
+			}
 		]
 	},
 	externals: {
@@ -65,12 +50,6 @@ var config = {
 			modules: false,
 			assets: true
 		})
-		//,
-		// new webpack.DefinePlugin({
-		// 	"process.env": {
-		// 		NODE_ENV: JSON.stringify('production')
-		// 	}
-		// })
 	],
 	resolve: {
 		extensions: ['', '.js', '.coffee']
@@ -91,13 +70,13 @@ if (production) {
 		new webpack.optimize.OccurenceOrderPlugin()
 	);
 } else {
-	config.devServer = {
-		port: devServerPort,
-		headers: { 'Access-Control-Allow-Origin': '*' }
-	};
-	config.output.publicPath = '//localhost:' + devServerPort + '/webpack/';
+	// config.devServer = {
+	// 	port: devServerPort,
+	// 	headers: { 'Access-Control-Allow-Origin': '*' }
+	// };
+	//config.output.publicPath = '//localhost:' + devServerPort + '/webpack/';
 	// Source maps
-	config.devtool = 'cheap-module-eval-source-map';
+	// config.devtool = 'cheap-module-eval-source-map';
 }
 
 module.exports = config;
