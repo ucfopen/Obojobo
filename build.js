4600ce95159a009c3c61288c9c7c6c8cfb718079
Zachary Berry <zachberry@gmail.com>
2016-05-23 14:14:46 -0400

    fixes to build script and dependencies

--- build.js
+++ build.js
@@ -1,7 +1,12 @@
-// Simply calls `webpack -p`, but looks for the file since it can either be in node_modules or ../node_modules (if installed elsewhere)
+// Simply calls `webpack -p`, but looks for the file since it can either be in node_modules or ../ (if installed elsewhere)
 
 var fs = require('fs');
 var exec = require('child_process').exec;
+
+exec('pwd', function(err, stdout, stderr) {
+	console.log('PWD');
+	console.log(stdout);
+});
 
 fs.stat('./node_modules/webpack/bin/webpack.js', function(err, stat) {
 	if(err == null)
@@ -12,10 +17,10 @@ fs.stat('./node_modules/webpack/bin/webpack.js', function(err, stat) {
 	}
 	else
 	{
-		fs.stat('../node_modules_webpack/bin/webpack.js', function(err, stat) {
+		fs.stat('../webpack/bin/webpack.js', function(err, stat) {
 			if(err == null)
 			{
-				exec('../node_modules/webpack/bin/webpack.js -p', function(err, stdout, stderr) {
+				exec('../webpack/bin/webpack.js -p', function(err, stdout, stderr) {
 					console.log(stdout);
 				});
 			}
