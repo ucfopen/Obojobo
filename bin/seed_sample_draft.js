d9851e765c396da07104358d141bdb9cf352be33
Ian Turgeon <iturgeon@gmail.com>
2017-02-13 16:43:20 -0500

    adds sample draft seed script

---
+++ bin/seed_sample_draft.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+var db = require('../db');
+var fs = require('fs');
+
+const sampleKey = '00000000-0000-0000-0000-000000000000';
+const sampleDoc = 'node_modules/obojobo-draft-document-engine/test-object.json';
+
+db.query('SELECT * FROM drafts where id = $1', [sampleKey])
+.then( result => {
+  if(result.length != 1){
+    console.log('ADDING DRAFT')
+    return db.query('INSERT INTO drafts (id) VALUES($1)', [sampleKey])
+  }
+  return 0
+})
+.then( (result) => {
+  console.log(`Seeding sample draft content from ${sampleDoc}`)
+  console.log(`Sample draft id is: ${sampleKey}`)
+  let content = fs.readFileSync(sampleDoc)
+  let json = JSON.parse(content)
+  return db.query('INSERT INTO drafts_content (draft_id, content) VALUES($1, $2)', [sampleKey, json])
+})
+.then( x => {
+  process.exit();
+})
+.catch( error => {
+  console.log('error', error)
+  process.exit(-1);
+})
