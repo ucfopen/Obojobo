da1a406720a7e1e477b6128019953a4bd992595f
Ian Turgeon <iturgeon@gmail.com>
2017-02-13 16:08:03 -0500

    adds console logging 500 errors

--- app.js
+++ app.js
@@ -121,6 +121,8 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  console.log(err)
+
   // render the error page
   res.status(err.status || 500);
   res.render('error.pug');
