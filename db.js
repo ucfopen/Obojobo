6b4155ed1ce9ec274c97ae6281cbc478fbe8d72f
Ian Turgeon <iturgeon@gmail.com>
2017-03-16 15:31:22 -0400

    reformat the pg-promise connection data so it works in production

--- db.js
+++ db.js
@@ -1,6 +1,14 @@
 let pgp = require('pg-promise')(/*options*/)
 let config = require('./config')
 
-let db = pgp(config.db.connectionString);
+let cn = {
+	host: config.db.host,
+	port: config.db.port,
+	database: config.db.database,
+	user: config.db.user,
+	password: config.db.password
+}
+
+let db = pgp(cn);
 
 module.exports = db;
