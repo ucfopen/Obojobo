702cc4a7053cc740b4384657d2411f8eb4ec4a95
Zachary Berry <zachberry@gmail.com>
2017-03-23 16:13:50 -0400

    migration to add preview to attempt column

---
+++ migrations/20170323181055-modify-attempts-add-preview-column.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+  return db.addColumn('attempts', 'preview', {
+    type: 'boolean',
+    notNull: true,
+    defaultValue: false
+  })
+  .then(result => {
+    return db.addIndex('attempts', 'attempts_preview', ['preview'])
+  })
+};
+
+exports.down = function(db) {
+  return db.removeIndex('attempts', 'attempts_preview')
+  .then(result => {
+    return db.removeColumn('attempts', 'preview')
+  })
+};
+
+exports._meta = {
+  "version": 1
+};
