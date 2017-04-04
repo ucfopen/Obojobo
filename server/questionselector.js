590a53dbe70e33687e74497a1b126b8267c01662
Zachary Berry <zachberry@gmail.com>
2017-04-04 11:21:55 -0400

    * Cleaning up code

--- server/questionselector.js
+++ server/questionselector.js
@@ -1,106 +1,16 @@
-let getQuestionHistory = (attemptHistory, questionBank) => {
-	console.log('   GET QUESTION HISTORY', questionBank.node.id)
+// 1. create a tree structure from the assessment question bank
+// 2. create another tree structure from the attempt history
+// 3. use the attempt history and node settings to trim the tree,
+//  leaving only the nodes to send to the client
+// 4. flatten the tree to only questions
 
-	let attempt, question
-	let questionHistory = new Map(
-		[...questionBank.immediateChildrenSet]
-			.filter( (id) => {
-				console.log('TEST', id, questionBank.draftTree.findNodeClass(id).node.type !== 'ObojoboDraft.Chunks.QuestionBank')
-				return questionBank.draftTree.findNodeClass(id).node.type !== 'ObojoboDraft.Chunks.QuestionBank'
-			} )
-			.map( (id) => { return [id, 0] })
-	)
+let getBankOptions = (questionBankNode) => {
+	let content = questionBankNode.content
 
-	for(let i in attemptHistory)
-	{
-		attempt = attemptHistory[i]
-
-		for(let j in attempt.state.questions)
-		{
-			question = attempt.state.questions[j]
-
-			if(questionHistory.has(question.id))
-			{
-				questionHistory.set(question.id, questionHistory.get(question.id) + 1)
-			}
-		}
-	}
-
-	return questionHistory
-}
-
-let getQuestionHistoryArray = (questionHistory) => {
-	return [...questionHistory.entries()].map( (item) => { return { id:item[0], timesUsed:item[1] } } )
-}
-
-let getRandomUnseenQuestions = (questionHistory, questionBank) => {
-	let opts = getBankOptions(questionBank.node)
-	let questionHistoryArray = getQuestionHistoryArray(questionHistory)
-
-	// randomize
-	questionHistoryArray = shuffleArray(questionHistoryArray)
-
-	// sort so that the most unseen items are in the first array, the more seen items in the next, and so on
-	questionHistoryArray.sort( (a, b) => {
-		return a.timesUsed - b.timesUsed
+	return ({
+		choose: content.choose    || Infinity,
+		select: content.select    || 'sequential',
 	})
-
-	return getQuestions(questionHistoryArray, questionBank, opts.choose)
-}
-
-let getRandomAllQuestions = (questionHistory, questionBank) => {
-	let opts = getBankOptions(questionBank.node)
-	let questionHistoryArray = getQuestionHistoryArray(questionHistory)
-
-	// randomize
-	questionHistoryArray = shuffleArray(questionHistoryArray)
-
-	return getQuestions(questionHistoryArray, questionBank, opts.choose)
-}
-
-let getSequentialQuestions = (questionHistory, questionBank) => {
-	console.log('   GET SEQ', questionBank.node.id, questionBank.node.content)
-	let opts = getBankOptions(questionBank.node)
-	let questionHistoryArray = getQuestionHistoryArray(questionHistory)
-	console.log('   QH ARRAY', questionBank.node.id, questionHistoryArray)
-
-	questionHistoryArray.sort( (a, b) => {
-		if(a.timesUsed === b.timesUsed)
-		{
-			let children = [...questionBank.immediateChildrenSet]
-			return children.indexOf(a.id) - children.indexOf(b.id)
-		}
-
-		return a.timesUsed - b.timesUsed
-	})
-
-	return getQuestions(questionHistoryArray, questionBank, opts.choose)
-}
-
-let getQuestions = (questionHistoryArray, questionBank, choose) => {
-	console.log('   GET QUESTIONS', choose)
-
-	let result = questionHistoryArray
-		.filter( (questionItem) => {
-			let question = questionBank.draftTree.findNodeClass(questionItem.id)
-			let content = question.node.content
-			let limitExceeded = content.limit && content.limit !== 0 && questionItem.timesUsed >= content.limit
-
-			return !limitExceeded
-		} )
-		.slice(0, choose)
-		.map( (questionItem) => {
-			return questionBank.draftTree.findNodeClass(questionItem.id)
-		} )
-
-	console.log('   result')
-	for(let i in result)
-	{
-		console.log('      ', result[i].node.id)
-	}
-	console.log('   ---------')
-
-	return result
 }
 
 let shuffleArray = function(array) {
@@ -122,24 +32,145 @@ let shuffleArray = function(array) {
 	return array;
 }
 
-let getBankOptions = (questionBankNode) => {
-	let content = questionBankNode.content
+let constructUses = function(uses, node)
+{
+	// console.log('CU', node)
+	if(uses.has(node.id))
+	{
+		uses.set(node.id, uses.get(node.id) + 1)
+	}
 
-	return ({
-		choose: content.choose    || Infinity,
-		select: content.select    || 'sequential',
+	for(let i in node.children)
+	{
+		constructUses(uses, node.children[i])
+	}
+}
+
+let getDraftNodesFromIds = function(draftTree, ids) {
+	return ids.map( (id) => {
+		return draftTree.findNodeClass(id).toObject()
 	})
 }
 
-module.exports = function(questionBank, attemptHistory) {
-	let questionHistory = getQuestionHistory(attemptHistory, questionBank)
-	console.log('QHISTORY', questionBank.node.id, questionHistory)
-	let opts = getBankOptions(questionBank.node)
+let chooseChildren = function(uses, draftTree, choose, select, node)
+{
+	console.log('choose children-', choose, select, node.id)
 
-	switch(opts.select) {
-		case 'random-all':          return getRandomAllQuestions(questionHistory, questionBank)
-		case 'random-unseen':       return getRandomUnseenQuestions(questionHistory, questionBank)
-		case 'sequential': default: return getSequentialQuestions(questionHistory, questionBank)
+	let draftNode = draftTree.findNodeClass(node.id)
+	let myChildren = [...draftNode.immediateChildrenSet]
+	let slice
+
+	switch(select)
+	{
+		case 'sequential':
+			myChildren.sort(function(a, b) {
+				return uses.get(a) - uses.get(b)
+			})
+
+			slice = getDraftNodesFromIds(draftTree, myChildren).slice(0, choose)
+
+			break
+
+		case 'random-all':
+			myChildren = shuffleArray(myChildren)
+
+			slice = getDraftNodesFromIds(draftTree, myChildren).slice(0, choose)
+
+			break
+
+		case 'random-unseen':
+			myChildren.sort(function(a, b) {
+				if(uses.get(a) === uses.get(b))
+				{
+					return Math.random() < 0.5 ? -1 : 1;
+				}
+				return uses.get(a) - uses.get(b)
+			})
+
+			slice = myChildren.map( (id) => {
+				return draftTree.findNodeClass(id).toObject()
+			}).slice(0, choose)
+
 			break
 	}
+
+	console.log('i chose', slice.map(function(dn) { return dn.id }))
+
+	return slice
 }
+
+let trimTree = function(uses, draftTree, node)
+{
+	if(node.type === 'ObojoboDraft.Chunks.QuestionBank')
+	{
+		console.log('TEST', node.id, node.content, node.content.choose)
+		let opts = getBankOptions(node)
+		node.children = chooseChildren(uses, draftTree, opts.choose, opts.select, node)
+	}
+
+	for(let i in node.children)
+	{
+		trimTree(uses, draftTree, node.children[i])
+	}
+}
+
+let flattenTree = function(questions, node)
+{
+	if(node.type === 'ObojoboDraft.Chunks.Question')
+	{
+		questions.push(assessment.draftTree.findNodeClass(node.id))
+	}
+
+	for(let i in node.children)
+	{
+		flattenTree(questions, node.children[i])
+	}
+}
+
+let getQuiz = function(assessment, attemptHistory) {
+	let childrenIds = assessment.children[1].childrenSet
+
+	let uses = new Map()
+		childrenIds.forEach( (id) => {
+		let type = assessment.draftTree.findNodeClass(id).node.type
+		if(type === 'ObojoboDraft.Chunks.QuestionBank' || type === 'ObojoboDraft.Chunks.Question')
+		{
+			uses.set(id, 0)
+		}
+	})
+
+	for(let i in attemptHistory)
+	{
+		constructUses(uses, attemptHistory[i].state.qb)
+	}
+
+
+	console.log('uses___', uses)
+
+	assessmentQBTree = assessment.children[1].toObject()
+	// console.log('assessmentQBTree', assessmentQBTree)
+
+	trimTree(uses, assessment.draftTree, assessmentQBTree)
+
+	let questions = []
+
+	flattenTree(questions, assessmentQBTree)
+
+	return {
+		qb: assessmentQBTree,
+		questions: questions
+	}
+}
+
+module.exports = getQuiz
+
+
+
+
+
+
+
+
+
+
+
