a05049284eab416ebc6bf96634446c5a95830e04
Sidney Packer <si512833@cdl215378.local>
2017-04-13 11:43:25 -0400

    Add other return cases to question test

--- server/question.test.js
+++ server/question.test.js
@@ -1,5 +1,5 @@
 global.oboRequire = require('../../../obo_require')
-const MCChoice    = require('./question')
+const MCChoice    = require('./question') // Naming error here, currently an issue.
 const testJson    = require('../test-object.json')
 const Draft       = oboRequire('models/draft')
 
@@ -15,14 +15,25 @@ describe('question', () => {
   })
 
   it('handles sending to assessment', () => {
-    mcChoice.node.content = {practice: true}
+    mcChoice.node.content = { practice: true }
     mcChoice._listeners.get(events.sendToAssessment)()
     expect(mcChoice.node.content.practice).toBeFalsy()
   })
 
   it('handles ending of an attempt', () => {
     const rootNode = new Draft(testJson)
-    mcChoice.node.id = '12345test'
+    mcChoice.node.id = 'test12345'
     expect(mcChoice.yell(events.attemptEnd, {}, {}, rootNode.root, {}, {})).toEqual([])
+
+    // Case where there are no question responses.
+    mcChoice.node.id = 'the-content'
+    let responseHistory = []
+    expect(mcChoice.yell(events.attemptEnd, {}, {}, rootNode.root, responseHistory, {})).toEqual([])
+
+    // Case where score should be calculated.
+    let responseRecord = { question_id: mcChoice.node.id }
+    responseHistory = [responseRecord]
+    expect(mcChoice.yell(events.attemptEnd, {}, {}, rootNode.root, responseHistory, {})).toEqual([[]])
+    
   })
 })
