5f428cac2f02ce70129a43c8f2c2547bbb9b136b
Sidney Packer <si512833@cdl215378.local>
2017-06-07 15:34:37 -0400

    Read in file names from directory in node tests

--- __tests__/nodes.test.js
+++ __tests__/nodes.test.js
@@ -1,52 +1,22 @@
 const fs = require('fs')
 const xmlToDraftObject = require('../xml-to-draft-object')
 
-const nodes = [
-  'actionbutton',
-  'assessment',
-  'break',
-  'code',
-  'content',
-  'figure',
-  'heading',
-  'html',
-  'list',
-  'mathequation',
-  'mcanswer',
-  'mcassessment',
-  'mcchoice',
-  'mcfeedback',
-  'module',
-  'page',
-  'question',
-  'questionbank',
-  'table',
-  'text',
-  'youtube'
-]
+const filterFileType = arr => arr.filter(s => s.includes('.xml'))
+                                  .map(s => s.substring(0, s.length - 4))
 
-const shorthandNodes = [
-  'break',
-  'code',
-  'heading',
-  'list',
-  'table',
-  'text'
-]
+const nodes           = filterFileType(fs.readdirSync('./__tests__/obo_node_xml/'))
+const shorthandNodes  = filterFileType(fs.readdirSync('./__tests__/obo_node_shorthand/'))
 
 describe('XML to Draft Object Tranformation', () => {
   for (let node of nodes) {
     it(`Converts ${node} xml to ${node} node`, () => {
-      const xml = fs.readFileSync(`./__tests__/obo_node_xml/${node}.xml`, 'utf8')
+      const xml = fs.readFileSync(`./__tests__/obo_node_xml/${node}.xml`)
       expect(xmlToDraftObject(xml)).toMatchSnapshot()
-    })
-  }
 
-  for (let node of shorthandNodes) {
-    it(`Converts ${node} html notation to ${node} node`, () => {
-      const xml = fs.readFileSync(`./__tests__/obo_node_xml/${node}.xml`, 'utf8')
-      const xmlShorthand = fs.readFileSync(`./__tests__/obo_node_shorthand/${node}.xml`, 'utf8')
-      expect(xmlToDraftObject(xml)).toEqual(xmlToDraftObject(xmlShorthand))
+      if (shorthandNodes.includes(`${node}`)) {
+        const xmlShorthand = fs.readFileSync(`./__tests__/obo_node_shorthand/${node}.xml`)
+        expect(xmlToDraftObject(xml)).toEqual(xmlToDraftObject(xmlShorthand))
+      }
     })
   }
 })
