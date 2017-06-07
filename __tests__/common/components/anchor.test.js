a8b255d6ad9ea9fb1611e4295bd4f3b8839f7b9a
Zachary Berry <zachberry@gmail.com>
2017-06-07 10:17:21 -0400

    update anchor test

--- __tests__/common/components/anchor.test.js
+++ __tests__/common/components/anchor.test.js
@@ -2,10 +2,20 @@ import React from 'react'
 import Anchor from '../../../src/scripts/common/components/anchor'
 import renderer from 'react-test-renderer'
 
-test('Uh', () => {
+test('Anchor', () => {
 	const component = renderer.create(
 		<Anchor />
 	)
 	let tree = component.toJSON()
+
+	expect(tree).toMatchSnapshot()
+})
+
+test('Anchor shouldPreventTab', () => {
+	const component = renderer.create(
+		<Anchor shouldPreventTab={true} />
+	)
+	let tree = component.toJSON()
+
 	expect(tree).toMatchSnapshot()
 })
