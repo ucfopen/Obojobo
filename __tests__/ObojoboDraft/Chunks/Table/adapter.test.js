bdb1a2a88aa7ead43852387b98032bae8a64f7be
Sidney Packer <si512833@cdl215378.local>
2017-07-06 14:59:25 -0400

    Add tests for table chunk adapter

--- __tests__/ObojoboDraft/Chunks/Table/adapter.test.js
+++ __tests__/ObojoboDraft/Chunks/Table/adapter.test.js
@@ -3,106 +3,106 @@ import GridTextGroup from '../../../../ObojoboDraft/Chunks/Table/grid-text-group
 import StylableText from '../../../../../obojobo-draft-document-engine/src/scripts/common/text/styleable-text'
 
 describe('Table adapter', () => {
-  it('can be constructed WITHOUT attributes', () => {
-    let model = { modelState: {} }
-    TableAdapter.construct(model)
+	it('can be constructed WITHOUT attributes', () => {
+		let model = { modelState: {} }
+		TableAdapter.construct(model)
 
-    expect(model.modelState.textGroup).toEqual(GridTextGroup.create(3, 2, null))
-  })
+		expect(model.modelState.textGroup).toEqual(GridTextGroup.create(3, 2, null))
+	})
 
+	it('can be constructed WITH attributes', () => {
+		let model = { modelState: {} }
+		TableAdapter.construct(model, {
+			content: {
+				header: true,
+				textGroup: {
+					numRows: 2,
+					numCols: 2,
+					textGroup: [
+						{
+							text: {
+								value: 'First column heading'
+							}
+						},
+						{
+							text: {
+								value: 'Second column heading'
+							}
+						},
+						{
+							text: {
+								value: 'First column second row'
+							}
+						},
+						{
+							text: {
+								value: 'Second column second row'
+							}
+						}
+					]
+				}
+			}
+		})
+		let tempGridTextGroup = GridTextGroup.create(2, 2, { indent: 0, value: true })
+		tempGridTextGroup.set(0, new StylableText('First column heading'))
+		tempGridTextGroup.set(1, new StylableText('Second column heading'))
+		tempGridTextGroup.set(2, new StylableText('First column second row'))
+		tempGridTextGroup.set(3, new StylableText('Second column second row'))
 
-  it('can be constructed WITH attributes', () => {
-    let model = { modelState: {} }
-    TableAdapter.construct(model, {
-      content: {
-        header: true,
-        textGroup: {
-          numRows: 2,
-          numCols: 2,
-          textGroup: [
-            {
-              text: {
-                value: "First column heading"
-              }
-            },
-            {
-              text: {
-                value: "Second column heading"
-              }
-            },
-            {
-              text: {
-                value: "First column second row"
-              }
-            },
-            {
-              text: {
-                value: "Second column second row"
-              }
-            }
-          ]
-        }
-      }
-    })
-    let tempGridTextGroup = GridTextGroup.create(2, 2, { indent: 0, value: true })
-    tempGridTextGroup.set(0, new StylableText('First column heading'))
-    tempGridTextGroup.set(1, new StylableText('Second column heading'))
-    tempGridTextGroup.set(2, new StylableText('First column second row'))
-    tempGridTextGroup.set(3, new StylableText('Second column second row'))
+		expect(model.modelState.textGroup).toEqual(
+			GridTextGroup.fromDescriptor(tempGridTextGroup.toDescriptor(), 4, { indent: 0 }, null)
+		)
+	})
 
-    expect(model.modelState.textGroup).toEqual(GridTextGroup.fromDescriptor(tempGridTextGroup.toDescriptor(), 4, { indent: 0 }, null))
-  })
+	it('can clone', () => {
+		let a = { modelState: {} }
+		let b = { modelState: {} }
 
-  it('can clone', () => {
-    let a = { modelState: {} }
-    let b = { modelState: {} }
+		TableAdapter.construct(a)
+		TableAdapter.clone(a, b)
 
-    TableAdapter.construct(a)
-    TableAdapter.clone(a, b)
+		expect(a.modelState.textGroup).toEqual(b.modelState.textGroup)
+	})
 
-    expect(a.modelState.textGroup).toEqual(b.modelState.textGroup)
-  })
-
-  it('can convert to text', () => {
-    let model = { modelState: {} }
-    let expectedResult =
-    `------------------------
+	it('can convert to text', () => {
+		let model = { modelState: {} }
+		let expectedResult = `------------------------
 | First column heading     | Second column heading    |
 ------------------------
 | First column second row  | Second column second row |
 ------------------------`
 
-    TableAdapter.construct(model, {
-      content: {
-        header: true,
-        textGroup: {
-          numRows: 2,
-          numCols: 2,
-          textGroup: [
-            {
-              text: {
-                value: "First column heading"
-              }
-            },
-            {
-              text: {
-                value: "Second column heading"
-              }
-            },
-            {
-              text: {
-                value: "First column second row"
-              }
-            },
-            {
-              text: {
-                value: "Second column second row"
-              }
-            }
-          ]
-        }
-      }
-    })
-    expect(TableAdapter.toText(model)).toMatch(expectedResult)
-  })
+		TableAdapter.construct(model, {
+			content: {
+				header: true,
+				textGroup: {
+					numRows: 2,
+					numCols: 2,
+					textGroup: [
+						{
+							text: {
+								value: 'First column heading'
+							}
+						},
+						{
+							text: {
+								value: 'Second column heading'
+							}
+						},
+						{
+							text: {
+								value: 'First column second row'
+							}
+						},
+						{
+							text: {
+								value: 'Second column second row'
+							}
+						}
+					]
+				}
+			}
+		})
+		expect(TableAdapter.toText(model)).toMatch(expectedResult)
+	})
 })
