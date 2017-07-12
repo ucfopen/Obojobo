82d05483bfe4a95e83bd91d2e77c6bfded944432
Sidney Packer <si512833@cdl215378.local>
2017-07-12 11:49:52 -0400

    Add tests for ActionButton adapter

--- __tests__/ObojoboDraft/Chunks/ActionButton/adapter.test.js
+++ __tests__/ObojoboDraft/Chunks/ActionButton/adapter.test.js
@@ -4,89 +4,93 @@ import Common from 'Common'
 const TextGroup = Common.textGroup.TextGroup
 
 describe('ActionButton adapter', () => {
-  it('can be constructed WITHOUT attributes', () => {
-    let model = { modelState: {} }
-    let expected = { modelState: { align: "center", label: "" }}
-    ActionButtonAdapter.construct(model)
+	it('can be constructed WITHOUT attributes', () => {
+		let model = { modelState: {} }
+		let expected = { modelState: { align: 'center', label: '' } }
+		ActionButtonAdapter.construct(model)
 
-    expect(model).toEqual(expected)
-  })
+		expect(model).toEqual(expected)
+	})
 
-  it('can be constructed WITH attributes', () => {
-    let model = { modelState: {} }
-    let attrs = {
-      content: {
-        label: "Start Assessment",
-        triggers: [
-          {
-            type: "onClick",
-            actions: [
-              {
-                type: "assessment:startAttempt",
-                value: {
-                  id: "assessment"
-                }
-              }
-            ]
-          }
-        ]
-      }
-    }
-    let expected = { modelState: { label: 'Start Assessment', align: 'center' } }
-    ActionButtonAdapter.construct(model, attrs)
+	it('can be constructed WITH attributes', () => {
+		let model = { modelState: {} }
+		let attrs = {
+			content: {
+				label: 'Start Assessment',
+				triggers: [
+					{
+						type: 'onClick',
+						actions: [
+							{
+								type: 'assessment:startAttempt',
+								value: {
+									id: 'assessment'
+								}
+							}
+						]
+					}
+				]
+			}
+		}
+		let expected = { modelState: { label: 'Start Assessment', align: 'center' } }
+		ActionButtonAdapter.construct(model, attrs)
 
-    expect(model).toEqual(expected)
-  })
+		expect(model).toEqual(expected)
+	})
 
-  it('can be cloned', () => {
-    let a = { modelState: {} }
-    let b = { modelState: {} }
+	it('can be cloned', () => {
+		let a = { modelState: {} }
+		let b = { modelState: {} }
 
-    ActionButtonAdapter.construct(a)
-    ActionButtonAdapter.clone(a, b)
+		ActionButtonAdapter.construct(a)
+		ActionButtonAdapter.clone(a, b)
 
-    expect(a.modelState).toEqual(b.modelState)
-  })
+		expect(a.modelState).toEqual(b.modelState)
+	})
 
-  it('can be converted to JSON', () => {
-    let model = { modelState: {} }
-    let attrs = {
-      content: {
-        label: "Start Assessment",
-        triggers: [
-          {
-            type: "onClick",
-            actions: [
-              {
-                type: "assessment:startAttempt",
-                value: {
-                  id: "assessment"
-                }
-              }
-            ]
-          }
-        ]
-      }
-    }
+	it('can be converted to JSON', () => {
+		let model = { modelState: {} }
+		let attrs = {
+			content: {
+				label: 'Start Assessment',
+				triggers: [
+					{
+						type: 'onClick',
+						actions: [
+							{
+								type: 'assessment:startAttempt',
+								value: {
+									id: 'assessment'
+								}
+							}
+						]
+					}
+				]
+			}
+		}
 
-    let expected = {
-      content: {
-        align: "center",
-        label: "Start Assessment",
-        triggers: [{
-          actions: [{
-            type: "assessment:startAttempt",
-            value: {
-              id: "assessment"
-            }
-          }],
-          type: "onClick"
-        }]
-      }
-    }
-    ActionButtonAdapter.construct(model, attrs)
-    ActionButtonAdapter.toJSON(model, attrs) 
+		let expected = {
+			content: {
+				align: 'center',
+				label: 'Start Assessment',
+				triggers: [
+					{
+						actions: [
+							{
+								type: 'assessment:startAttempt',
+								value: {
+									id: 'assessment'
+								}
+							}
+						],
+						type: 'onClick'
+					}
+				]
+			}
+		}
+		ActionButtonAdapter.construct(model, attrs)
+		ActionButtonAdapter.toJSON(model, attrs)
 
-    expect(attrs).toEqual(expected)
-  })
+		expect(attrs).toEqual(expected)
+	})
 })
