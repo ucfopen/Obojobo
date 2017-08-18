2bb5fd8c03eb4705fb134e1782607503dcf2565e
Zachary Berry <zachberry@gmail.com>
2017-08-18 10:13:50 -0400

    fix issue where a false value for an oboGlobal would cause an error to the thrown

--- src/scripts/common/util/obo-globals.js
+++ src/scripts/common/util/obo-globals.js
@@ -4,7 +4,7 @@ let globals = new Map()
 export default {
 	get(key) {
 		if (globals.has(key)) return globals.get(key)
-		if (!window[GLOBAL_KEY][key]) throw 'No Obo Global found for key ' + key
+		if (typeof window[GLOBAL_KEY][key] === 'undefined') throw 'No Obo Global found for key ' + key
 
 		globals.set(key, window[GLOBAL_KEY][key])
 
