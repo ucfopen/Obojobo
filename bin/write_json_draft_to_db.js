2486c4004b0eea63855709675ba70fc01b4d3a79
Ian Turgeon <iturgeon@gmail.com>
2017-08-23 10:11:54 -0400

    Fixes several issues in sampleDraft:seed (#23)
    * Fixes several issues in sampleDraft:seed
    
    * converted to match changes done by @gitnix on another branch

--- bin/write_json_draft_to_db.js
+++ bin/write_json_draft_to_db.js
@@ -1,21 +1,23 @@
 #!/usr/bin/env node
-global.oboRequire = (name) => {return require(`${__dirname}/../${name}`)}
+global.oboRequire = name => {
+	return require(`${__dirname}/../${name}`)
+}
 let fs = require('fs')
-let logger = oboRequire('logger')
 
 let usageError = new Error(`Usage:
 	node write_json_draft_to_db.js insert file.json [user_id] [draft_id]
-	node write_json_draft_to_db.js update file.json draft_id`
-)
+	node write_json_draft_to_db.js update file.json draft_id`)
 
 let db = oboRequire('db')
 let insertNewDraft = oboRequire('routes/api/drafts/insert_new_draft')
 let updateDraft = oboRequire('routes/api/drafts/update_draft')
 
-let draftId, userId
+let draftId
+let userId
+let generatedDraftId
 
-try{
-	if(process.argv.length <= 2) throw usageError
+try {
+	if (process.argv.length <= 2) throw usageError
 
 	let fn = process.argv[2]
 	let jsonFilePath = process.argv[3]
@@ -23,55 +25,64 @@ try{
 	let file = fs.readFileSync(jsonFilePath)
 	let json = JSON.parse(file)
 
-	switch(fn){
+	switch (fn) {
 		case 'insert':
 			userId = process.argv[4] || 0
 			draftId = process.argv[5] || null
 
 			insertNewDraft(userId, json)
-			.then(newDraft => {
-				return db.one(`
-					UPDATE drafts
-					SET id = $[newId]
-					WHERE id = $[currentId]
-					`, {newId: draftId, currentId: newDraft.id}
-				)
-			})
-			.then(()=>{
-				logger.debug('OK');
-				process.exit()
-				return
-			})
-			.catch(err => {
-				logger.error(err.message)
-				process.exit(1)
-				return
-			})
-			break;
+				.then(newDraft => {
+					if (draftId) {
+						console.info(draftId)
+						generatedDraftId = newDraft.id
+						return db.tx('Update if given id', t => {
+							return t.batch([
+								t.none(
+									`UPDATE drafts
+									SET id = $[newId]
+									WHERE id = $[currentId]`,
+									{ newId: draftId, currentId: generatedDraftId }
+								),
+								t.none(
+									`UPDATE drafts_content
+									SET draft_id = $[newId]
+									WHERE draft_id = $[currentId]`,
+									{ newId: draftId, currentId: generatedDraftId }
+								)
+							])
+						})
+					}
+					console.info(newDraft.id)
+					return newDraft
+				})
+				.then(() => {
+					process.exit()
+				})
+				.catch(err => {
+					console.error(err.message)
+					process.exit(1)
+				})
+			break
 
 		case 'update':
 			draftId = process.argv[4] || 0
 
 			updateDraft(draftId, json)
-			.then(id => {
-				logger.debug('OK. id=' + id)
-				process.exit()
-				return
-			})
-			.catch(err => {
-				logger.error(err.message)
-				process.exit(1)
-				return
-			})
-			break;
+				.then(id => {
+					console.info('OK. id=' + id)
+					process.exit()
+				})
+				.catch(err => {
+					console.error(err.message)
+					process.exit(1)
+				})
+			break
 
 		default:
 			throw usageError
 			break
 	}
-}
-catch(e){
-	logger.error('erroror')
-	logger.error(e.message);
-	process.exit(1);
+} catch (e) {
+	console.error(e.message)
+	process.exit(1)
 }
