25ee3d7ea140fb56952c6746e232f9046de3b77c
Ian Turgeon <iturgeon@gmail.com>
2017-11-29 19:01:39 -0500

    adds tests for logout url and increases tests for profile index

--- __tests__/routes/profile.test.js
+++ __tests__/routes/profile.test.js
@@ -8,21 +8,45 @@ describe('lti route', () => {
 
 	test('registers the expected routes ', () => {
 		let editor = oboRequire('routes/profile')
-		expect(mockRouterMethods.get).toHaveBeenCalledTimes(1)
+		expect(mockRouterMethods.get).toHaveBeenCalledTimes(2)
 		expect(mockRouterMethods.get).toBeCalledWith('/', expect.any(Function))
+		expect(mockRouterMethods.get).toBeCalledWith('/logout', expect.any(Function))
+	})
+
+	test('logout calls resetCurrentUser', () => {
+		expect.assertions(4)
+		let editor = oboRequire('routes/profile')
+		let routeFunction = mockRouterMethods.get.mock.calls[1][1]
+		expect(mockRouterMethods.get.mock.calls[1][0]).toBe('/logout')
+
+		let mockReq = {
+			resetCurrentUser: jest.fn()
+		}
+
+		let mockRes = {
+			status: jest.fn(),
+			render: jest.fn(),
+			send: jest.fn()
+		}
+
+		let mockNext = jest.fn()
+
+		routeFunction(mockReq, mockRes, mockNext)
+		expect(mockReq.resetCurrentUser).toHaveBeenCalledTimes(1)
+		expect(mockRes.send).toHaveBeenCalledTimes(1)
+		expect(mockRes.send).toBeCalledWith('Logged out')
 	})
 
 	test('index calls send', () => {
-		expect.assertions(1)
+		expect.assertions(4)
 
 		let editor = oboRequire('routes/profile')
 		let User = oboRequire('models/user')
 		let routeFunction = mockRouterMethods.get.mock.calls[0][1]
+		expect(mockRouterMethods.get.mock.calls[0][0]).toBe('/')
 
 		let mockReq = {
-			getCurrentUser: () => {
-				return Promise.resolve(new User())
-			}
+			getCurrentUser: jest.fn(() => Promise.resolve(new User()))
 		}
 
 		let mockRes = {
@@ -34,6 +58,8 @@ describe('lti route', () => {
 		let mockNext = jest.fn()
 
 		return routeFunction(mockReq, mockRes, mockNext).then(() => {
+			expect(mockReq.getCurrentUser).toHaveBeenCalledTimes(1)
+			expect(mockRes.send).toHaveBeenCalledTimes(1)
 			expect(mockRes.send).toBeCalledWith('Hello guest!')
 		})
 	})
