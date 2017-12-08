8097dc9003d460643ea20bee9601c64a94ba64e7
Ian Turgeon <iturgeon@gmail.com>
2017-12-08 16:50:23 -0500

    fixes main app.js to launch properly

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express')
 
 // Load an example default Obojobo middleware
-let DefaultObojoboMiddleware = require('./middleware.default')
+const DefaultObojoboMiddleware = require('./middleware.default')
 
 // Start a web server
 const app = express()
 
 // add the middleware to Express
-app.use(DefaultObojoboMiddleware)
+DefaultObojoboMiddleware(app)
 
 module.exports = app
