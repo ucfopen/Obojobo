334169a8c9e1dba3db8ad5872d0960aa4c001c05
Zachary Berry <zachberry@gmail.com>
2018-03-01 14:06:32 -0500

    comment out the storing of a locked nav since this will be problematic before we have persisting of which page a user was on

--- viewer_events.js
+++ viewer_events.js
@@ -1,13 +1,14 @@
 let oboEvents = oboRequire('obo_events')
 let viewerState = oboRequire('viewer/viewer_state')
 
-oboEvents.on('client:nav:lock', (event, req) => {
-	setNavLocked(event.userId, event.draftId, true)
-})
+// @TODO: Enable this when we're able to restore the user to their last page
+// oboEvents.on('client:nav:lock', (event, req) => {
+// 	setNavLocked(event.userId, event.draftId, true)
+// })
 
-oboEvents.on('client:nav:unlock', (event, req) => {
-	setNavLocked(event.userId, event.draftId, false)
-})
+// oboEvents.on('client:nav:unlock', (event, req) => {
+// 	setNavLocked(event.userId, event.draftId, false)
+// })
 
 oboEvents.on('client:nav:open', (event, req) => {
 	setNavOpen(event.userId, event.draftId, true)
@@ -29,9 +30,10 @@ function setNavOpen(userId, draftId, value) {
 	viewerState.set(userId, draftId, 'nav:isOpen', 1, value)
 }
 
-function setNavLocked(userId, draftId, value) {
-	viewerState.set(userId, draftId, 'nav:isLocked', 1, value)
-}
+// @TODO: Enable this when we're able to restore the user to their last page
+// function setNavLocked(userId, draftId, value) {
+// 	viewerState.set(userId, draftId, 'nav:isLocked', 1, value)
+// }
 
 module.exports = (req, res, next) => {
 	next()
