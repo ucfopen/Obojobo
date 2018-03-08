6c0544a2357b970a1292f07a8efd1c4b1c2d1b2e
Sidney Packer <sidp@knights.ucf.edu>
2018-03-08 11:08:25 -0500

    Start setting up sequential choosing tests

--- __tests__/server/attempt-start.test.js
+++ __tests__/server/attempt-start.test.js
@@ -1,8 +1,8 @@
 import {
   startAttempt,
   getQuestionBankProperties,
-  getAssessmentChildrenMap,
-  incrementUsedQuestionIds,
+  createAssessmentUsedQuestionMap,
+  initAssessmentUsedQuestions,
   chooseQuestionsSequentially,
   createChosenQuestionTree,
   getNodeQuestions,
@@ -20,10 +20,11 @@ jest.mock('../../../../db')
 
 describe('start attempt route', () => {
   // Create an assessment children map with some test data passed in.
+  // TODO: This is kind of hacky, find a better way to test.
   const createTestAssessmentChildMap = () => {
     const types = [QUESTION_BANK_NODE_TYPE, QUESTION_NODE_TYPE]
     let i = 0
-    return getAssessmentChildrenMap({
+    return createAssessmentUsedQuestionMap({
       nodeChildrenIds: ['test', 'test1', 'test2', 'test3'],
       draftTree: {
         getChildNodeById: jest.fn(() => ({
@@ -76,13 +77,32 @@ describe('start attempt route', () => {
     ]
     const mockQbTree = { id: 'test', children: fakeChildNodes }
 
-    incrementUsedQuestionIds(mockQbTree, assessmentChildrenMap)
+    initAssessmentUsedQuestions(mockQbTree, assessmentChildrenMap)
     expect(assessmentChildrenMap).toMatchSnapshot()
   })
 
   it('can choose to display questions sequentially', () => {
-    // TODO: Use the test-object.json and test the method on the tree.
     const mockDraft = new Draft(testJson)
-    const assessmentNode = mockDraft.getChildNodeById('assessment')
+    const mockUsedQuestionMap = new Map()
+    const mockAssessmentProperties = {
+      node: { draftTree: mockDraft },
+      childrenMap: mockUsedQuestionMap
+    }
+
+    mockUsedQuestionMap.set('qb1', 1)
+    mockUsedQuestionMap.set('qb1.q1', 0)
+    mockUsedQuestionMap.set('qb1.q2', 0)
+    mockUsedQuestionMap.set('qb2', 1)
+    mockUsedQuestionMap.set('qb2.q1', 1)
+    mockUsedQuestionMap.set('qb2.q2', 1)
+
+    // TODO: Choose questions sequentially chooses the question banks to show
+    // sequentially AS WELL as the questions within those question banks. So
+    // create some probable scenarios for varying uses of each to fully test
+    // functionality.
+
+    // Choosing questions where numQuestionsPerAttempt = 1.
+    expect(chooseQuestionsSequentially(mockAssessmentProperties, 'qb1', 1)).toMatchSnapshot()
+
   })
 })
