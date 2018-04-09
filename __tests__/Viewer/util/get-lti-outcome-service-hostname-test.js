0a0f90e656a7bd4e2ebf040f0802ae639fb60564
iturgeon <iturgeon@gmail.com>
2018-04-09 17:08:41 -0400

    adds tests for get-lti-outcome-service-hostname

--- __tests__/Viewer/util/get-lti-outcome-service-hostname-test.js
+++ __tests__/Viewer/util/get-lti-outcome-service-hostname-test.js
@@ -1,9 +1,15 @@
+const Util = require('../../../src/scripts/viewer/util/get-lti-outcome-service-hostname').default
+
 describe('get-lti-outcome-service-hostname', () => {
-	test.skip('returns the hostname of the service URL', () => {
-		//@TODO
+	test('returns the hostname of the service URL', () => {
+		expect(Util('http://this-site.com/return?trick=false')).toBe('this-site.com')
+		expect(Util('https://hunter.io#test')).toBe('hunter.io')
 	})
 
-	test.skip('returns "the external system" if unable to find a hostname', () => {
-		//@TODO
+	test('returns "the external system" if unable to find a hostname', () => {
+		expect(Util('')).toBe('the external system')
+		expect(Util()).toBe('the external system')
+		expect(Util(undefined)).toBe('the external system')
+		expect(Util({})).toBe('the external system')
 	})
 })
