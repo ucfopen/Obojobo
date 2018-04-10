741a0923461ee7e4ab9d5b6dec0e5e2364ea08e6
iturgeon <iturgeon@gmail.com>
2018-04-09 22:05:26 -0400

    adds tests for questioncontent and converts it to a functional component

---
+++ __tests__/ObojoboDraft/Chunks/Question/Conent/viewer-component.test.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+
+const QuestionContent = require('../../../../../ObojoboDraft/Chunks/Question/Content/viewer-component')
+	.default
+const mockComponent = props => <div {...props} />
+
+describe('QuestionContent', () => {
+	test.skip('renders with no children', () => {
+		let props = {
+			model: {
+				children: {
+					models: []
+				}
+			},
+			moduleData: 'mockModuleData'
+		}
+
+		const component = renderer.create(<QuestionContent {...props} />)
+		let tree = component.toJSON()
+
+		expect(tree).toMatchSnapshot()
+	})
+
+	test('renders all child except the last one', () => {
+		let key = 0
+		let mockChild = {
+			getComponentClass: () => mockComponent,
+			get: () => key++
+		}
+
+		let props = {
+			model: {
+				children: {
+					models: [mockChild, mockChild, mockChild]
+				}
+			},
+			moduleData: 'mockModuleData'
+		}
+
+		const component = renderer.create(<QuestionContent {...props} />)
+
+		// this incrementing key indicates how many children were added
+		expect(key).toBe(2)
+
+		let tree = component.toJSON()
+		expect(tree).toMatchSnapshot()
+	})
+})
