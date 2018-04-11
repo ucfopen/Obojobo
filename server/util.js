87fd14460438d58b5da7b437ebe68b71324e2a8d
iturgeon <iturgeon@gmail.com>
2018-04-11 16:46:06 -0400

    brings back commented out logger call

--- server/util.js
+++ server/util.js
@@ -64,7 +64,7 @@ let zeroOutScores = oboNode => {
 }
 
 const logAndRespondToUnexpected = (errorMessage, res, req, jsError) => {
-	// logger.error('logAndRespondToUnexpected', errorMessage, jsError)
+	logger.error('logAndRespondToUnexpected', errorMessage, jsError)
 	res.unexpected(errorMessage)
 }
 
