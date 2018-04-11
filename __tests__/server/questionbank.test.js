3e22a6f2f58942756b9b8a49fe5515e250045e22
iturgeon <iturgeon@gmail.com>
2018-04-10 23:12:00 -0400

    adds test for questionbank

--- __tests__/server/questionbank.test.js
+++ __tests__/server/questionbank.test.js
@@ -1,19 +1,50 @@
-import QuestionBank from '../../server/questionbank'
+jest.mock('../../server/questionselector', () => jest.fn())
+
+const QuestionBank = require('../../server/questionbank')
+const getQuestions = require('../../server/questionselector')
 
 describe('QuestionBank', () => {
-	const questionBank = new QuestionBank()
-	const events = {
-		onSendToClient: 'ObojoboDraft.Sections.Assessment:sendToClient',
-		onAttemptStart: 'ObojoboDraft.Sections.Assessment:attemptStart'
-	}
-
-	it('has empty children array on send to client event', () => {
+	test('has empty children array on send to client event', () => {
+		const questionBank = new QuestionBank()
+		const events = {
+			onSendToClient: 'ObojoboDraft.Sections.Assessment:sendToClient',
+			onAttemptStart: 'ObojoboDraft.Sections.Assessment:attemptStart'
+		}
 		questionBank.children = ['test']
 		questionBank.yell(events.onSendToClient)
-		expect(questionBank.children).toEqual([])
+		expect(questionBank.children).toHaveLength(0)
 	})
 
-	it.skip('handles attempt start event', () => {
-		// TODO: Test this functionality once the code is in use (in express.js)
+	test('onAttemptStart inserts new questions', () => {
+		const node = { id: 9, g: true }
+		const questionBank = new QuestionBank({}, node)
+
+		const currentAttempt = {
+			getQuestions: jest.fn(() => [{ node: { id: 1 } }, questionBank.node, { node: { id: 2 } }]),
+			setQuestions: jest.fn()
+		}
+		// newQuestions
+		getQuestions.mockReturnValueOnce([{ node: { id: 'a' } }, { node: { id: 'b' } }])
+		questionBank.onAttemptStart({}, {}, {}, {}, currentAttempt)
+		expect(currentAttempt.setQuestions).toHaveBeenCalledWith([
+			{ node: { id: 1 } },
+			{ node: { id: 'a' } },
+			{ node: { id: 'b' } },
+			{ node: { id: 2 } }
+		])
+	})
+
+	test('onAttemptStart does nothing when node isnt in questions', () => {
+		const node = { id: 9, g: true }
+		const questionBank = new QuestionBank({}, node)
+
+		const currentAttempt = {
+			getQuestions: jest.fn(() => [{ node: { id: 1 } }, { node: { id: 3 } }, { node: { id: 2 } }]),
+			setQuestions: jest.fn()
+		}
+		// newQuestions
+		getQuestions.mockReturnValueOnce([{ node: { id: 'a' } }, { node: { id: 'b' } }])
+		questionBank.onAttemptStart({}, {}, {}, {}, currentAttempt)
+		expect(currentAttempt.setQuestions).not.toHaveBeenCalled()
 	})
 })
--- server/questionbank.js
+++ server/questionbank.js
@@ -15,51 +15,23 @@ class QuestionBank extends DraftNode {
 	}
 
 	onAttemptStart(req, res, assessment, attemptHistory, currentAttempt) {
-		console.log('OAS', this.node.id)
 		let allQuestions = currentAttempt.getQuestions()
-		console.log(
-			'allQuestions',
-			this.node.id,
-			allQuestions.map(function(q) {
-				return q.id
-			})
-		)
 		let newQuestions = getQuestions(this, attemptHistory)
-		console.log(
-			'newQuestions',
-			this.node.id,
-			newQuestions.map(function(q) {
-				return q.node.id
-			})
-		)
 
-		// let insertionIndex = allQuestions.length
 		let insertionIndex = null
 		if (allQuestions.indexOf(this.node) > -1) {
 			insertionIndex = allQuestions.indexOf(this.node)
-			console.log('OK INSERTION INDEX IS NOW', insertionIndex)
 			allQuestions.splice(insertionIndex, 1)
 		}
-		console.log('insertionIndex', this.node.id, insertionIndex)
+
 		if (insertionIndex === null) {
-			console.log('no insertion index, quit')
 			return
 		}
-
-		console.log('KEEP IT UP')
 
 		allQuestions = allQuestions
 			.slice(0, insertionIndex)
 			.concat(newQuestions)
 			.concat(allQuestions.slice(insertionIndex))
-
-		console.log(
-			'currentAttempt.setQuestions',
-			this.node.id,
-			allQuestions.map(function(q) {
-				return q.node.id
-			})
-		)
 
 		currentAttempt.setQuestions(allQuestions)
 	}
