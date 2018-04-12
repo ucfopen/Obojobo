b9191047406a8cfcb493bdd7d343556effb77412
iturgeon <iturgeon@gmail.com>
2018-04-12 15:28:09 -0400

    MCChoice converted to statless component

--- ObojoboDraft/Chunks/MCAssessment/MCChoice/viewer-component.js
+++ ObojoboDraft/Chunks/MCAssessment/MCChoice/viewer-component.js
@@ -8,76 +8,68 @@ let { OboModel } = Common.models
 
 let { QuestionUtil } = Viewer.util
 
-// @TODO: see if we need ref="input", if not convert to stateless component
-export default class MCChoice extends React.Component {
-	static get defaultProps() {
-		return {
-			responseType: null,
-			revealAll: false,
-			questionSubmitted: false
-		}
-	}
-
-	getQuestionModel() {
-		return this.props.model.getParentOfType('ObojoboDraft.Chunks.Question')
-	}
-
-	getInputType() {
-		switch (this.props.responseType) {
-			case 'pick-all':
-				return 'checkbox'
-			case 'pick-one':
-			case 'pick-one-multiple-correct':
-			default:
-				return 'radio'
-		}
-	}
-
-	render() {
-		let response = QuestionUtil.getResponse(
-			this.props.moduleData.questionState,
-			this.getQuestionModel()
-		) || { ids: [] }
-		let isSelected = response.ids.indexOf(this.props.model.get('id')) !== -1
-
-		return (
-			<OboComponent
-				model={this.props.model}
-				moduleData={this.props.moduleData}
-				className={
-					'obojobo-draft--chunks--mc-assessment--mc-choice' +
-					(isSelected ? ' is-selected' : ' is-not-selected') +
-					(this.props.model.modelState.score === 100 ? ' is-correct' : ' is-incorrect')
-				}
-				data-choice-label={this.props.label}
-			>
-				<input
-					ref="input"
-					type={this.getInputType()}
-					value={this.props.model.get('id')}
-					checked={isSelected}
-					onChange={function() {}}
-					name={this.props.model.parent.get('id')}
-				/>
-				<div className="children">
-					{this.props.model.children.map((child, index) => {
-						let type = child.get('type')
-						let isAnswerItem = type === 'ObojoboDraft.Chunks.MCAssessment.MCAnswer'
-						let isFeedbackItem = type === 'ObojoboDraft.Chunks.MCAssessment.MCFeedback'
-
-						if (isAnswerItem) {
-							let Component = child.getComponentClass()
-							return (
-								<Component key={child.get('id')} model={child} moduleData={this.props.moduleData} />
-							)
-						}
-					})}
-				</div>
-			</OboComponent>
-		)
+const getInputType = responseType => {
+	switch (responseType) {
+		case 'pick-all':
+			return 'checkbox'
+		case 'pick-one':
+		case 'pick-one-multiple-correct':
+		default:
+			return 'radio'
 	}
 }
 
-function __guard__(value, transform) {
-	return typeof value !== 'undefined' && value !== null ? transform(value) : undefined
+const questionIsSelected = (questionState, model) => {
+	let response = QuestionUtil.getResponse(questionState,
+		model.getParentOfType('ObojoboDraft.Chunks.Question')
+	) || { ids: [] }
+
+	return response.ids.indexOf(model.get('id')) !== -1
 }
+
+const MCChoice = props => {
+	let isSelected = questionIsSelected(props.moduleData.questionState, props.model)
+
+	return (
+		<OboComponent
+			model={props.model}
+			moduleData={props.moduleData}
+			className={
+				'obojobo-draft--chunks--mc-assessment--mc-choice' +
+				(isSelected ? ' is-selected' : ' is-not-selected') +
+				(props.model.modelState.score === 100 ? ' is-correct' : ' is-incorrect')
+			}
+			data-choice-label={props.label}
+		>
+			<input
+				type={getInputType(props.responseType)}
+				value={props.model.get('id')}
+				checked={isSelected}
+				onChange={function() {}}
+				name={props.model.parent.get('id')}
+			/>
+			<div className="children">
+				{props.model.children.map((child, index) => {
+					let type = child.get('type')
+					let isAnswerItem = type === 'ObojoboDraft.Chunks.MCAssessment.MCAnswer'
+					let isFeedbackItem = type === 'ObojoboDraft.Chunks.MCAssessment.MCFeedback'
+
+					if (isAnswerItem) {
+						let Component = child.getComponentClass()
+						return (
+							<Component key={child.get('id')} model={child} moduleData={props.moduleData} />
+						)
+					}
+				})}
+			</div>
+		</OboComponent>
+	)
+}
+
+MCChoice.defaultProps = {
+	responseType: null,
+	revealAll: false,
+	questionSubmitted: false
+}
+
+export default MCChoice
