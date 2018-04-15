1fbbff16c53b25e93ad293bfeb14085b54a00b99
iturgeon <iturgeon@gmail.com>
2018-04-15 14:56:48 -0400

    switch to docker image to use correct node version

--- bin/docker_clone_and_test.sh
+++ bin/docker_clone_and_test.sh
@@ -1,8 +1,5 @@
 #! /bin/ash
 
-# this will set the credential helper to use environment variables when git over http asks for it
-git config --global credential.helper '!f() { sleep 1; printf "username=${GIT_USER}\npassword=${GIT_PASSWORD}"; }; f'
-
 # shallow clone a specific banch into the /project directory
 git clone -b $GIT_BRANCH $GIT_REPO --depth 1 /project
 
@@ -11,4 +8,4 @@ cd /project
 # install deps
 yarn install --force
 
-yarn test --coverage
+yarn test:ci
