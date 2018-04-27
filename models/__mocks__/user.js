6958325220c486336c3dc7c77d1438d50299b704
Elli Howard <elise.howard@ucf.edu>
2018-04-27 13:15:33 -0400

    Edited user for caliper tests

--- models/__mocks__/user.js
+++ models/__mocks__/user.js
@@ -1,18 +1,27 @@
 let permissions = oboRequire('config').permissions
 
 class MockUser {
-	constructor({id=0, firstName='Guest', lastName='Guest', email='guest@obojobo.ucf.edu', username='guest', createdAt=Date.now(), roles=[]} = {}){
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-		this.username = username;
-		this.createdAt = createdAt;
-		this.roles = roles;
+	constructor(
+		{
+			id = 1,
+			firstName = 'Guest',
+			lastName = 'Guest',
+			email = 'guest@obojobo.ucf.edu',
+			username = 'guest',
+			createdAt = Date.now(),
+			roles = []
+		} = {}
+	) {
+		this.id = id
+		this.firstName = firstName
+		this.lastName = lastName
+		this.email = email
+		this.username = username
+		this.createdAt = createdAt
+		this.roles = roles
 
 		// creates 'canEditDrafts' getter if 'canEditDrafts' is set in config/role_groups.json
-		for(let permName in permissions)
-		{
+		for (let permName in permissions) {
 			Object.defineProperty(this, permName, {
 				get: this.hasPermission.bind(this, permName)
 			})
@@ -20,13 +29,13 @@ class MockUser {
 	}
 
 	hasPermission(permName) {
-		if(!permissions[permName]) return false
+		if (!permissions[permName]) return false
 		return this.hasOneOfRole(permissions[permName])
 	}
 
-	saveOrCreate(){
+	saveOrCreate() {
 		return Promise.resolve(this)
 	}
 }
 
-module.exports = MockUser;
+module.exports = MockUser
