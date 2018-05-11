c1bf249d382095acba4960b6250cbdb686d9779a
Corey Peterson <corey.peterson@ucf.edu>
2018-05-11 11:43:09 -0400

    dashboard route will now accept deeper urls

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,7 +2,8 @@ var express = require('express')
 var router = express.Router()
 
 /* GET home page. */
-router.get('/:type?', (req, res, next) => {
+
+router.get('/*', (req, res, next) => {
 	// type doesn't do anything yet, it's handled by react-router
 	res.render('data_dashboard', {
 		title: 'Obojobo Next',
