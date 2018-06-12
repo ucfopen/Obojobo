57f8f2f04b76a9baee318f06b2b2c7790e5ef81c
Zachary Berry <zachberry@gmail.com>
2018-06-12 15:35:47 -0400

    remove comments

--- __tests__/ObojoboDraft/Chunks/IFrame/viewer-component.test.js
+++ __tests__/ObojoboDraft/Chunks/IFrame/viewer-component.test.js
@@ -7,31 +7,6 @@ import renderer from 'react-test-renderer'
 import IFrame from '../../../../ObojoboDraft/Chunks/IFrame/viewer-component'
 import OboModel from '../../../../__mocks__/_obo-model-with-chunks'
 
-// const possibleModelStateValues = {
-// 	type: [undefined, 'webpage', 'media'],
-// 	newWindow: [undefined, true, false],
-// 	border: [undefined, true, false],
-// 	fit: [undefined, 'scroll', 'scale'],
-// 	src: [undefined, 'mocked-src'],
-// 	width: [undefined, 500],
-// 	height: [undefined, 400],
-// 	zoom: [undefined, 0.5, 1, 2],
-// 	newWindowSrc: [undefined, 'mocked-new-window-src'],
-// 	autoload: [undefined, true, false],
-// 	title: [undefined, 'mocked-title'],
-// 	controls: [
-// 		undefined,
-// 		['reload'],
-// 		['expand'],
-// 		['zoom'],
-// 		['reload', 'expand'],
-// 		['reload', 'zoom'],
-// 		['zoom', 'expand'],
-// 		['reload', 'zoom', 'expand']
-// 	],
-// 	expandedSize: [undefined, 'full', 'restricted']
-// }
-
 describe('IFrame', () => {
 	let model = OboModel.create({
 		id: 'id',
