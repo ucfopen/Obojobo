120cc1da956244160d7924c267870e8f344a74e7
Elli Howard <elise.howard@ucf.edu>
2018-06-13 16:00:59 -0400

    Fully covered Viewer.stores

--- __tests__/viewer/stores/assessment-store.test.js
+++ __tests__/viewer/stores/assessment-store.test.js
@@ -1,13 +1,14 @@
 import OboModel from '../../../__mocks__/_obo-model-with-chunks'
 import { Store } from '../../../src/scripts/common/store'
 import AssessmentStore from '../../../src/scripts/viewer/stores/assessment-store'
+import AssessmentUtil from '../../../src/scripts/viewer/util/assessment-util'
 import QuestionStore from '../../../src/scripts/viewer/stores/question-store'
 import NavStore from '../../../src/scripts/viewer/stores/nav-store'
+import NavUtil from '../../../src/scripts/viewer/util/nav-util'
 import APIUtil from '../../../src/scripts/viewer/util/api-util'
 import Dispatcher from '../../../src/scripts/common/flux/dispatcher'
 import ModalUtil from '../../../src/scripts/common/util/modal-util'
 import ErrorUtil from '../../../src/scripts/common/util/error-util'
-import NavUtil from '../../../src/scripts/viewer/util/nav-util'
 import QuestionUtil from '../../../src/scripts/viewer/util/question-util'
 
 jest.mock('../../../src/scripts/common/util/modal-util', () => ({ show: jest.fn() }))
@@ -17,12 +18,13 @@ jest.mock('../../../src/scripts/common/util/error-util', () => ({
 	errorResponse: jest.fn()
 }))
 
-jest.mock('../../../src/scripts/viewer/util/api-util', () => ({
-	postEvent: jest.fn(),
-	startAttempt: jest.fn()
-}))
+jest.mock('../../../src/scripts/viewer/util/question-util')
+jest.mock('../../../src/scripts/viewer/util/nav-util')
+jest.mock('../../../src/scripts/viewer/util/api-util')
+jest.mock('../../../src/scripts/viewer/util/assessment-util.js')
+jest.mock('../../../src/scripts/viewer/assessment/assessment-score-reporter')
 
-describe('AssessmentStore', () => {
+let originalError = describe('AssessmentStore', () => {
 	let getExampleAssessment = () => ({
 		id: 'rootId',
 		type: 'ObojoboDraft.Modules.Module',
@@ -94,7 +96,7 @@ describe('AssessmentStore', () => {
 		})
 	})
 
-	it('should init state with a specific structure and return it', () => {
+	test('init builds state with a specific structure and return it', () => {
 		AssessmentStore.init()
 
 		expect(AssessmentStore.getState()).toEqual({
@@ -102,7 +104,7 @@ describe('AssessmentStore', () => {
 		})
 	})
 
-	test.skip('inits with history (populates models and state, shows dialog for unfinished attempt', () => {
+	test('init builds with history (populates models and state, shows dialog for unfinished attempt', () => {
 		let q1 = OboModel.create({
 			id: 'question1',
 			type: 'ObojoboDraft.Chunks.Question'
@@ -114,28 +116,40 @@ describe('AssessmentStore', () => {
 		let history = [
 			{
 				assessmentId: 'assessmentId',
-				startTime: '1/1/2017 00:05:00',
-				endTime: '1/1/2017 0:05:20',
-				state: {
-					questions: [
-						{ id: 'question1', type: 'ObojoboDraft.Chunks.Question' },
-						{ id: 'question2', type: 'ObojoboDraft.Chunks.Question' }
-					]
-				},
-				result: {
-					assessmentScore: 100
-				}
-			},
-			{
-				assessmentId: 'assessmentId',
-				startTime: '1/2/2017 00:05:10',
-				endTime: null,
-				state: {
-					questions: [
-						{ id: 'question1', type: 'ObojoboDraft.Chunks.Question' },
-						{ id: 'questionNonExistant', type: 'ObojoboDraft.Chunks.Question' }
-					]
-				}
+				attempts: [
+					{
+						assessmentId: 'assessmentId',
+						startTime: '1/1/2017 00:05:00',
+						endTime: '1/1/2017 0:05:20',
+						state: {
+							questions: [
+								{ id: 'question1', type: 'ObojoboDraft.Chunks.Question' },
+								{ id: 'question2', type: 'ObojoboDraft.Chunks.Question' }
+							]
+						},
+						result: {
+							assessmentScore: 100
+						},
+						questionScores: [{ id: 'question1' }, { id: 'question2' }],
+						isFinished: true
+					},
+					{
+						assessmentId: 'assessmentId',
+						startTime: '1/2/2017 00:05:00',
+						endTime: '1/2/2017 0:05:20',
+						state: {
+							questions: [
+								{ id: 'question1', type: 'ObojoboDraft.Chunks.Question' },
+								{ id: 'question2', type: 'ObojoboDraft.Chunks.Question' }
+							]
+						},
+						result: {
+							assessmentScore: 100
+						},
+						questionScores: [{ id: 'question1' }, { id: 'question2' }],
+						isFinished: false
+					}
+				]
 			}
 		]
 
@@ -144,33 +158,36 @@ describe('AssessmentStore', () => {
 		expect(AssessmentStore.getState()).toEqual({
 			assessments: {
 				assessmentId: {
-					id: 'assessmentId',
-					current: null,
-					currentResponses: [],
 					attempts: [
 						{
 							assessmentId: 'assessmentId',
-							startTime: '1/1/2017 00:05:00',
 							endTime: '1/1/2017 0:05:20',
+							isFinished: true,
+							questionScores: [{ id: 'question1' }, { id: 'question2' }],
+							result: { assessmentScore: 100 },
+							startTime: '1/1/2017 00:05:00',
 							state: {
 								questions: [
 									{ id: 'question1', type: 'ObojoboDraft.Chunks.Question' },
 									{ id: 'question2', type: 'ObojoboDraft.Chunks.Question' }
 								]
-							},
-							result: {
-								assessmentScore: 100
 							}
 						}
 					],
-					score: 100,
-					lti: null
+					current: null,
+					currentResponses: [],
+					highestAssessmentScoreAttempts: undefined,
+					highestAttemptScoreAttempts: undefined,
+					id: 'assessmentId',
+					isShowingAttemptHistory: false,
+					lti: undefined,
+					ltiErrorCount: 0,
+					ltiNetworkState: 'idle'
 				}
 			}
 		})
 
-		expect(Object.keys(OboModel.models).length).toEqual(3)
-		expect(OboModel.models.questionNonExistant.id).toEqual('questionNonExistant')
+		expect(Object.keys(OboModel.models).length).toEqual(2)
 		expect(ModalUtil.show).toHaveBeenCalledTimes(1)
 	})
 
@@ -223,6 +240,29 @@ describe('AssessmentStore', () => {
 		})
 	})
 
+	test('tryStartAttempt catches unexpected errors', () => {
+		OboModel.create(getExampleAssessment())
+
+		APIUtil.startAttempt.mockResolvedValueOnce({
+			status: 'error',
+			value: {
+				message: 'attempt limit reached'
+			}
+		})
+		ErrorUtil.show.mockImplementationOnce(() => {
+			throw new Error('Mock Error')
+		})
+		let originalError = console.error
+		console.error = jest.fn()
+
+		return AssessmentStore.tryStartAttempt('assessmentId').then(res => {
+			let errorMock = console.error
+			console.error = originalError
+			expect(ErrorUtil.show).toHaveBeenCalledTimes(1)
+			expect(errorMock).toHaveBeenCalledWith(expect.any(Error))
+		})
+	})
+
 	test('startAttempt injects question models, creates state, updates the nav and processes the onStartAttempt trigger', () => {
 		mockValidStartAttempt()
 		OboModel.create(getExampleAssessment())
@@ -248,12 +288,156 @@ describe('AssessmentStore', () => {
 		})
 	})
 
+	test('startAttempt builds an attempt if it doesnt exist', () => {
+		mockValidStartAttempt()
+		OboModel.create(getExampleAssessment())
+
+		AssessmentStore.setState({ assessments: { assessmentId: {} } })
+
+		NavUtil.rebuildMenu = jest.fn()
+		NavUtil.goto = jest.fn()
+
+		let assessmentModel = OboModel.models.rootId.children.at(0)
+		let qBank = assessmentModel.children.at(1)
+
+		assessmentModel.processTrigger = jest.fn()
+
+		return AssessmentStore.tryStartAttempt('assessmentId').then(res => {
+			expect(assessmentModel.children.length).toBe(2)
+			expect(qBank.children.length).toBe(2)
+			expect(qBank.children.at(0).id).toBe('q1')
+			expect(qBank.children.at(1).id).toBe('q2')
+			expect(NavUtil.rebuildMenu).toHaveBeenCalledTimes(1)
+			expect(NavUtil.goto).toHaveBeenCalledTimes(1)
+			expect(NavUtil.goto).toHaveBeenCalledWith('assessmentId')
+			expect(OboModel.models.assessmentId.processTrigger).toHaveBeenCalledWith('onStartAttempt')
+			expect(AssessmentStore.triggerChange).toHaveBeenCalledTimes(1)
+		})
+	})
+
+	test('tryResendLTIScore catches unexpected errors', () => {
+		OboModel.create(getExampleAssessment())
+
+		AssessmentUtil.getAssessmentForModel.mockReturnValueOnce({})
+		AssessmentStore.setState({ assessments: { assessmentId: {} } })
+
+		APIUtil.resendLTIAssessmentScore.mockResolvedValueOnce({
+			status: 'error',
+			value: {
+				message: 'attempt limit reached'
+			}
+		})
+		ErrorUtil.errorResponse.mockImplementationOnce(() => {
+			throw new Error('Mock Error')
+		})
+		let originalError = console.error
+		console.error = jest.fn()
+
+		return AssessmentStore.tryResendLTIScore('assessmentId').then(res => {
+			let errorMock = console.error
+			console.error = originalError
+			expect(ErrorUtil.errorResponse).toHaveBeenCalledTimes(1)
+			expect(errorMock).toHaveBeenCalledWith(expect.any(Error))
+		})
+	})
+
+	test('tryResendLTIScore returns with error', () => {
+		expect.assertions(2)
+		mockValidStartAttempt()
+		OboModel.create(getExampleAssessment())
+
+		AssessmentStore.setState({
+			assessments: {
+				assessmentId: {}
+			}
+		})
+
+		AssessmentUtil.getAssessmentForModel.mockReturnValueOnce(
+			AssessmentStore.getState().assessments.assessmentId
+		)
+		APIUtil.resendLTIAssessmentScore.mockResolvedValueOnce({
+			status: 'error',
+			value: {
+				message: 'Some unexpected error that was not accounted for'
+			}
+		})
+
+		return AssessmentStore.tryResendLTIScore('assessmentId').then(res => {
+			expect(ErrorUtil.errorResponse).toHaveBeenCalledTimes(1)
+			expect(AssessmentStore.triggerChange).toHaveBeenCalledTimes(1)
+		})
+	})
+
+	test('tryResendLTIScore updates and triggersChange', () => {
+		expect.assertions(2)
+		mockValidStartAttempt()
+		OboModel.create(getExampleAssessment())
+
+		AssessmentStore.setState({
+			assessments: {
+				assessmentId: {}
+			}
+		})
+
+		AssessmentUtil.getAssessmentForModel.mockReturnValueOnce(
+			AssessmentStore.getState().assessments.assessmentId
+		)
+		AssessmentUtil.getAssessmentForModel.mockReturnValueOnce(
+			AssessmentStore.getState().assessments.assessmentId
+		)
+
+		APIUtil.resendLTIAssessmentScore.mockResolvedValueOnce({
+			status: 'success',
+			value: {}
+		})
+
+		return AssessmentStore.tryResendLTIScore('assessmentId').then(res => {
+			expect(ErrorUtil.errorResponse).toHaveBeenCalledTimes(0)
+			expect(AssessmentStore.triggerChange).toHaveBeenCalledTimes(2)
+		})
+	})
+
+	test('updateLTIScore counts errors', () => {
+		AssessmentUtil.isLTIScoreNeedingToBeResynced.mockReturnValueOnce(true)
+		let assessment = {
+			ltiErrorCount: 0
+		}
+
+		AssessmentStore.updateLTIScore(assessment)
+
+		expect(assessment.ltiErrorCount).toEqual(1)
+	})
+
+	test('tryEndAttempt catches unexpected errors', () => {
+		OboModel.create(getExampleAssessment())
+
+		AssessmentStore.setState({ assessments: { assessmentId: {} } })
+
+		APIUtil.endAttempt.mockResolvedValueOnce({
+			status: 'error',
+			value: {
+				message: 'attempt limit reached'
+			}
+		})
+		ErrorUtil.errorResponse.mockImplementationOnce(() => {
+			throw new Error('Mock Error')
+		})
+		let originalError = console.error
+		console.error = jest.fn()
+
+		return AssessmentStore.tryEndAttempt('assessmentId').then(res => {
+			let errorMock = console.error
+			console.error = originalError
+			expect(ErrorUtil.errorResponse).toHaveBeenCalledTimes(1)
+			expect(errorMock).toHaveBeenCalledWith(expect.any(Error))
+		})
+	})
+
 	test('endAttempt shows an error if the endAttempt request fails and triggers a change', () => {
 		expect.assertions(2)
 		mockValidStartAttempt()
 		OboModel.create(getExampleAssessment())
 
-		APIUtil.endAttempt = jest.fn()
 		APIUtil.endAttempt.mockResolvedValueOnce({
 			status: 'error',
 			value: {
@@ -269,10 +453,36 @@ describe('AssessmentStore', () => {
 			})
 	})
 
-	test.skip('endAttempt hides questions, resets responses, updates state, processes onEndAttempt trigger and triggers a change', () => {
-		expect.assertions(6)
+	test('endAttempt hides questions, resets responses, updates state, processes onEndAttempt trigger and triggers a change', () => {
+		expect.assertions(4)
 		mockValidStartAttempt()
 		OboModel.create(getExampleAssessment())
+
+		APIUtil.endAttempt = jest.fn()
+		APIUtil.endAttempt.mockResolvedValueOnce({
+			status: 'mockStatus',
+			value: {
+				assessmentId: 'assessmentId',
+				attempts: [
+					{
+						assessmentId: 'assessmentId',
+						endTime: '1/1/2017 0:05:20',
+						isFinished: true,
+						questionScores: [{ id: 'question1' }, { id: 'question2' }],
+						result: { assessmentScore: 100 },
+						startTime: '1/1/2017 00:05:00',
+						state: {
+							questions: [
+								{ id: 'question1', type: 'ObojoboDraft.Chunks.Question' },
+								{ id: 'question2', type: 'ObojoboDraft.Chunks.Question' }
+							]
+						}
+					}
+				]
+			}
+		})
+
+		AssessmentUtil.getLastAttemptForModel.mockReturnValueOnce({ attemptNumber: 1 })
 
 		return AssessmentStore.tryStartAttempt('assessmentId')
 			.then(() => AssessmentStore.trySetResponse('q1', { responseForR1: 'someValue' }))
@@ -282,8 +492,6 @@ describe('AssessmentStore', () => {
 				expect(QuestionUtil.hideQuestion).toHaveBeenCalledTimes(2)
 				expect(QuestionUtil.hideQuestion).toHaveBeenCalledWith('q1')
 				expect(QuestionUtil.hideQuestion).toHaveBeenCalledWith('q2')
-				expect(OboModel.models.assessmentId.processTrigger).toHaveBeenCalledWith('onEndAttempt')
-				expect(AssessmentStore.triggerChange).toHaveBeenCalledTimes(3)
 			})
 	})
 
@@ -295,7 +503,91 @@ describe('AssessmentStore', () => {
 		return AssessmentStore.tryStartAttempt('assessmentId')
 			.then(() => AssessmentStore.trySetResponse('q1', ['some response']))
 			.then(res => {
-				expect(AssessmentStore.getState().assessments.assessmentId.currentResponses).toEqual(['q1'])
+				expect(AssessmentUtil.getAssessmentForModel).toHaveBeenCalled()
 			})
+	})
+
+	test('setState will update state', () => {
+		expect.assertions(1)
+
+		AssessmentStore.setState('mockState')
+
+		expect(AssessmentStore.getState()).toEqual('mockState')
+	})
+
+	test('assessment:startAttempt calls tryStartAttempt', () => {
+		jest.spyOn(AssessmentStore, 'tryStartAttempt')
+		AssessmentStore.tryStartAttempt.mockReturnValueOnce('mock')
+
+		Dispatcher.trigger('assessment:startAttempt', { value: {} })
+
+		expect(AssessmentStore.tryStartAttempt).toHaveBeenCalled()
+	})
+
+	test('assessment:endAttempt calls tryEndAttempt', () => {
+		jest.spyOn(AssessmentStore, 'tryEndAttempt')
+		AssessmentStore.tryEndAttempt.mockReturnValueOnce('mock')
+
+		AssessmentStore.setState({
+			assessments: {
+				assessmentId: {}
+			}
+		})
+		APIUtil.endAttempt.mockResolvedValueOnce({
+			status: 'error',
+			value: {
+				message: 'Some unexpected error that was not accounted for'
+			}
+		})
+
+		Dispatcher.trigger('assessment:endAttempt', { value: { id: 'assessmentId' } })
+
+		expect(AssessmentStore.tryEndAttempt).toHaveBeenCalled()
+	})
+
+	test('assessment:resendLTIScore calls tryResendLTIScore', () => {
+		jest.spyOn(AssessmentStore, 'tryResendLTIScore')
+		AssessmentStore.tryResendLTIScore.mockReturnValueOnce('mock')
+
+		AssessmentStore.setState({
+			assessments: {
+				assessmentId: {}
+			}
+		})
+		APIUtil.endAttempt.mockResolvedValueOnce({
+			status: 'error',
+			value: {
+				message: 'Some unexpected error that was not accounted for'
+			}
+		})
+
+		Dispatcher.trigger('assessment:resendLTIScore', { value: { id: 'assessmentId' } })
+
+		expect(AssessmentStore.tryResendLTIScore).toHaveBeenCalled()
+	})
+
+	test('question:setResponse calls trySetResponse', () => {
+		jest.spyOn(AssessmentStore, 'trySetResponse')
+		AssessmentStore.trySetResponse.mockReturnValueOnce('mock')
+
+		Dispatcher.trigger('question:setResponse', { value: { id: 'q1' } })
+
+		expect(AssessmentStore.trySetResponse).toHaveBeenCalled()
+	})
+
+	test('viewer:closeAttempted calls AssessmentUtil', () => {
+		Dispatcher.trigger('viewer:closeAttempted')
+
+		expect(AssessmentUtil.isInAssessment).toHaveBeenCalled()
+	})
+
+	test('viewer:closeAttempted calls AssessmentUtil', () => {
+		AssessmentUtil.isInAssessment.mockReturnValueOnce(true)
+		let mockFunction = jest.fn()
+
+		Dispatcher.trigger('viewer:closeAttempted', mockFunction)
+
+		expect(AssessmentUtil.isInAssessment).toHaveBeenCalled()
+		expect(mockFunction).toHaveBeenCalled()
 	})
 })
--- __tests__/viewer/stores/question-store.test.js
+++ __tests__/viewer/stores/question-store.test.js
@@ -95,7 +95,7 @@ describe('QuestionStore', () => {
 		UUID.mockReturnValue('mock-uuid')
 	})
 
-	it('should init state with a specific structure and return it', () => {
+	test('init builds state with a specific structure and return it', () => {
 		QuestionStore.init()
 
 		expect(QuestionStore.getState()).toEqual({
@@ -107,18 +107,18 @@ describe('QuestionStore', () => {
 		})
 	})
 
-	it('should set state', () => {
+	test('should set state', () => {
 		QuestionStore.setState({ x: 1 })
 
 		expect(QuestionStore.getState()).toEqual({ x: 1 })
 	})
 
-	it('registers the expected dispatch listeners', () => {
+	test('registers the expected dispatch listeners', () => {
 		// See where eventListeners is stored above ^
 		expect(eventListeners).toMatchSnapshot()
 	})
 
-	it('question:setResponse calls triggerChange and postEvent', () => {
+	test('question:setResponse calls triggerChange and postEvent', () => {
 		__createModels()
 
 		__mockTrigger('question:setResponse', {
@@ -139,7 +139,7 @@ describe('QuestionStore', () => {
 		})
 	})
 
-	it('question:clearResponse calls triggerChange and updates state', () => {
+	test('question:clearResponse calls triggerChange and updates state', () => {
 		QuestionStore.setState({ responses: { mockContext: { mockId: true } } })
 
 		__mockTrigger('question:clearResponse', {
@@ -157,7 +157,20 @@ describe('QuestionStore', () => {
 		})
 	})
 
-	it("question:clearResponse calls triggerChange but doesn't change state", () => {
+	test('question:clearResponse does nothing with no context', () => {
+		QuestionStore.setState({ responses: {} })
+
+		__mockTrigger('question:clearResponse', {
+			value: {
+				context: 'mockContext',
+				id: 'mockId'
+			}
+		})
+
+		expect(QuestionStore.triggerChange).not.toHaveBeenCalled()
+	})
+
+	test("question:clearResponse calls triggerChange but doesn't change state", () => {
 		QuestionStore.setState({ responses: { mockContext: { mockId: true } } })
 
 		__mockTrigger('question:clearResponse', {
@@ -175,7 +188,56 @@ describe('QuestionStore', () => {
 		})
 	})
 
-	it('question:setData calls triggerChange and updates state', () => {
+	test('assessment:endAttempt calls triggerChange and updates state', () => {
+		QuestionStore.setState({ responses: { mockContext: { mockId: true } } })
+
+		__mockTrigger('assessment:endAttempt', {
+			value: {
+				context: 'mockContext',
+				id: 'mockId'
+			}
+		})
+
+		expect(QuestionStore.triggerChange).toHaveBeenCalledTimes(1)
+		expect(QuestionStore.getState()).toEqual({
+			responses: {
+				mockContext: {}
+			}
+		})
+	})
+
+	test('assessment:endAttempt does nothing with no context', () => {
+		QuestionStore.setState({ responses: {} })
+
+		__mockTrigger('assessment:endAttempt', {
+			value: {
+				context: 'mockContext',
+				id: 'mockId'
+			}
+		})
+
+		expect(QuestionStore.triggerChange).not.toHaveBeenCalled()
+	})
+
+	test("assessment:endAttempt calls triggerChange but doesn't change state", () => {
+		QuestionStore.setState({ responses: { mockContext: { mockId: true } } })
+
+		__mockTrigger('assessment:endAttempt', {
+			value: {
+				context: 'mockContext',
+				id: 'otherMockId'
+			}
+		})
+
+		expect(QuestionStore.triggerChange).toHaveBeenCalledTimes(1)
+		expect(QuestionStore.getState()).toEqual({
+			responses: {
+				mockContext: { mockId: true }
+			}
+		})
+	})
+
+	test('question:setData calls triggerChange and updates state', () => {
 		QuestionStore.setState({
 			data: {}
 		})
@@ -195,7 +257,7 @@ describe('QuestionStore', () => {
 		})
 	})
 
-	it('qquestion:clearData calls triggerChange and updates state', () => {
+	test('question:clearData calls triggerChange and updates state', () => {
 		QuestionStore.setState({
 			data: {
 				dataKey: 'dataValue'
@@ -214,7 +276,7 @@ describe('QuestionStore', () => {
 		})
 	})
 
-	it('question:clearData calls triggerChange even when theres no state change', () => {
+	test('question:clearData calls triggerChange even when theres no state change', () => {
 		QuestionStore.setState({
 			data: {
 				dataKey: 'dataValue'
@@ -235,7 +297,7 @@ describe('QuestionStore', () => {
 		})
 	})
 
-	it('question:view calls triggerChange and updates state', () => {
+	test('question:view calls triggerChange and updates state', () => {
 		__createModels()
 
 		QuestionStore.setState({
@@ -258,7 +320,7 @@ describe('QuestionStore', () => {
 		})
 	})
 
-	it('question:view updates state.viewing with the latest question', () => {
+	test('question:view updates state.viewing with the latest question', () => {
 		__createModels()
 
 		QuestionStore.setState({
@@ -296,7 +358,7 @@ describe('QuestionStore', () => {
 		})
 	})
 
-	it('question:hide marks questions as hidden, and clears viewing if it matches', () => {
+	test('question:hide marks questions as hidden, and clears viewing if it matches', () => {
 		__createModels()
 
 		QuestionStore.setState({
@@ -322,7 +384,7 @@ describe('QuestionStore', () => {
 		})
 	})
 
-	it('question:hide marks questions as hidden, and does not clear viewing if it doesnt match', () => {
+	test('question:hide marks questions as hidden, and does not clear viewing if it doesnt match', () => {
 		__createModels()
 
 		QuestionStore.setState({
@@ -348,7 +410,7 @@ describe('QuestionStore', () => {
 		})
 	})
 
-	it('question:showExplanation calls postEvent and sets question data', () => {
+	test('question:showExplanation calls postEvent and sets question data', () => {
 		__createModels()
 
 		__mockTrigger('question:showExplanation', {
@@ -366,7 +428,7 @@ describe('QuestionStore', () => {
 		expect(QuestionUtil.clearData).not.toHaveBeenCalled()
 	})
 
-	it('question:hideExplanation calls triggerChange and updates state', () => {
+	test('question:hideExplanation calls triggerChange and updates state', () => {
 		__createModels()
 
 		__mockTrigger('question:hideExplanation', {
@@ -385,7 +447,7 @@ describe('QuestionStore', () => {
 		expect(QuestionUtil.setData).not.toHaveBeenCalled()
 	})
 
-	it('question:scoreSet calls triggerChange', () => {
+	test('question:scoreSet calls triggerChange', () => {
 		__createModels()
 
 		expect(QuestionStore.triggerChange).toHaveBeenCalledTimes(0)
@@ -401,7 +463,7 @@ describe('QuestionStore', () => {
 		expect(QuestionStore.triggerChange).toHaveBeenCalledTimes(1)
 	})
 
-	it('question:scoreSet initializes context if missing', () => {
+	test('question:scoreSet initializes context if missing', () => {
 		__createModels()
 
 		expect(QuestionStore.getState().scores).not.toHaveProperty('mockContext')
@@ -417,7 +479,7 @@ describe('QuestionStore', () => {
 		expect(QuestionStore.getState().scores).toHaveProperty('mockContext')
 	})
 
-	it('question:scoreSet blurs focus if correct', () => {
+	test('question:scoreSet blurs focus if correct', () => {
 		__createModels()
 
 		expect(FocusUtil.unfocus).toHaveBeenCalledTimes(0)
@@ -433,7 +495,7 @@ describe('QuestionStore', () => {
 		expect(FocusUtil.unfocus).toHaveBeenCalledTimes(1)
 	})
 
-	it('question:scoreSet doesnt blur if incorrect', () => {
+	test('question:scoreSet doesnt blur if incorrect', () => {
 		__createModels()
 
 		__mockTrigger('question:scoreSet', {
@@ -447,7 +509,7 @@ describe('QuestionStore', () => {
 		expect(FocusUtil.unfocus).not.toHaveBeenCalled()
 	})
 
-	it('question:scoreSet updates the state', () => {
+	test('question:scoreSet updates the state', () => {
 		__createModels()
 
 		expect(QuestionStore.getState()).toMatchSnapshot()
@@ -463,7 +525,7 @@ describe('QuestionStore', () => {
 		expect(QuestionStore.getState()).toMatchSnapshot()
 	})
 
-	it('question:scoreSet posts an event', () => {
+	test('question:scoreSet posts an event', () => {
 		__createModels()
 
 		__mockTrigger('question:scoreSet', {
@@ -478,7 +540,7 @@ describe('QuestionStore', () => {
 		expect(APIUtil.postEvent.mock.calls[0]).toMatchSnapshot()
 	})
 
-	it('question:scoreClear calls triggerChange', () => {
+	test('question:scoreClear calls triggerChange', () => {
 		__createModels()
 		QuestionStore.setState({
 			scores: {
@@ -501,7 +563,7 @@ describe('QuestionStore', () => {
 		expect(QuestionStore.triggerChange).toHaveBeenCalledTimes(1)
 	})
 
-	it('question:scoreClear triggers an event', () => {
+	test('question:scoreClear triggers an event', () => {
 		__createModels()
 		QuestionStore.setState({
 			scores: {
@@ -524,7 +586,7 @@ describe('QuestionStore', () => {
 		expect(QuestionStore.triggerChange).toHaveBeenCalledTimes(1)
 	})
 
-	it('question:scoreClear posts an event', () => {
+	test('question:scoreClear posts an event', () => {
 		__createModels()
 		QuestionStore.setState({
 			scores: {
@@ -548,7 +610,7 @@ describe('QuestionStore', () => {
 		expect(APIUtil.postEvent.mock.calls[0]).toMatchSnapshot()
 	})
 
-	it('question:retry clears responses, hides explanations and clears scores', () => {
+	test('question:retry clears responses, hides explanations and clears scores', () => {
 		__createModels()
 
 		QuestionUtil.isShowingExplanation.mockReturnValue(true)
@@ -571,6 +633,34 @@ describe('QuestionStore', () => {
 
 		// make sure hideExplanation was called
 		expect(QuestionUtil.hideExplanation).toHaveBeenCalledWith('questionId', 'viewerClient')
+
+		// make sure state.responses.mockContext.questionId is wiped out
+		expect(QuestionStore.getState().responses.mockContext).not.toHaveProperty('questionId')
+	})
+
+	test('question:retry only clears scores with no explanation', () => {
+		__createModels()
+
+		QuestionUtil.isShowingExplanation.mockReturnValue(false)
+
+		QuestionStore.setState({
+			viewing: 'questionId',
+			viewedQuestions: { questionId: true },
+			responses: { mockContext: { questionId: 'response' } }
+		})
+
+		__mockTrigger('question:retry', {
+			value: {
+				context: 'mockContext',
+				id: 'questionId'
+			}
+		})
+
+		// make sure clear score was called
+		expect(QuestionUtil.clearScore).toHaveBeenCalledWith('questionId', 'mockContext')
+
+		// make sure hideExplanation was called
+		expect(QuestionUtil.hideExplanation).not.toHaveBeenCalled()
 
 		// make sure state.responses.mockContext.questionId is wiped out
 		expect(QuestionStore.getState().responses.mockContext).not.toHaveProperty('questionId')
