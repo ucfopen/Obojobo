74c847918c6d5a93621803106685134b141a4fca
Zachary Berry <zachberry@gmail.com>
2018-07-05 10:49:42 -0400

    Creating branch to keep track of the expand button feature

--- ObojoboDraft/Chunks/IFrame/render-settings.js
+++ ObojoboDraft/Chunks/IFrame/render-settings.js
@@ -1,24 +1,27 @@
+//@TODO: This includes the expand button features. Need to make this work in all containers
+//including those that create a new stacking context
+
 import Viewer from 'Viewer'
-let MediaUtil = Viewer.util.MediaUtil
+const MediaUtil = Viewer.util.MediaUtil
 
-let SIZE_STATE_EXPANDED = 'expanded'
-let SIZE_STATE_ABLE_TO_EXPAND = 'ableToExpand'
-let SIZE_STATE_UNABLE_TO_EXPAND = 'unableToExpand'
+const SIZE_STATE_EXPANDED = 'expanded'
+const SIZE_STATE_ABLE_TO_EXPAND = 'ableToExpand'
+const SIZE_STATE_UNABLE_TO_EXPAND = 'unableToExpand'
 
-let getIsShowing = (mediaState, model) => {
+const getIsShowing = (mediaState, model) => {
 	return (
 		(model.modelState.autoload || MediaUtil.isShowingMedia(mediaState, model)) &&
 		model.modelState.src !== null
 	)
 }
 
-let getControlsOptions = (modelState, sizeState) => {
-	let isZoomControlEnabled = modelState.controls.indexOf('zoom') > -1
-	let isReloadControlEnabled = modelState.controls.indexOf('reload') > -1
-	let isExpandControlNeeded =
+const getControlsOptions = (modelState, sizeState) => {
+	const isZoomControlEnabled = modelState.controls.indexOf('zoom') > -1
+	const isReloadControlEnabled = modelState.controls.indexOf('reload') > -1
+	const isExpandControlNeeded =
 		modelState.controls.indexOf('expand') > -1 && sizeState === SIZE_STATE_ABLE_TO_EXPAND
-	let isUnexpandControlNeeded = sizeState === SIZE_STATE_EXPANDED
-	let newWindowEnabled = modelState.newWindow !== null
+	const isUnexpandControlNeeded = sizeState === SIZE_STATE_EXPANDED
+	const newWindowEnabled = modelState.newWindow !== null
 
 	return {
 		zoom: isZoomControlEnabled,
@@ -35,18 +38,19 @@ let getControlsOptions = (modelState, sizeState) => {
 	}
 }
 
-let getSizeState = (expandedSize, scaleAmount, mediaSize) => {
+const getSizeState = (expandedSize, scaleAmount, mediaSize) => {
 	if (mediaSize === 'large') return SIZE_STATE_EXPANDED
-	if (expandedSize === 'full' || (expandedSize === 'restricted' && scaleAmount < 1))
+	if (expandedSize === 'full' || (expandedSize === 'restricted' && scaleAmount < 1)) {
 		return SIZE_STATE_ABLE_TO_EXPAND
+	}
 	return SIZE_STATE_UNABLE_TO_EXPAND
 }
 
-let getMediaSize = (mediaState, model, defaultSizeIfNotSet) => {
+const getMediaSize = (mediaState, model, defaultSizeIfNotSet) => {
 	return MediaUtil.getSize(mediaState, model) || defaultSizeIfNotSet
 }
 
-let getDisplayedTitle = modelState => {
+const getDisplayedTitle = modelState => {
 	if (modelState.src === null) {
 		return 'IFrame missing src attribute'
 	} else if (modelState.title) {
@@ -56,18 +60,18 @@ let getDisplayedTitle = modelState => {
 	return (modelState.src || '').replace(/^https?:\/\//, '')
 }
 
-let getSetDimensions = (modelState, defaultWidth, defaultHeight) => {
+const getSetDimensions = (modelState, defaultWidth, defaultHeight) => {
 	return {
 		w: modelState.width || defaultWidth,
 		h: modelState.height || defaultHeight
 	}
 }
 
-let getScaleAmount = (actualWidth, padding, setWidth) => {
+const getScaleAmount = (actualWidth, padding, setWidth) => {
 	return Math.min(1, (actualWidth - padding) / setWidth)
 }
 
-let getScaleDimensions = (modelState, zoom, isExpanded, scaleAmount, minScale, setDimensions) => {
+const getScaleDimensions = (modelState, zoom, isExpanded, scaleAmount, minScale, setDimensions) => {
 	let scale
 	let containerStyle = {}
 
@@ -78,18 +82,16 @@ let getScaleDimensions = (modelState, zoom, isExpanded, scaleAmount, minScale, s
 			containerStyle.maxWidth = setDimensions.w
 			containerStyle.maxHeight = setDimensions.h
 		}
+	} else if (modelState.fit === 'scroll') {
+		scale = zoom
+		containerStyle = {
+			width: setDimensions.w,
+			height: setDimensions.h
+		}
 	} else {
-		if (modelState.fit === 'scroll') {
-			scale = zoom
-			containerStyle = {
-				width: setDimensions.w,
-				height: setDimensions.h
-			}
-		} else {
-			scale = scaleAmount * zoom
-			containerStyle = {
-				width: setDimensions.w
-			}
+		scale = scaleAmount * zoom
+		containerStyle = {
+			width: setDimensions.w
 		}
 	}
 
@@ -101,7 +103,7 @@ let getScaleDimensions = (modelState, zoom, isExpanded, scaleAmount, minScale, s
 	}
 }
 
-let getIFrameStyle = scale => {
+const getIFrameStyle = scale => {
 	return {
 		transform: `scale(${scale})`,
 		width: 1 / scale * 100 + '%',
@@ -109,7 +111,7 @@ let getIFrameStyle = scale => {
 	}
 }
 
-let getAfterStyle = (setWidth, setHeight, fit) => {
+const getAfterStyle = (setWidth, setHeight, fit) => {
 	return fit === 'scale'
 		? {
 				paddingTop: setHeight / setWidth * 100 + '%'
@@ -119,11 +121,11 @@ let getAfterStyle = (setWidth, setHeight, fit) => {
 		  }
 }
 
-let getZoomValues = (mediaState, model) => {
-	let userZoom = MediaUtil.getZoom(mediaState, model)
-	let initialZoom = model.modelState.zoom
-	let currentZoom = userZoom || initialZoom
-	let isZoomDifferentFromInitial = currentZoom !== initialZoom
+const getZoomValues = (mediaState, model) => {
+	const userZoom = MediaUtil.getZoom(mediaState, model)
+	const initialZoom = model.modelState.zoom
+	const currentZoom = userZoom || initialZoom
+	const isZoomDifferentFromInitial = currentZoom !== initialZoom
 
 	return {
 		userZoom,
@@ -133,7 +135,7 @@ let getZoomValues = (mediaState, model) => {
 	}
 }
 
-let getRenderSettings = (
+const getRenderSettings = (
 	model,
 	actualWidth,
 	padding,
@@ -143,16 +145,16 @@ let getRenderSettings = (
 	minScale,
 	mediaState
 ) => {
-	let ms = model.modelState
-	let zoomValues = getZoomValues(mediaState, model)
-	let zoom = zoomValues.currentZoom
-	let setDimensions = getSetDimensions(ms, defaultWidth, defaultHeight)
-	let mediaSize = getMediaSize(mediaState, model, defaultSize)
-	let scaleAmount = getScaleAmount(actualWidth, padding, setDimensions.w)
-	let displayedTitle = getDisplayedTitle(ms)
-	let sizeState = getSizeState(ms.expandedSize, scaleAmount, mediaSize)
-	let isExpanded = sizeState === SIZE_STATE_EXPANDED
-	let scaleDimensions = getScaleDimensions(
+	const ms = model.modelState
+	const zoomValues = getZoomValues(mediaState, model)
+	const zoom = zoomValues.currentZoom
+	const setDimensions = getSetDimensions(ms, defaultWidth, defaultHeight)
+	const mediaSize = getMediaSize(mediaState, model, defaultSize)
+	const scaleAmount = getScaleAmount(actualWidth, padding, setDimensions.w)
+	const displayedTitle = getDisplayedTitle(ms)
+	const sizeState = getSizeState(ms.expandedSize, scaleAmount, mediaSize)
+	const isExpanded = sizeState === SIZE_STATE_EXPANDED
+	const scaleDimensions = getScaleDimensions(
 		ms,
 		zoom,
 		isExpanded,
@@ -160,11 +162,11 @@ let getRenderSettings = (
 		minScale,
 		setDimensions
 	)
-	let controlsOpts = getControlsOptions(ms, sizeState)
-	let isAtMinScale = scaleDimensions.scale === minScale
-	let iframeStyle = getIFrameStyle(scaleDimensions.scale)
-	let afterStyle = getAfterStyle(setDimensions.w, setDimensions.h, ms.fit)
-	let isShowing = getIsShowing(mediaState, model)
+	const controlsOpts = getControlsOptions(ms, sizeState)
+	const isAtMinScale = scaleDimensions.scale === minScale
+	const iframeStyle = getIFrameStyle(scaleDimensions.scale)
+	const afterStyle = getAfterStyle(setDimensions.w, setDimensions.h, ms.fit)
+	const isShowing = getIsShowing(mediaState, model)
 
 	return {
 		zoomValues,
