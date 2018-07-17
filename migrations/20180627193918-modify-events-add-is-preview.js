ab291ee12a45df6168c478020440ac1550491c1e
Zachary Berry <zachberry@gmail.com>
2018-07-17 09:32:05 -0400

    wip to fix migrations

--- migrations/20180627193918-modify-events-add-is-preview.js
+++ migrations/20180627193918-modify-events-add-is-preview.js
@@ -1,36 +1,148 @@
-'use strict'
+// 'use strict'
 
-var dbm
-var type
-var seed
+// const canViewEditorRoles = require('../config').permissions.canViewEditor
+// const doRolesIncludeCanViewEditorRole = (roles) => {
+// 	let uniqCanViewEditorRoles = [...new Set(canViewEditorRoles)]
+// 	let uniqRoles = [...new Set(roles)]
+// 	let combinedRoles = new Set(canViewEditorRoles.concat(roles))
 
-/**
- * We receive the dbmigrate dependency from dbmigrate initially.
- * This enables us to not have to rely on NODE_PATH.
- */
-exports.setup = function(options, seedLink) {
-	dbm = options.dbmigrate
-	type = dbm.dataType
-	seed = seedLink
-}
+// 	// If the combined set size does not equal both role sets then there was
+// 	// an overlap of roles, meaning
+// 	return combinedRoles.size !== uniqCanViewEditorRoles.size + uniqRoles.size
+// }
 
-exports.up = function(db) {
-	return db
-		.addColumn('events', 'is_preview', {
-			type: 'boolean',
-			notNull: true
-		})
-		.then(result => {
-			return db.addIndex('events', 'events_is_preview', ['is_preview'])
-		})
-}
+// let dbm
+// let type
+// let seed
 
-exports.down = function(db) {
-	return db.removeIndex('events', 'events_is_preview').then(result => {
-		return db.removeColumn('events', 'is_preview')
-	})
-}
+// /**
+//  * We receive the dbmigrate dependency from dbmigrate initially.
+//  * This enables us to not have to rely on NODE_PATH.
+//  */
+// exports.setup = function(options, seedLink) {
+// 	dbm = options.dbmigrate
+// 	type = dbm.dataType
+// 	seed = seedLink
+// }
 
-exports._meta = {
-	version: 1
-}
+// exports.up = function(db) {
+// 	let launches = {}
+
+// 	return (
+// 		db
+// 			// Add is_preview but don't enforce notNull since there may be data in there already!
+// 			.addColumn('events', 'is_preview', {
+// 				type: 'boolean'
+// 			})
+// 			// Grab all the launches. In pre-amethyst releases preview mode was
+// 			// determined based on if the user had canViewEditor permissions and that
+// 			// was based on their LTI roles. Will attempt to grab the time of the event,
+// 			// reference it with the time of the launch (and user and draft)
+// 			.then(() => {
+// 				return db.runSql(`
+// 					SELECT
+// 						id, created_at, user_id, draft_id, data->>'roles' AS roles
+// 					FROM
+// 						launches
+// 					ORDER BY
+// 						created_at
+// 				`)
+// 			})
+// 			// Build a map of launches
+// 			.then(result => {
+// 				result.rows.forEach(row => {
+// 					let key = row.user_id + ':' + row.draft_id
+// 					if(!launches[key]) launches[key] = []
+
+// 					launches[key].push(row)
+// 				})
+
+// 				return
+// 			})
+// 			// Grab all the events
+// 			.then(() => {
+// 				return db.runSql(`
+// 					SELECT
+// 						id, created_at, actor as user_id, draft_id
+// 					FROM
+// 						events
+// 					ORDER BY
+// 						created_at
+// 				`)
+// 			})
+// 			// Go through all events and figure out the associated launch
+// 			.then(result => {
+// 				result.rows.forEach(event => {
+// 					let key = event.user_id + ':' + event.draft_id
+// 					let launches = launches[key]
+
+// 					if(!launches) {
+// 						console.error('Unable to find launch for event ' + event.id);
+// 						return
+// 					}
+
+// 					let i = 0;
+// 					for(len = launches.length; i < len; i++) {
+// 						let launch = launches[i];
+
+// 						if(launch.created_at > event.created_at) break;
+// 					}
+
+// 					if(i === 0) {
+// 						console.error('Unable to find launch for event ' + event.id)
+// 						return
+// 					}
+
+// 					let matchingLaunch = launches[i - 1];
+// 					let isPreview = doRolesIncludeCanViewEditorRole(matchingLaunch.roles)
+
+// 				})
+// 			})
+
+// 				const updates = result.rows
+// 					.map(row => {
+// 						const id = row.id
+// 						const payload = row.payload
+// 						let isPreview = true
+
+// 						if (payload.extensions && typeof payload.extensions.previewMode !== 'undefined') {
+// 							isPreview = Boolean(payload.extensions.previewMode)
+// 							console.log('isPreview', isPreview)
+// 						}
+
+// 						return `
+// 						UPDATE
+// 							events
+// 						SET
+// 							is_preview=${isPreview}
+// 						WHERE
+// 							id='${id}'
+// 					`
+// 					})
+// 					.join(';')
+
+// 				return db.runSql(updates)
+// 			})
+// 			// Require notNull after content has been filled out
+// 			.then(() => {
+// 				return db.changeColumn('events', 'is_preview', {
+// 					type: 'boolean',
+// 					notNull: true
+// 				})
+// 			})
+// 			// Finally, add the index
+// 			.then(() => {
+// 				return db.addIndex('events', 'events_is_preview', ['is_preview'])
+// 			})
+// 	)
+// }
+
+// exports.down = function(db) {
+// 	return db.removeIndex('events', 'events_is_preview').then(() => {
+// 		return db.removeColumn('events', 'is_preview')
+// 	})
+// }
+
+// exports._meta = {
+// 	version: 1
+// }
--- migrations/20180627200648-modify-caliper-store-add-is-preview.js
+++ migrations/20180627200648-modify-caliper-store-add-is-preview.js
@@ -1,8 +1,8 @@
 'use strict'
 
-var dbm
-var type
-var seed
+let dbm
+let type
+let seed
 
 /**
  * We receive the dbmigrate dependency from dbmigrate initially.
@@ -15,18 +15,62 @@ exports.setup = function(options, seedLink) {
 }
 
 exports.up = function(db) {
-	return db
-		.addColumn('caliper_store', 'is_preview', {
-			type: 'boolean',
-			notNull: true
-		})
-		.then(result => {
-			return db.addIndex('caliper_store', 'caliper_store_is_preview', ['is_preview'])
-		})
+	return (
+		db
+			// Add is_preview but don't enforce notNull since there may be data in there already!
+			.addColumn('caliper_store', 'is_preview', {
+				type: 'boolean'
+			})
+			// Grab all the current records, which should have a 'previewMode' property
+			// in extensions
+			.then(() => {
+				return db.runSql(`
+					SELECT
+						*
+					FROM caliper_store
+				`)
+			})
+			.then(result => {
+				const updates = result.rows
+					.map(row => {
+						const id = row.id
+						const payload = row.payload
+						let isPreview = true
+
+						if (payload.extensions && typeof payload.extensions.previewMode !== 'undefined') {
+							isPreview = Boolean(payload.extensions.previewMode)
+							console.log('isPreview', isPreview)
+						}
+
+						return `
+						UPDATE
+							caliper_store
+						SET
+							is_preview=${isPreview}
+						WHERE
+							id='${id}'
+					`
+					})
+					.join(';')
+
+				return db.runSql(updates)
+			})
+			// Require notNull after content has been filled out
+			.then(() => {
+				return db.changeColumn('caliper_store', 'is_preview', {
+					type: 'boolean',
+					notNull: true
+				})
+			})
+			// Finally, add the index
+			.then(() => {
+				return db.addIndex('caliper_store', 'caliper_store_is_preview', ['is_preview'])
+			})
+	)
 }
 
 exports.down = function(db) {
-	return db.removeIndex('caliper_store', 'caliper_store_is_preview').then(result => {
+	return db.removeIndex('caliper_store', 'caliper_store_is_preview').then(() => {
 		return db.removeColumn('caliper_store', 'is_preview')
 	})
 }
