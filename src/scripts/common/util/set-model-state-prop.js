301f52b4dfefcd11877d3786b865a00ef2db5409
iturgeon <iturgeon@gmail.com>
2018-07-31 11:14:24 -0400

    adds docs to set-model-state-prop

--- src/scripts/common/util/set-model-state-prop.js
+++ src/scripts/common/util/set-model-state-prop.js
@@ -1,3 +1,30 @@
+/*
+Filters the values used in OBONode models
+
+pulls data from `attrs.content` in the document and sets them in `model.modelState` based on a set of rules
+
+//// Usage Example:
+
+// import util method
+const setModelStateProp = Common.util.setModelStateProp
+
+// bind the OboNode adapter to the method for convenience
+const setProp = setModelStateProp.bind(this, model, attrs)
+
+// setProp(propName, defaultValue, getPropValueFn, allowedValues)
+setProp('type', 'media', p => (p.toLowerCase() === 'webpage' ? 'webpage' : 'media'), ['webpage', 'media'])
+
+//// Values
+
+propName is the value of `type` from `attrs.content.type`. note `.type` is the value `propName`
+defaultValue is used if:
+ 1. `attrs.content.type` is null or undefined
+ 2. getPropValueFn returns null
+ 3. getPropValueFn returns a value that isnt in allowedValues (if set)
+
+getPropValueFN is used to alter the incoming value of `attrs.content.type` before it's tested agains allowed values. `p` set to the value of `attrs.content.type`
+
+*/
 export default (
 	model,
 	attrs,
