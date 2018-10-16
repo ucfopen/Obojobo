e8eb5aed4b69b35ce47714f51b7b4c7a0dd3860d
Samuel Belcastro <sa119937@cdl215469.local>
2018-10-16 16:06:09 -0400

    Added endpoint to get media by media id and media dimensions

--- packages/obojobo-express/routes/api/media.js
+++ packages/obojobo-express/routes/api/media.js
@@ -22,14 +22,13 @@ const {
 } = oboRequire('express_validators')
 
 // Upload media file
-// mounted as /api/media/new
+// mounted as /api/media/upload
 router
     .route('/upload')
     .post([requireCurrentUser])
     .post((req, res, next) => {
         upload(req, res, err => {
-            if(err){
-                
+            if(err){     
                 next(new Error("An unexpected error occurred while attempting to upload the image."));
             } else {
                 MediaModel
@@ -40,6 +39,21 @@ router
                     .catch(next)
             }
         })
+    })
+
+// Get media file
+// mounted as /api/media/:mediaId/:dimensions
+router
+    .route('/:mediaId/:dimensions')
+    .get([requireCurrentUser])
+    .get((req, res, next) => {
+        MediaModel
+            .fetchByIdAndDimensions(req.params.mediaId, req.params.dimensions)
+            .then(imageData => {
+                console.log(imageData);
+                res.send("Success!");
+            })
+            .catch(next)
     })
 
 module.exports = router
