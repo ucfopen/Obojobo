ce38a0395d1a7c4343460671acefc2e8f6fb3207
Elli Howard <elise.howard@ucf.edu>
2018-10-22 14:14:47 -0400

    Removed invalid selection restore from clipboard commands

--- packages/obojobo-document-engine/src/scripts/oboeditor/components/editor-nav.js
+++ packages/obojobo-document-engine/src/scripts/oboeditor/components/editor-nav.js
@@ -68,15 +68,9 @@ class EditorNav extends React.Component {
 		el.style.position = 'absolute'
 		el.style.left = '-9999px'
 		document.body.appendChild(el)
-		const selected =
-			document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false
 		el.select()
 		document.execCommand('copy')
 		document.body.removeChild(el)
-		if (selected) {
-			document.getSelection().removeAllRanges()
-			document.getSelection().addRange(selected)
-		}
 		window.alert('Copied ' + str + ' to the clipboard')
 	}
 
