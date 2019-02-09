60664d2a7e2d7d543d95c4bc1019c3f2938555ab
Zachary Berry <zachberry@gmail.com>
2019-02-09 14:15:23 -0500

    continued code node indent simplification
    Co-Authored-By: iturgeon <iturgeon@gmail.com>

--- packages/obojobo-document-engine/ObojoboDraft/Chunks/Code/changes/increase-indent.js
+++ packages/obojobo-document-engine/ObojoboDraft/Chunks/Code/changes/increase-indent.js
@@ -2,7 +2,6 @@ const increaseIndent = (event, change) => {
 	event.preventDefault()
 	change.value.blocks.forEach(block => {
 		const newIndent = Math.min(block.data.get('content').indent + 1, 20)
-		if (newIndent > 20) newIndent = 20
 
 		change.setNodeByKey(block.key, {
 			data: { content: { indent: newIndent } }
