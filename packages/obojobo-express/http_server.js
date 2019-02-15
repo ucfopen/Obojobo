b9423c530c7736622bcc6e0af6b67f322cf52320
Ian Turgeon <iturgeon@gmail.com>
2019-02-15 17:55:05 -0500

    adds note about environment vars on server startup

--- packages/obojobo-express/http_server.js
+++ packages/obojobo-express/http_server.js
@@ -1,67 +1,72 @@
-var http = require('http');
+var http = require('http')
 
-const normalizePort = (val) => {
-  var port = parseInt(val, 10);
+/**
+ * This code is derived from bin/www used in Express 4
+ * We extracted the code from that file to make it 1: testable
+ * and 2: easier to manipulate for customizing Obojobo.
+ * Now you can build your own express app, add middleware, and
+ * pass it to startServer to start your app
+ */
 
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
+const normalizePort = val => {
+	var port = parseInt(val, 10)
 
-  if (port >= 0) {
-    // port number
-    return port;
-  }
+	if (isNaN(port)) {
+		// named pipe
+		return val
+	}
 
-  return false;
+	if (port >= 0) {
+		// port number
+		return port
+	}
+
+	return false
 }
-
 
 const startServer = (app, logger, port = '3000') => {
+	console.log('Note: Logging and config options can be set using environment variables.')
 
-  port = normalizePort(port)
-  app.set('port', port);
-  const server = http.createServer(app);
+	port = normalizePort(port)
+	app.set('port', port)
+	const server = http.createServer(app)
 
-  server.on('error', (error) => {
-    if (error.syscall !== 'listen') {
-      throw error;
-    }
+	server.on('error', error => {
+		if (error.syscall !== 'listen') {
+			throw error
+		}
 
-    var bind = typeof port === 'string'
-      ? 'Pipe ' + port
-      : 'Port ' + port;
+		var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port
 
-    // handle specific listen errors with friendly messages
-    switch (error.code) {
-      case 'EACCES':
-        logger.error(bind + ' requires elevated privileges');
-        process.exit(1);
-        break;
+		// handle specific listen errors with friendly messages
+		switch (error.code) {
+			case 'EACCES':
+				logger.error(bind + ' requires elevated privileges')
+				process.exit(1)
+				break
 
-      case 'EADDRINUSE':
-        logger.error(bind + ' is already in use');
-        process.exit(1);
-        break;
+			case 'EADDRINUSE':
+				logger.error(bind + ' is already in use')
+				process.exit(1)
+				break
 
-      default:
-        throw error;
-    }
-  })
+			default:
+				throw error
+		}
+	})
 
-  server.on('listening', () => {
-    let addr = server.address()
-    const bind = (typeof addr === 'string') ? `pipe ${addr}` : `port ${addr.port}`
-    logger.info('Listening on ' + bind)
-  })
+	server.on('listening', () => {
+		let addr = server.address()
+		const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`
+		logger.info('Listening on ' + bind)
+	})
 
-  /**
-   * Listen on provided port, on all network interfaces.
-   */
-  server.listen(port);
+	/**
+	 * Listen on provided port, on all network interfaces.
+	 */
+	server.listen(port)
 
-  return server
+	return server
 }
-
 
 module.exports = startServer
