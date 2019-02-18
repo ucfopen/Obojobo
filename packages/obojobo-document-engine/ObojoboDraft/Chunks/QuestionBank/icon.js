623257af9a7f660349496876178f2c3ec5ddde13
Zachary Berry <zachberry@gmail.com>
2019-02-18 14:18:21 -0500

    Updates icons

--- packages/obojobo-document-engine/ObojoboDraft/Chunks/QuestionBank/icon.js
+++ packages/obojobo-document-engine/ObojoboDraft/Chunks/QuestionBank/icon.js
@@ -2,24 +2,9 @@ import React from 'react'
 
 const Icon = () => (
 	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
-		<rect width="32" height="32" opacity="0" />
-		<path
-			d="M6.3,27.06A1.09,1.09,0,0,1,5.21,26v-18H3.3A1.09,1.09,0,0,0,2.21,9V29A1.09,1.09,0,0,0,3.3,30.06H22.7A1.09,1.09,0,0,0,23.79,29V27.06Z"
-			fill="none"
-			className="stroke"
-			strokeLinecap="round"
-			strokeMiterlimit="10"
-		/>
-		<path
-			d="M9.3,24.06A1.09,1.09,0,0,1,8.21,23v-18H6.3A1.09,1.09,0,0,0,5.21,6V26A1.09,1.09,0,0,0,6.3,27.06H25.7A1.09,1.09,0,0,0,26.79,26V24.06Z"
-			fill="none"
-			className="stroke"
-			strokeLinecap="round"
-			strokeMiterlimit="10"
-		/>
 		<rect
-			x="8.23"
-			y="2.48"
+			x="7.94"
+			y="2.21"
 			width="22.12"
 			height="21.58"
 			rx="1.09"
@@ -29,54 +14,23 @@ const Icon = () => (
 			strokeLinecap="round"
 			strokeMiterlimit="10"
 		/>
-		<line
-			x1="13.99"
-			y1="21.58"
-			x2="17.89"
-			y2="17.68"
-			className="stroke"
-			strokeLinecap="round"
-			strokeMiterlimit="10"
+		<path
+			d="M14,9c.82-2,2.55-3.11,5.09-3.11C22.06,5.86,24,7.4,24,9.12c0,2.84-3.63,3-3.63,5.46a.47.47,0,0,1-.48.45h-2c-.31,0-.5-.19-.5-.55,0-3.15,2.89-3.8,2.89-5.15a1.11,1.11,0,0,0-1.23-1,2.14,2.14,0,0,0-1.94,1.46c-.11.25-.21.28-.49.23l-2.44-.53C14,9.43,14,9.18,14,9Zm6.86,10.62c0,.4-.19.55-.63.55H17.39c-.38,0-.5-.15-.5-.47V16.82c0-.29.14-.42.36-.42h3.29c.21,0,.34.13.34.38Z"
+			className="fill"
 		/>
-		<line
-			x1="12.05"
-			y1="19.63"
-			x2="13.99"
-			y2="21.58"
-			className="stroke"
-			strokeLinecap="round"
-			strokeMiterlimit="10"
-		/>
-		<line
-			x1="22.19"
-			y1="21.58"
-			x2="26.09"
-			y2="17.68"
-			className="stroke"
-			strokeLinecap="round"
-			strokeMiterlimit="10"
-		/>
-		<line
-			x1="22.19"
-			y1="17.68"
-			x2="26.09"
-			y2="21.58"
-			className="stroke"
-			strokeLinecap="round"
-			strokeMiterlimit="10"
-		/>
-		<line
-			x1="8.23"
-			y1="15.06"
-			x2="30.34"
-			y2="15.06"
+		<path
+			d="M6.3,27.06A1.09,1.09,0,0,1,5.21,26v-18H3.3A1.09,1.09,0,0,0,2.21,9V29A1.09,1.09,0,0,0,3.3,30.06H22.7A1.09,1.09,0,0,0,23.79,29V27.06Z"
+			fill="none"
 			className="stroke"
 			strokeLinecap="round"
 			strokeMiterlimit="10"
 		/>
 		<path
-			d="M16.62,5.88a2.82,2.82,0,0,1,2.73-1.6c1.58,0,2.62.85,2.62,1.93C22,8,19.77,8.28,19.77,10a.18.18,0,0,1-.19.18H18.8c-.12,0-.2-.07-.2-.22,0-2,1.94-2.44,1.94-3.65a1.06,1.06,0,0,0-1.2-1,1.68,1.68,0,0,0-1.51,1c0,.09-.09.11-.2.09l-.93-.25C16.6,6.06,16.59,6,16.62,5.88Zm3.35,7c0,.17-.07.23-.26.23H18.59c-.16,0-.21-.06-.21-.2V11.69c0-.12.05-.17.15-.17h1.3c.09,0,.14,0,.14.15Z"
-			className="fill"
+			d="M9,23.79A1.09,1.09,0,0,1,7.94,22.7V4.94H6.3A1.09,1.09,0,0,0,5.21,6V26A1.09,1.09,0,0,0,6.3,27.06H25.7A1.09,1.09,0,0,0,26.79,26V23.79Z"
+			fill="none"
+			className="stroke"
+			strokeLinecap="round"
+			strokeMiterlimit="10"
 		/>
 	</svg>
 )
