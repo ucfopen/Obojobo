5f7e799c369b7c8c02ce56c1659c87bbd07b3341
Ian Turgeon <iturgeon@gmail.com>
2019-02-27 12:35:58 -0500

    adds tests for page

--- packages/obojobo-pages-page/viewer-component.test.js
+++ packages/obojobo-pages-page/viewer-component.test.js
@@ -4,10 +4,12 @@ import renderer from 'react-test-renderer'
 import { shallow } from 'enzyme'
 
 jest.mock('obojobo-document-engine/src/scripts/viewer/util/nav-util')
+jest.mock('obojobo-document-engine/src/scripts/common/page/focus')
 
 import Page from './viewer-component'
 import OboModel from 'obojobo-document-engine/src/scripts/common/models/obo-model'
 import NavUtil from 'obojobo-document-engine/src/scripts/viewer/util/nav-util'
+import focus from 'obojobo-document-engine/src/scripts/common/page/focus'
 
 require('./viewer') // used to register this oboModel
 require('obojobo-chunks-break/viewer') // // dependency on Obojobo.Chunks.Break
@@ -37,5 +39,43 @@ describe('Page', () => {
 		const tree = component.toJSON()
 
 		expect(tree).toMatchSnapshot()
+	})
+
+	test('Page focusOnContent calls focus with the first childs DOM element', () => {
+		const model = OboModel.create({
+			id: 'id',
+			type: 'ObojoboDraft.Pages.Page',
+			children: [
+				{
+					id: 'child',
+					type: 'ObojoboDraft.Chunks.Break'
+				}
+			]
+		})
+		const moduleData = {
+			focusState: {}
+		}
+		const component = shallow(<Page model={model} moduleData={moduleData} />)
+
+		// mock the first child's method for getDomEl
+		model.children.at(0).getDomEl = jest.fn().mockReturnValue('mock-dom-element')
+
+		expect(Page.focusOnContent(model)).toBe(undefined) // there's no return
+		expect(focus).toHaveBeenCalledWith('mock-dom-element') // but it should have called focus with the return of our mocked getDomEl
+	})
+
+	test('Page focusOnContent handles no children', () => {
+		const model = OboModel.create({
+			id: 'id',
+			type: 'ObojoboDraft.Pages.Page',
+			children: []
+		})
+		const moduleData = {
+			focusState: {}
+		}
+		const component = shallow(<Page model={model} moduleData={moduleData} />)
+
+		expect(Page.focusOnContent(model)).toBe(undefined) // there's no return
+		expect(focus).not.toHaveBeenCalled()
 	})
 })
