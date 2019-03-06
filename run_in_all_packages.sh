d790c617f4eeb8d3c9138c0e2b66eb251205eddd
Ian Turgeon <iturgeon@gmail.com>
2019-03-05 19:50:23 -0500

    fix run_in_all to return failure status

--- run_in_all_packages.sh
+++ run_in_all_packages.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 HOME_PATH=$PWD
-for d in ./packages/*/*
-do
-	echo "=================="
+RETURN_CODE=0
+
+for d in ./packages/*/*; do
+	echo ""
+	echo "===================================="
 	basename "$d"
-	echo "=================="
+	echo "===================================="
 	cd "$HOME_PATH"
 	cd "$d"
-	$@
+	eval "$@";
+
+	# store the command's return status
+	RET=$?
+	if [ $RET != 0 ]; then
+		RETURN_CODE=RET
+	fi
 done
+
+exit $RETURN_CODE
