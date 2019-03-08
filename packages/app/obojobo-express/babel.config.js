afc0047efd195114078982567e9c37eca4534d3f
Ian Turgeon <iturgeon@gmail.com>
2019-03-08 10:43:00 -0500

    fixes to webpack build after split upheaval

--- packages/app/obojobo-express/babel.config.js
+++ packages/app/obojobo-express/babel.config.js
@@ -2,7 +2,6 @@ module.exports = function(api) {
 	api.cache(true)
 	return {
 		presets: ['@babel/preset-env', '@babel/preset-react'],
-		plugins: [['@babel/transform-runtime']],
 		env: {
 			test: {
 				compact: false,
