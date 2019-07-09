36d4f4a1ab3a10a7d4914d00506a8b0a5f0fcb00
Toan Vu <vutoan1245@gmail.com>
2019-07-09 10:43:23 -0400

    Add bin directive to obojobo-document-json-parser

--- packages/app/obojobo-document-json-parser/bin/json2xml.js
+++ packages/app/obojobo-document-json-parser/bin/json2xml.js
@@ -1,11 +1,7 @@
-const argv = process.argv.slice(2)
+#!/usr/bin/env node
 
 const jsonToXml = require('../json-to-xml-parser')
 
-if (argv.length === 0) {
-    console.error('Usage: json2xml.js json-file.json');
-    return process.exit(1);
-}
+const json = require('../examples/test-object.json')
 
-const json = require('../' + argv[0])
 console.log(jsonToXml(json))
