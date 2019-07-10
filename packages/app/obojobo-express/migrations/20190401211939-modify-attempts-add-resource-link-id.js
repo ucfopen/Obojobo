109f74e100f2e254cfdb994c5db52820494ab22c
Ian Turgeon <iturgeon@gmail.com>
2019-07-10 17:48:06 -0400

    add backfilling queries to new resource_link_id migrations

--- packages/app/obojobo-express/migrations/20190401211939-modify-attempts-add-resource-link-id.js
+++ packages/app/obojobo-express/migrations/20190401211939-modify-attempts-add-resource-link-id.js
@@ -20,6 +20,62 @@ exports.up = function(db) {
 			type: 'varchar'
 		})
 		.then(() => db.addIndex('attempts', 'attempts_resource_link_id', ['resource_link_id']))
+		.then(() => {
+			// backfill resource_link_id
+			return db.runSql(`
+				-- make a table with with start and end time windows for each launch
+					CREATE TEMP TABLE tmp_launch_windows AS
+						SELECT * FROM (
+							select
+								id as launch_id,
+								draft_id,
+								draft_content_id,
+								user_id,
+								data -> 'resource_link_id' as rlid,
+								created_at as start_date,
+								CASE
+									WHEN lead(user_id) over wnd = user_id
+										AND lead(data -> 'resource_link_id') over wnd = data -> 'resource_link_id'
+									THEN lead(created_at) over wnd
+									ELSE now()
+								END as end_date,
+								CASE
+									WHEN lead(user_id) over wnd = user_id
+										AND lead(data -> 'resource_link_id') over wnd = data -> 'resource_link_id'
+									THEN lead(id) over wnd
+									ELSE null
+								END as next_launch_id
+								FROM launches
+								WHERE data -> 'resource_link_id' IS NOT null
+								WINDOW wnd as (
+									PARTITION BY data -> 'resource_link_id', user_id ORDER BY data -> 'resource_link_id', user_id, created_at
+								)
+						) as foo;
+
+				-- use the temp table to copy data
+				UPDATE
+					attempts
+				SET
+					resource_link_id = approximate_rlids.rlid
+				FROM
+				(
+					SELECT
+						attempts.id as attempt_id,
+						rlid
+					FROM attempts
+					JOIN tmp_launch_windows
+						ON attempts.draft_content_id = tmp_launch_windows.draft_content_id
+						AND attempts.user_id = tmp_launch_windows.user_id
+					WHERE
+						attempts.created_at BETWEEN tmp_launch_windows.start_date AND tmp_launch_windows.end_date
+				) AS approximate_rlids
+				WHERE
+					attempts.id = approximate_rlids.attempt_id;
+
+				-- remove the tmp table
+				Drop table tmp_launch_windows;
+			`)
+		})
 }
 
 exports.down = function(db) {
--- packages/app/obojobo-express/migrations/20190401212325-modify-view-state-add-resource-link-id.js
+++ packages/app/obojobo-express/migrations/20190401212325-modify-view-state-add-resource-link-id.js
@@ -30,6 +30,62 @@ exports.up = function(db) {
 				true
 			)
 		)
+		.then(() => {
+			// backfill resource_link_id
+			return db.runSql(`
+				-- make a table with with start and end time windows for each launch
+					CREATE TEMP TABLE tmp_launch_windows AS
+						SELECT * FROM (
+							select
+								id as launch_id,
+								draft_id,
+								draft_content_id,
+								user_id,
+								data -> 'resource_link_id' as rlid,
+								created_at as start_date,
+								CASE
+									WHEN lead(user_id) over wnd = user_id
+										AND lead(data -> 'resource_link_id') over wnd = data -> 'resource_link_id'
+									THEN lead(created_at) over wnd
+									ELSE now()
+								END as end_date,
+								CASE
+									WHEN lead(user_id) over wnd = user_id
+										AND lead(data -> 'resource_link_id') over wnd = data -> 'resource_link_id'
+									THEN lead(id) over wnd
+									ELSE null
+								END as next_launch_id
+								FROM launches
+								WHERE data -> 'resource_link_id' IS NOT null
+								WINDOW wnd as (
+									PARTITION BY data -> 'resource_link_id', user_id ORDER BY data -> 'resource_link_id', user_id, created_at
+								)
+						) as foo;
+
+				-- use the temp table to copy data
+				UPDATE
+					view_state
+				SET
+					resource_link_id = approximate_rlids.rlid
+				FROM
+				(
+					SELECT
+						view_state.id as view_state_id,
+						rlid
+					FROM view_state
+					JOIN tmp_launch_windows
+						ON view_state.draft_content_id = tmp_launch_windows.draft_content_id
+						AND view_state.user_id = tmp_launch_windows.user_id
+					WHERE
+						view_state.created_at BETWEEN tmp_launch_windows.start_date AND tmp_launch_windows.end_date
+				) AS approximate_rlids
+				WHERE
+					view_state.id = approximate_rlids.view_state_id;
+
+				-- remove the tmp table
+				Drop table tmp_launch_windows;
+			`)
+		})
 }
 
 exports.down = function(db) {
