962394197ffb0ba26d62ca2b0bb2970f4a1a47e2
Ian Turgeon <iturgeon@gmail.com>
2019-07-17 12:29:14 -0400

    minor polish to json parster tests

--- packages/app/obojobo-document-json-parser/__tests__/json-to-xml.test.js
+++ packages/app/obojobo-document-json-parser/__tests__/json-to-xml.test.js
@@ -1,15 +1,15 @@
 const fs = require('fs')
 const jsonToXmlParser = require('../json-to-xml-parser')
 
-const filterFileType = arr =>
-	arr.filter(s => s.includes('.json')).map(s => s.substring(0, s.length - 4))
+describe('JSON to XML Parser', () => {
+	expect.hasAssertions()
 
-const nodes = filterFileType(fs.readdirSync(`${__dirname}/obo_node_json/`))
+	const basePath = `${__dirname}/sample-node-json/`
+	const sampleJson = fs.readdirSync(basePath).filter(s => s.includes('.json'))
 
-describe('JSON to XML Test', () => {
-	for (let node of nodes) {
-		it(`Converts ${node}json to ${node}xml`, () => {
-			const json = require(`${__dirname}/obo_node_json/${node}json`)
+	for (const file of sampleJson) {
+		it(`Converts ${file} to xml`, () => {
+			const json = require(`${basePath}${file}`)
 			expect(jsonToXmlParser(json)).toMatchSnapshot()
 		})
 	}
--- packages/app/obojobo-document-json-parser/src/module-node-parser/module-node-parser.js
+++ packages/app/obojobo-document-json-parser/src/module-node-parser/module-node-parser.js
@@ -4,14 +4,6 @@ const processAttrs = require('../process-attrs')
 const moduleNodeParser = (node, childrenParser) => {
     const id = node.id ? ` id="${node.id}"` : ''
 
-    // let attrs = '';
-    // for (const attr in node.content) {
-    //     attrs += (
-    //         node.content[attr] != null ?
-    //         ` ${[attr]}="${xmlEncode(node.content[attr])}"` :
-    //         ''
-    //     )
-    // }
     const attrs = processAttrs(node.content, [])
 
     return (
