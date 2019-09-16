7486be40e71fb53b698b97a6e52253348448d2a8
Toan Vu <vutoan1245@gmail.com>
2019-09-16 09:26:47 -0400

    use constant instead of string for numeric assessment

--- packages/obonode/obojobo-chunks-numeric-assessment/components/numeric-input.js
+++ packages/obonode/obojobo-chunks-numeric-assessment/components/numeric-input.js
@@ -5,46 +5,46 @@ import Common from 'obojobo-document-engine/src/scripts/common'
 
 const { Button } = Common.components
 
-const requirementDropdown = [
-	'Exact answer',
-	'Margin of error',
-	'Within a range',
-	'Precise response'
-]
-const marginDropdown = ['Percent', 'Absolute']
-const precisionDropdown = ['Significant digits', 'Decimal places']
+const EXACT_ANSWER = 'Exact answer'
+const MARGIN_OF_ERROR = 'Margin of error'
+const WITHIN_A_RANGE = 'Within a range'
+const PRECISE_RESPONSE = 'Precise response'
+const PERCENT = 'Percent'
+const ABSOLUTE = 'Absolute'
+const SIGNIFICANT_DIGITS = 'Significant digits'
+const DECIMAL_PLACES = 'Decimal places'
+
+const requirementDropdown = [EXACT_ANSWER, MARGIN_OF_ERROR, WITHIN_A_RANGE, PRECISE_RESPONSE]
+const marginDropdown = [PERCENT, ABSOLUTE]
+const precisionDropdown = [SIGNIFICANT_DIGITS, DECIMAL_PLACES]
 
 class NumericInput extends React.Component {
 	renderHeader(requirement) {
 		switch (requirement) {
-			case 'Exact answer':
+			case EXACT_ANSWER:
 				return (
 					<>
-						<th>Requirement</th>
 						<th>Answer</th>
 					</>
 				)
-			case 'Margin of error':
+			case MARGIN_OF_ERROR:
 				return (
 					<>
-						<th>Requirement</th>
 						<th>Answer</th>
 						<th>Margin</th>
 						<th>Type</th>
 					</>
 				)
-			case 'Within a range':
+			case WITHIN_A_RANGE:
 				return (
 					<>
-						<th>Requirement</th>
 						<th>Start</th>
 						<th>End</th>
 					</>
 				)
-			case 'Precise response':
+			case PRECISE_RESPONSE:
 				return (
 					<>
-						<th>Requirement</th>
 						<th>Answer</th>
 						<th>Precision</th>
 						<th>Type</th>
@@ -66,123 +66,106 @@ class NumericInput extends React.Component {
 			precisionType
 		} = response
 
-		return (
-			<>
-				<td>
-					<select
-						className="select-item"
-						name="requirement"
-						onChange={event => this.props.onClickDropdown(event)}
-					>
-						{requirementDropdown.map(requirement => (
-							<option>{requirement}</option>
-						))}
-					</select>
-				</td>
-				{(() => {
-					switch (requirement) {
-						case 'Exact answer':
-							return (
-								<td>
-									<input
-										className="input-item"
-										name="answerInput"
-										value={answerInput}
-										onChange={event => props.onInputChange(event)}
-									/>
-								</td>
-							)
-						case 'Precise response':
-							return (
-								<>
-									<td>
-										<input
-											className="input-item"
-											name="answerInput"
-											value={answerInput}
-											onChange={event => props.onInputChange(event)}
-										/>
-									</td>
-									<td>
-										<input
-											className="input-item"
-											name="precisionInput"
-											value={precisionInput}
-											onChange={() => props.onInputChange(event)}
-										/>
-									</td>
-									<td>
-										<select
-											className="select-item"
-											name="precisionType"
-											value={precisionType}
-											onChange={event => this.props.onClickDropdown(event)}
-										>
-											{precisionDropdown.map(requirement => (
-												<option>{requirement}</option>
-											))}
-										</select>
-									</td>
-								</>
-							)
-						case 'Within a range':
-							return (
-								<>
-									<td>
-										<input
-											className="input-item"
-											name="startInput"
-											value={startInput}
-											onChange={() => props.onInputChange(event)}
-										/>
-									</td>
-									<td>
-										<input
-											className="input-item"
-											name="endInput"
-											value={endInput}
-											onChange={() => props.onInputChange(event)}
-										/>
-									</td>
-								</>
-							)
-						case 'Margin of error':
-							return (
-								<>
-									<td>
-										<input
-											className="input-item"
-											name="answerInput"
-											value={answerInput}
-											onChange={() => props.onInputChange(event)}
-										/>
-									</td>
-									<td>
-										<input
-											className="input-item"
-											name="marginInput"
-											value={marginInput}
-											onChange={() => props.onInputChange(event)}
-										/>
-									</td>
-									<td>
-										<select
-											className="select-item"
-											name="marginType"
-											value={marginType}
-											onChange={event => this.props.onClickDropdown(event)}
-										>
-											{marginDropdown.map(requirement => (
-												<option>{requirement}</option>
-											))}
-										</select>
-									</td>
-								</>
-							)
-					}
-				})()}
-			</>
-		)
+		switch (requirement) {
+			case EXACT_ANSWER:
+				return (
+					<td>
+						<input
+							className="input-item"
+							name="answerInput"
+							value={answerInput}
+							onChange={event => props.onInputChange(event)}
+						/>
+					</td>
+				)
+			case PRECISE_RESPONSE:
+				return (
+					<>
+						<td>
+							<input
+								className="input-item"
+								name="answerInput"
+								value={answerInput}
+								onChange={event => props.onInputChange(event)}
+							/>
+						</td>
+						<td>
+							<input
+								className="input-item"
+								name="precisionInput"
+								value={precisionInput}
+								onChange={() => props.onInputChange(event)}
+							/>
+						</td>
+						<td>
+							<select
+								className="select-item"
+								name="precisionType"
+								value={precisionType}
+								onChange={event => props.onClickDropdown(event)}
+							>
+								{precisionDropdown.map(requirement => (
+									<option>{requirement}</option>
+								))}
+							</select>
+						</td>
+					</>
+				)
+			case WITHIN_A_RANGE:
+				return (
+					<>
+						<td>
+							<input
+								className="input-item"
+								name="startInput"
+								value={startInput}
+								onChange={() => props.onInputChange(event)}
+							/>
+						</td>
+						<td>
+							<input
+								className="input-item"
+								name="endInput"
+								value={endInput}
+								onChange={() => props.onInputChange(event)}
+							/>
+						</td>
+					</>
+				)
+			case MARGIN_OF_ERROR:
+				return (
+					<>
+						<td>
+							<input
+								className="input-item"
+								name="answerInput"
+								value={answerInput}
+								onChange={() => props.onInputChange(event)}
+							/>
+						</td>
+						<td>
+							<input
+								className="input-item"
+								name="marginInput"
+								value={marginInput}
+								onChange={() => props.onInputChange(event)}
+							/>
+						</td>
+						<td>
+							<select
+								className="select-item"
+								name="marginType"
+								value={marginType}
+								onChange={event => props.onClickDropdown(event)}
+							>
+								{marginDropdown.map(requirement => (
+									<option>{requirement}</option>
+								))}
+							</select>
+						</td>
+					</>
+				)
+		}
 	}
 
 	render() {
@@ -192,8 +175,24 @@ class NumericInput extends React.Component {
 					×
 				</Button>
 				<table>
-					<tr>{this.renderHeader(this.props.response.requirement)}</tr>
-					<tr>{this.renderOption(this.props)}</tr>
+					<tr>
+						<th>Requirement</th>
+						{this.renderHeader(this.props.response.requirement)}
+					</tr>
+					<tr>
+						<td>
+							<select
+								className="select-item"
+								name="requirement"
+								onChange={event => this.props.onClickDropdown(event)}
+							>
+								{requirementDropdown.map(requirement => (
+									<option>{requirement}</option>
+								))}
+							</select>
+						</td>
+						{this.renderOption(this.props)}
+					</tr>
 				</table>
 			</div>
 		)
