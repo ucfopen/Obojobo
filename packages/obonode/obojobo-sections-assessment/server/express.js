ec333458307f12f4bf3bb01734d6ade64408be8d
Ian Turgeon <iturgeon@gmail.com>
2019-11-06 17:09:35 -0500

    WIP: add import columns to attempt tables and start dealing with duplication query

--- packages/obonode/obojobo-sections-assessment/server/express.js
+++ packages/obonode/obojobo-sections-assessment/server/express.js
@@ -17,6 +17,22 @@ const {
 	requireAssessmentId
 } = require('obojobo-express/express_validators')
 
+const sqlToSelectColumnNames = (table, exceptColumns) => {
+	const sqlExceptColumns = exceptColumns ?
+		`AND c.column_name NOT IN(${exceptColumns.map(n => `'${n}'`)})`
+		: ''
+
+	return `SELECT array_to_string(
+		ARRAY(
+			SELECT c.column_name::VARCHAR
+			FROM information_schema.columns As c
+			WHERE table_name = '${table}'
+			${sqlExceptColumns}
+		),
+		', '
+	) AS columns`
+}
+
 router
 	.route('/api/lti/state/draft/:draftId')
 	.get([requireCurrentDocument, requireCurrentVisit, requireCurrentUser])
