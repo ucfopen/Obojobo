66296162ce335beb000c64735fbca13569a3661d
Ian Turgeon <iturgeon@gmail.com>
2019-12-10 20:00:09 -0500

    Refactor repository routes and add perm checking
    Moves queries out of express routes into services
    Adds user validation and Permission checking to sharing
    Refactors some of the search and permission code

--- packages/app/obojobo-repository/server/routes/api.js
+++ packages/app/obojobo-repository/server/routes/api.js
@@ -7,25 +7,17 @@ const {
 	requireCurrentDocument
 } = require('obojobo-express/express_validators')
 const UserModel = require('obojobo-express/models/user')
-const db = require('obojobo-express/db')
-
-const resultsToUsers = results => {
-	return results.map(u => {
-		return new UserModel({
-			id: u.id,
-			firstName: u.first_name,
-			lastName: u.last_name,
-			email: u.email,
-			username: u.username,
-			createdAt: u.created_at,
-			roles: u.roles
-		})
-	})
-}
+const { searchForUserByString } = require('../services/search')
+const {
+	addUserPermissionToDraft,
+	userHasPermissionToDraft,
+	fetchAllUsersWithPermissionToDraft,
+	removeUserPermissionToDraft
+} = require('../services/permissions')
+const publicLibCollectionId = '00000000-0000-0000-0000-000000000000'
 
 // List public drafts
 router.route('/drafts-public').get((req, res) => {
-	const publicLibCollectionId = '00000000-0000-0000-0000-000000000000'
 	return RepositoryCollection.fetchById(publicLibCollectionId)
 		.then(collection => collection.loadRelatedDrafts())
 		.then(collection => {
@@ -48,32 +40,19 @@ router
 router
 	.route('/users/search')
 	.get([requireCurrentUser, requireCanPreviewDrafts])
-	.get((req, res) => {
+	.get(async (req, res) => {
+		// empty search string? return empty array
 		if (!req.query.q || !req.query.q.trim()) {
 			res.success([])
 			return
 		}
-		const search = `%${req.query.q}%`
-		return db
-			.none(
-				`CREATE OR REPLACE FUNCTION obo_immutable_concat_ws(s text, t1 text, t2 text)
-				RETURNS text AS
-				$func$
-				SELECT concat_ws(s, t1, t2)
-				$func$ LANGUAGE sql IMMUTABLE;
 
-				`
-			)
-			.then(() => {
-				return db.manyOrNone(
-					`SELECT * FROM users WHERE obo_immutable_concat_ws(' ', first_name, last_name) ILIKE $[search] OR email ILIKE $[search] OR username ILIKE $[search] ORDER BY first_name, last_name LIMIT 25`,
-					{ search }
-				)
-			})
-			.then(searchResults => {
-				res.success(resultsToUsers(searchResults))
-			})
-			.catch(res.unexpected)
+		try{
+			const users = await searchForUserByString(req.query.q)
+			res.success(users)
+		} catch (error){
+			res.unexpected(error)
+		}
 	})
 
 // list a draft's permissions
@@ -81,58 +60,65 @@ router
 	.route('/drafts/:draftId/permission')
 	.get([requireCurrentUser, requireCurrentDocument, requireCanPreviewDrafts])
 	.get((req, res) => {
-		return db
-			.manyOrNone(
-				`SELECT users.* FROM repository_map_user_to_draft JOIN users ON users.id = user_id WHERE draft_id = $[draft_id] ORDER BY users.first_name, users.last_name`,
-				{ draft_id: req.params.draftId }
-			)
-			.then(permsResult => {
-				res.success(resultsToUsers(permsResult))
-			})
-			.catch(res.unexpected)
+		return fetchAllUsersWithPermissionToDraft(req.params.draftId)
+		.then(res.success)
+		.catch(res.unexpected)
 	})
 
 // add a permission for a user to a draft
-// @TODO: make sure the user adding the permission has permission
-// @TODO: make sure the user being added exists
 router
 	.route('/drafts/:draftId/permission')
 	.post([requireCurrentUser, requireCurrentDocument])
-	.post((req, res) => {
-		return Promise.resolve()
-			.then(() => {
-				const userId = req.body.userId
-				const draftId = req.currentDocument.draftId
-				return db.none(
-					`INSERT INTO repository_map_user_to_draft (draft_id, user_id) VALUES($[draftId], $[userId]) ON CONFLICT DO NOTHING`,
-					{ userId, draftId }
-				)
-			})
-			.then(() => {
-				res.success()
-			})
-			.catch(res.unexpected)
+	.post( async (req, res) => {
+		try{
+			const userId = req.body.userId
+			const draftId = req.currentDocument.draftId
+
+			// check currentUser's permissions
+			const canShare = await userHasPermissionToDraft(req.currentUser.id, draftId)
+			if(!canShare){
+				res.notAuthorized('Current User has no permissions to selected draft')
+				return
+			}
+
+			// make sure the target userId exists
+			// fetchById will throw if not found
+			await UserModel.fetchById(userId)
+
+			// add permissions
+			await addUserPermissionToDraft(userId, draftId)
+			res.success()
+		} catch (error){
+			res.unexpected(error)
+		}
 	})
 
 // delete a permission for a user to a draft
-// @TODO: make sure the user removing the permission has permission
 router
 	.route('/drafts/:draftId/permission/:userId')
 	.delete([requireCurrentUser, requireCurrentDocument])
-	.delete((req, res) => {
-		return UserModel.fetchById(req.params.userId)
-			.then(user => {
-				const userId = user.id
-				const draftId = req.currentDocument.draftId
-				return db.none(
-					`DELETE FROM repository_map_user_to_draft WHERE draft_id = $[draftId] AND user_id = $[userId]`,
-					{ userId, draftId }
-				)
-			})
-			.then(() => {
-				res.success()
-			})
-			.catch(res.unexpected)
+	.delete(async (req, res) => {
+		try{
+			const userIdToRemove = req.params.userId
+			const draftId = req.currentDocument.draftId
+
+			// check currentUser's permissions
+			const canShare = await userHasPermissionToDraft(req.currentUser.id, draftId)
+			if(!canShare){
+				res.notAuthorized('Current User has no permissions to selected draft')
+				return
+			}
+
+			// make sure the userToRemove exists
+			// fetchById throws when not found
+			const userToRemove = await UserModel.fetchById(userIdToRemove)
+
+			// remove perms
+			await removeUserPermissionToDraft(userToRemove.id, draftId)
+			res.success()
+		} catch(error){
+			res.unexpected(error)
+		}
 	})
 
 module.exports = router
---
+++ packages/app/obojobo-repository/server/services/search.js
@@ -0,0 +1,40 @@
+const db = require('obojobo-express/db')
+const UserModel = require('obojobo-express/models/user')
+
+const searchForUserByString = async searchString => {
+	// creates a function to make searching firstname and lastname together faster
+	await db
+		.none(
+			`CREATE OR REPLACE FUNCTION obo_immutable_concat_ws(s text, t1 text, t2 text)
+			RETURNS text AS
+			$func$
+			SELECT concat_ws(s, t1, t2)
+			$func$ LANGUAGE sql IMMUTABLE;
+			`
+		)
+
+	const users = await db
+		.manyOrNone(
+			`SELECT
+				id,
+				first_name AS "firstName",
+				last_name AS "lastName",
+				email,
+				username,
+				created_at AS "createdAt",
+				roles
+			FROM users
+			WHERE obo_immutable_concat_ws(' ', first_name, last_name) ILIKE $[search]
+			OR email ILIKE $[search]
+			OR username ILIKE $[search]
+			ORDER BY first_name, last_name
+			LIMIT 25`,
+			{ search: `%${searchString}%` }
+		)
+
+	return users.map(u => new UserModel(u))
+}
+
+module.exports = {
+	searchForUserByString
+}
