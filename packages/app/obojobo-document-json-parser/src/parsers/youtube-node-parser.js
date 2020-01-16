4c7321eee854613d5b37cbe549ad51d9e1539605
ctcuff <dev.ctcuff@gmail.com>
2020-01-16 16:54:20 -0500

    Fixed start/end times not appearing in XML

--- packages/app/obojobo-document-json-parser/src/parsers/youtube-node-parser.js
+++ packages/app/obojobo-document-json-parser/src/parsers/youtube-node-parser.js
@@ -2,15 +2,18 @@ const xmlEncode = require('../xml-encode')
 const processTriggers = require('../process-triggers')
 
 const youTubeNodeParser = node => {
+	const content = node.content
 	const id = node.id ? ` id="${node.id}"` : ''
+	const startTime = content.startTime ? `startTime="${content.startTime}"` : ''
+	const endTime = content.endTime ? `endTime="${content.endTime}"` : ''
 	const videoId = xmlEncode(node.content.videoId)
 	const triggersXML = processTriggers(node.content.triggers)
 
 	if (triggersXML) {
-		return `<YouTube videoId="${videoId}"${id}>${triggersXML}</YouTube>`
+		return `<YouTube videoId="${videoId}"${id} ${startTime} ${endTime}>${triggersXML}</YouTube>`
 	}
 
-	return `<YouTube videoId="${videoId}"${id}/>`
+	return `<YouTube videoId="${videoId}"${id} ${startTime} ${endTime}/>`
 }
 
 module.exports = youTubeNodeParser
