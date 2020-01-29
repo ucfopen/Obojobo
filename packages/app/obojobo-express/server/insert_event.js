f98bdbe4c6e5cb028baae8955acced7d8c6a7aa0
Ian Turgeon <iturgeon@gmail.com>
2020-01-28 20:54:23 -0500

    [optimize] uses db.taskIf for multi-query functions

--- packages/app/obojobo-express/server/insert_event.js
+++ packages/app/obojobo-express/server/insert_event.js
@@ -1,36 +1,35 @@
 const db = oboRequire('server/db')
 
-module.exports = insertObject => {
+module.exports = async insertObject => {
 	insertObject.visitId = insertObject.visitId || null
-	return db
-		.one(
-			`
-		INSERT INTO events
-		(actor_time, action, actor, ip, metadata, payload, draft_id, draft_content_id, version, is_preview, visit_id)
-		VALUES ($[actorTime], $[action], $[userId], $[ip], $[metadata], $[payload], $[draftId], $[contentId], $[eventVersion], $[isPreview], $[visitId])
-		RETURNING *`,
+	return db.taskIf(async t => {
+		const insertEventResult = await t.one(
+			`INSERT INTO events
+			(actor_time, action, actor, ip, metadata, payload, draft_id, draft_content_id, version, is_preview, visit_id)
+			VALUES ($[actorTime], $[action], $[userId], $[ip], $[metadata], $[payload], $[draftId], $[contentId], $[eventVersion], $[isPreview], $[visitId])
+			RETURNING *`,
 			insertObject
 		)
-		.then(insertEventResult => {
-			if (insertObject.caliperPayload) {
-				// Add in internal event id to extensions object:
-				if (!insertObject.caliperPayload.extensions) {
-					insertObject.caliperPayload.extensions = {}
-				}
-				insertObject.caliperPayload.extensions.internalEventId = insertEventResult.id
 
-				// Don't bother including this in the promise chain
-				// It's considered a non-essential insert and we're
-				// not going to wait for it
-				db.none(
-					`
-					INSERT INTO caliper_store
-					(payload, is_preview)
-					VALUES ($[caliperPayload], $[isPreview])`,
-					insertObject
-				)
+		if (insertObject.caliperPayload) {
+			// Add in internal event id to extensions object:
+			if (!insertObject.caliperPayload.extensions) {
+				insertObject.caliperPayload.extensions = {}
 			}
+			insertObject.caliperPayload.extensions.internalEventId = insertEventResult.id
 
-			return insertEventResult
-		})
+			// Don't bother including this in the promise chain
+			// It's considered a non-essential insert and we're
+			// not going to wait for it
+			await t.none(
+				`
+				INSERT INTO caliper_store
+				(payload, is_preview)
+				VALUES ($[caliperPayload], $[isPreview])`,
+				insertObject
+			)
+		}
+
+		return insertEventResult
+	})
 }
