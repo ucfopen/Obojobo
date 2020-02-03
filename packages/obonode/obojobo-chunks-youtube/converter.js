94e5aefbaa94f62cca06f676f49a6d788696d2e9
Elli Howard <elise.howard@ucf.edu>
2020-02-03 13:30:42 -0500

    Started updating Youtube node

--- packages/obonode/obojobo-chunks-youtube/converter.js
+++ packages/obonode/obojobo-chunks-youtube/converter.js
@@ -1,15 +1,31 @@
+/**
+ * Generates an Obojobo YouTube node from a Slate node.
+ * Copies the id, type, and triggers.  The conversion also saves the
+ * src, title, type, border, fit, width, height, initalZoom, autoload,
+ * and controlls attributes.
+ * @param {Object} node A Slate Node
+ * @returns {Object} An Obojobo YouTube node 
+ */
 const slateToObo = node => ({
-	id: node.key,
+	id: node.id,
 	type: node.type,
 	children: [],
-	content: node.data.get('content') || {}
+	content: {
+		triggers: node.content.triggers,
+		videoId: node.content.src,
+	}
 })
 
-const oboToSlate = node => ({
-	object: 'block',
-	key: node.id,
-	type: node.type,
-	data: { content: node.content }
-})
+/**
+ * Generates a Slate node from an Obojobo YouTube. Copies all attributes, and adds a dummy child
+ * @param {Object} node An Obojobo YouTube node 
+ * @returns {Object} A Slate node
+ */
+const oboToSlate = node => {
+	const slateNode = Object.assign({}, node)
+	slateNode.children = [{ text: '' }]
+
+	return slateNode
+}
 
 export default { slateToObo, oboToSlate }
