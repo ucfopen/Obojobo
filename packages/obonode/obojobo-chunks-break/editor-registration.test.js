b349371559121993fb0c5fc884bd5b2cc30b2e12
Elli Howard <elise.howard@ucf.edu>
2020-02-12 12:49:54 -0500

    Completed coverage for Break node

--- packages/obonode/obojobo-chunks-break/editor-registration.test.js
+++ packages/obonode/obojobo-chunks-break/editor-registration.test.js
@@ -3,6 +3,17 @@ import Break from './editor-registration'
 const BREAK_NODE = 'ObojoboDraft.Chunks.Break'
 
 describe('Break editor', () => {
+	test('plugins.isVoid to call next if not Break', () => {
+		const next = jest.fn()
+		Break.plugins.isVoid({}, {}, next)
+		expect(next).toHaveBeenCalled()
+	})
+
+	test('plugins.isVoid to return true if the node is a Break', () => {
+		const node = { type: BREAK_NODE }
+		expect(Break.plugins.isVoid(node, {}, jest.fn())).toEqual(true)
+	})
+
 	test('plugins.renderNode renders a break when passed', () => {
 		const props = {
 			attributes: { dummy: 'dummyData' },
