ebc9d7e82c737f6b8103eea7239af0cc9e4e29f1
Elli Howard <elise.howard@ucf.edu>
2020-02-14 13:49:29 -0500

    Updated MCAssessment tests

--- packages/obonode/obojobo-chunks-multiple-choice-assessment/MCAnswer/editor-registration.test.js
+++ packages/obonode/obojobo-chunks-multiple-choice-assessment/MCAnswer/editor-registration.test.js
@@ -1,7 +1,10 @@
 import { Transforms } from 'slate'
+import NormalizeUtil from 'obojobo-document-engine/src/scripts/oboeditor/util/normalize-util'
+jest.mock('obojobo-document-engine/src/scripts/oboeditor/util/normalize-util')
 
 import MCAnswer from './editor-registration'
 
+const MCCHOICE_NODE = 'ObojoboDraft.Chunks.MCAssessment.MCChoice'
 const MCANSWER_NODE = 'ObojoboDraft.Chunks.MCAssessment.MCAnswer'
 const TEXT_NODE = 'ObojoboDraft.Chunks.Text'
 
@@ -10,6 +13,8 @@ jest.mock('obojobo-document-engine/src/scripts/common/index', () => ({
 		contentTypes: [ 'ObojoboDraft.Chunks.Text' ]
 	}
 }))
+
+jest.mock('./converter', () => ({}))
 
 describe('MCAnswer editor', () => {
 	test('plugins.renderNode renders a node', () => {
@@ -41,6 +46,97 @@ describe('MCAnswer editor', () => {
 			children: [
 				{
 					id: 'mockKey',
+					type: MCCHOICE_NODE,
+					content: {},
+					children: [
+						{
+							id: 'mockKey',
+							type: MCANSWER_NODE,
+							content: {},
+							children: [
+								{
+									type: TEXT_NODE,
+									content: { indent: 1 },
+									children: [{ text: 'mockCode', b: true }]
+								}
+							]
+						}
+					]
+				}
+			],
+			isInline: () => false
+		}
+		MCAnswer.plugins.normalizeNode([editor.children[0].children[0], [0,0]], editor, next)
+
+		expect(next).toHaveBeenCalled()
+	})
+
+	test('normalizeNode on MCAnswer calls Transforms on invalid Element children', () => {
+		jest.spyOn(Transforms, 'removeNodes').mockReturnValueOnce(true)
+
+		const next = jest.fn()
+		const editor= {
+			children: [
+				{
+					id: 'mockKey',
+					type: MCCHOICE_NODE,
+					content: {},
+					children: [
+						{
+							id: 'mockKey',
+							type: MCANSWER_NODE,
+							content: {},
+							children: [
+								{
+									type: 'improperNode',
+									content: { indent: 1 },
+									children: [{ text: 'mockCode', b: true }]
+								}
+							]
+						}
+					]
+				}
+			],
+			isInline: () => false
+		}
+		MCAnswer.plugins.normalizeNode([editor.children[0].children[0], [0,0]], editor, next)
+
+		expect(Transforms.removeNodes).toHaveBeenCalled()
+	})
+
+	test('normalizeNode on MCAnswer calls Transforms on invalid Text children', () => {
+		jest.spyOn(Transforms, 'wrapNodes').mockReturnValueOnce(true)
+
+		const next = jest.fn()
+		const editor= {
+			children: [
+				{
+					id: 'mockKey',
+					type: MCCHOICE_NODE,
+					content: {},
+					children: [
+						{
+							id: 'mockKey',
+							type: MCANSWER_NODE,
+							content: {},
+							children: [{ text: 'mockCode', b: true }]
+						}
+					]
+				}
+			],
+			isInline: () => false
+		}
+		MCAnswer.plugins.normalizeNode([editor.children[0].children[0], [0,0]], editor, next)
+
+		expect(Transforms.wrapNodes).toHaveBeenCalled()
+	})
+
+	test('normalizeNode on MCFeedback calls Transforms with invalid parent', () => {
+		const next = jest.fn()
+		const editor= {
+			children: [
+				{
+					id: 'mockKey',
 					type: MCANSWER_NODE,
 					content: {},
 					children: [
@@ -54,54 +150,11 @@ describe('MCAnswer editor', () => {
 			],
 			isInline: () => false
 		}
+		NormalizeUtil.wrapOrphanedSiblings.mockImplementation((editor, entry, wrapper, match) => {
+			match(editor.children[0])
+		})
 		MCAnswer.plugins.normalizeNode([editor.children[0], [0]], editor, next)
 
-		expect(next).toHaveBeenCalled()
-	})
-
-	test('normalizeNode on MCAnswer calls Transforms on invalid Element children', () => {
-		jest.spyOn(Transforms, 'removeNodes').mockReturnValueOnce(true)
-
-		const next = jest.fn()
-		const editor= {
-			children: [
-				{
-					id: 'mockKey',
-					type: MCANSWER_NODE,
-					content: {},
-					children: [
-						{
-							type: 'improperNode',
-							content: { indent: 1 },
-							children: [{ text: 'mockCode', b: true }]
-						}
-					]
-				}
-			],
-			isInline: () => false
-		}
-		MCAnswer.plugins.normalizeNode([editor.children[0], [0]], editor, next)
-
-		expect(Transforms.removeNodes).toHaveBeenCalled()
-	})
-
-	test('normalizeNode on MCAnswer calls Transforms on invalid Text children', () => {
-		jest.spyOn(Transforms, 'wrapNodes').mockReturnValueOnce(true)
-
-		const next = jest.fn()
-		const editor= {
-			children: [
-				{
-					id: 'mockKey',
-					type: MCANSWER_NODE,
-					content: {},
-					children: [{ text: 'mockCode', b: true }]
-				}
-			],
-			isInline: () => false
-		}
-		MCAnswer.plugins.normalizeNode([editor.children[0], [0]], editor, next)
-
-		expect(Transforms.wrapNodes).toHaveBeenCalled()
+		expect(NormalizeUtil.wrapOrphanedSiblings).toHaveBeenCalled()
 	})
 })
