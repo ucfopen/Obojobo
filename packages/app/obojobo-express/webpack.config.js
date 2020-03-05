4fe8ceb7b4fca551926eff409d3a2160a0f4bc4b
Ian Turgeon <iturgeon@gmail.com>
2020-03-04 20:29:03 -0500

    adds plugin-transform-runtime to optimize build

--- packages/app/obojobo-express/webpack.config.js
+++ packages/app/obojobo-express/webpack.config.js
@@ -54,7 +54,7 @@ module.exports =
 					},
 					{
 						test: /\.(js|jsx)$/,
-						exclude: '/node_modules',
+						exclude: /node_modules/,
 						use: {
 							loader: 'babel-loader',
 							options: {
