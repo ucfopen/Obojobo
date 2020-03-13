473bda8b00721970d883316945cb55cf207a774b
Zachary Berry <zachberry@gmail.com>
2020-03-13 10:55:14 -0400

    Updates share dialog to include email address

--- packages/app/obojobo-repository/shared/components/people-list-item.jsx
+++ packages/app/obojobo-repository/shared/components/people-list-item.jsx
@@ -6,8 +6,11 @@ const Avatar = require('./avatar')
 const PeopleListItem = props => (
 	<li className="people-list-item">
 		<Avatar avatarUrl={props.avatarUrl} />
-		<div className="user-name">
-			{`${props.firstName} ${props.lastName}`} {props.isMe ? <i>(me)</i> : null}
+		<div className="user-info">
+			<div className="user-name">
+				{`${props.firstName} ${props.lastName}`} {props.isMe ? <i>(me)</i> : null}
+			</div>
+			<div className="email">{props.email}</div>
 		</div>
 		{props.children}
 	</li>
