9d1fdef173a781945fe94228ed554b3efe20c2e2
Brandon Stull <bstull@ucf.edu>
2020-03-26 11:27:15 -0400

    #684 Updated collection image.

--- packages/app/obojobo-repository/shared/components/collection.js
+++ packages/app/obojobo-repository/shared/components/collection.js
@@ -4,6 +4,7 @@ const React = require('react')
 const { useState } = require('react')
 const CollectionImage = require('./collection-image')
 const CollectionMenu = require('./collection-menu-hoc')
+const short = require('short-uuid')
 
 const Collection = props => {
 	let timeOutId
@@ -23,8 +24,12 @@ const Collection = props => {
 		clearTimeout(timeOutId)
 	}
 
+	const collectionTitleToUrl = encodeURI(props.title.replace(/\s+/g, '-').toLowerCase())
+	const translator = short()
+	const collectionIdToUrl = translator.fromUUID(props.id)
+
 	let mainRender = (
-		<a href={`/library/${props.id}`}>
+		<a href={`/collections/${collectionTitleToUrl}-${collectionIdToUrl}`}>
 			<CollectionImage id={props.id} />
 			<div className="repository--collection-icon--title">{props.title}</div>
 		</a>
@@ -32,7 +37,7 @@ const Collection = props => {
 	if (props.hasMenu) {
 		mainRender = (
 			<button onClick={onToggleMenu}>
-				<CollectionImage id={props.draftId} />
+				<CollectionImage id={props.id} />
 				<div className="repository--collection-icon--menu-control-button">
 					<svg height="32px" viewBox="0 0 32 32" width="32px" xmlns="http://www.w3.org/2000/svg">
 						<path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z" />
