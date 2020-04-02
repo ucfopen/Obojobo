907319f8a00e9fef725178c88d76f915d886458d
ctcuff <dev.ctcuff@gmail.com>
2020-04-02 16:13:11 -0400

    Added a confirmation dialog for when a module is restored

--- packages/app/obojobo-repository/shared/components/restore-module-dialog.jsx
+++ packages/app/obojobo-repository/shared/components/restore-module-dialog.jsx
@@ -6,47 +6,32 @@ const Button = require('./button')
 const ButtonLink = require('./button-link')
 const { urlForEditor } = require('../repository-utils')
 const APIUtil = require('../api-util')
+const ReactModal = require('react-modal')
+const moment = require('moment')
 
 const Revision = props => {
-	const monthName = props.date.toLocaleDateString('default', { month: 'long', day: 'numeric' })
-	const time = props.date.toLocaleTimeString('default', { hour: 'numeric', minute: 'numeric' })
-	const selectedClass = props.selected ? 'is-selected' : 'is-not-selected'
+	const date = moment(props.createdAt).format('MMMM Do - h:mm A')
+	const selectedClass = props.isSelected ? 'is-selected' : ''
 
 	return (
 		<div
-			className="revision-history--item"
+			className={`revision-history--item ${selectedClass}`}
 			onClick={() => {
 				// Pass the index so the revision history
-				// menu knows which item is currently active
-				props.onClickRevision(props.index, props.id)
+				// menu knows which item is currently selected
+				props.onClickRevision(props.revisionId, props.index)
 			}}
 		>
-			<span className={`date ${selectedClass}`}>
-				{monthName} - {time}
-			</span>
+			<span className="date">{date}</span>
 			<span className="username">{props.username}</span>
-			{props.isLatestVersion ? <span className="latest-version">Latest Version</span> : null}
+			{props.isLatestVersion ? (
+				<span className="latest-version">Latest Version</span>
+			) : (
+				<span className="version">Version {props.versionNumber}</span>
+			)}
 		</div>
 	)
 }
-
-const RevisionHistory = props => (
-	<div className={`revision-history ${props.isMenuOpen ? 'is-open' : 'is-closed'}`}>
-		<span className="revision-history--title">Revision history</span>
-		{props.revisions.map((revision, index) => (
-			<Revision
-				key={revision.id}
-				isLatestVersion={index === 0}
-				date={revision.date}
-				username={revision.username}
-				onClickRevision={props.onClickRevision}
-				id={revision.id}
-				selected={props.activeIndex === index}
-				index={index}
-			/>
-		))}
-	</div>
-)
 
 class RestoreModuleDialog extends React.Component {
 	constructor(props) {
@@ -56,6 +41,7 @@ class RestoreModuleDialog extends React.Component {
 
 		this.state = {
 			isMenuOpen: true,
+			isConfirmDialogOpen: false,
 			revisions: [],
 			editorUrl: this.baseUrl,
 			selectedIndex: 0
@@ -65,6 +51,10 @@ class RestoreModuleDialog extends React.Component {
 		this.setSelectedRevision = this.setSelectedRevision.bind(this)
 		this.toggleMenu = this.toggleMenu.bind(this)
 		this.loadDraftRevisions = this.loadDraftRevisions.bind(this)
+		this.openConfirmDialog = this.openConfirmDialog.bind(this)
+		this.closeConfirmDialog = this.closeConfirmDialog.bind(this)
+
+		this.menuRef = React.createRef()
 	}
 
 	componentDidMount() {
@@ -73,13 +63,13 @@ class RestoreModuleDialog extends React.Component {
 
 	loadDraftRevisions() {
 		APIUtil.getAllDraftRevisions(this.props.draftId).then(drafts => {
-			const revisions = drafts
-				.filter(draft => draft.json !== null)
+			const revisions = drafts.filter(draft => draft.json !== null)
 				.map((draft, index) => ({
-					date: new Date(draft.createdAt),
+					createdAt: new Date(draft.createdAt),
 					id: draft.revisionId,
 					username: draft.userFullName,
-					selected: this.state.selectedIndex === index
+					selected: this.state.selectedIndex === index,
+					versionNumber: drafts.length - index
 				}))
 
 			// Set selectedIndex to 0 to make sure the first draft
@@ -91,7 +81,7 @@ class RestoreModuleDialog extends React.Component {
 		})
 	}
 
-	setSelectedRevision(index, revisionId) {
+	setSelectedRevision(revisionId, index) {
 		if (this.state.selectedIndex === index) {
 			// Prevent the iframe from reloading if
 			// the same revision was clicked
@@ -110,17 +100,19 @@ class RestoreModuleDialog extends React.Component {
 			// the latest version
 			return
 		}
-		// eslint-disable-next-line no-alert
-		const response = window.confirm('Restore document to this version?')
-
-		if (!response) return
 
 		const revision = this.state.revisions[this.state.selectedIndex]
 		const draftId = this.props.draftId
 
 		// Load the selected revision and save it
 		APIUtil.getDraftRevision(draftId, revision.id).then(res => {
-			APIUtil.postDraft(draftId, JSON.stringify(res.json)).then(this.loadDraftRevisions)
+			APIUtil.postDraft(draftId, JSON.stringify(res.json)).then(() => {
+				this.loadDraftRevisions()
+				this.menuRef.current.scroll({
+					top: 0,
+					behavior: 'smooth'
+				})
+			})
 		})
 	}
 
@@ -130,12 +122,80 @@ class RestoreModuleDialog extends React.Component {
 		this.setState({ isMenuOpen: !isMenuOpen })
 	}
 
+	openConfirmDialog() {
+		this.setState({ isConfirmDialogOpen: true })
+	}
+
+	closeConfirmDialog() {
+		this.setState({ isConfirmDialogOpen: false })
+	}
+
+	renderConfirmDialog() {
+		if (!this.state.isConfirmDialogOpen) {
+			return null
+		}
+
+		const revision = this.state.revisions[this.state.selectedIndex]
+		const date = moment(revision.createdAt).format('MMMM Do')
+
+		return (
+			<ReactModal
+				isOpen={true}
+				onRequestClose={this.closeConfirmDialog}
+				className="repository--modal restoration-confirm-dialog"
+				overlayClassName="repository--modal-overlay"
+			>
+				<h1 className="dialog-title">Restore Document</h1>
+				<div className="dialog-content">
+					This will restore the document from {date} and save it as the latest version.
+					<small>You can always undo this action by restoring another version.</small>
+				</div>
+				<div className="dialog-controls">
+					<Button className="secondary-button" onClick={this.closeConfirmDialog}>
+						Cancel
+					</Button>
+					<Button
+						onClick={() => {
+							this.closeConfirmDialog()
+							this.restoreModule()
+						}}
+					>
+						Yes - Restore
+					</Button>
+				</div>
+			</ReactModal>
+		)
+	}
+
+	renderRevisionHistoryMenu() {
+		const menuClass = this.state.isMenuOpen ? 'is-open' : 'is-closed'
+
+		return (
+			<div className={`revision-history ${menuClass}`} ref={this.menuRef}>
+				<span className="revision-history--title">Revision history</span>
+				{this.state.revisions.map((revision, index) => (
+					<Revision
+						key={revision.id}
+						isLatestVersion={index === 0}
+						createdAt={revision.createdAt}
+						username={revision.username}
+						onClickRevision={this.setSelectedRevision}
+						revisionId={revision.id}
+						isSelected={this.state.selectedIndex === index}
+						index={index}
+						versionNumber={revision.versionNumber}
+					/>
+				))}
+			</div>
+		)
+	}
+
 	render() {
-		const { isMenuOpen, revisions } = this.state
-		const menuClass = isMenuOpen ? 'is-open' : 'is-not-open'
+		const menuClass = this.state.isMenuOpen ? 'is-open' : 'is-closed'
 
 		return (
 			<div className="restore-module-dialog">
+				{this.renderConfirmDialog()}
 				<div className="restore-module-dialog--header">
 					<ModuleImage id={this.props.draftId} />
 					<div className="title">{this.props.title}</div>
@@ -144,18 +204,12 @@ class RestoreModuleDialog extends React.Component {
 					</Button>
 				</div>
 				<div className="restore-module-dialog--body">
-					<RevisionHistory
-						isMenuOpen={isMenuOpen}
-						draftId={this.props.draftId}
-						revisions={revisions}
-						activeIndex={this.state.selectedIndex}
-						onClickRevision={this.setSelectedRevision}
-					/>
+					{this.renderRevisionHistoryMenu()}
 					<div className="editor-preview">
 						<div className="editor-preview--header">
 							<Button
 								className="restore-button"
-								onClick={this.restoreModule}
+								onClick={this.openConfirmDialog}
 								disabled={this.state.selectedIndex === 0}
 							>
 								Restore document to this version
