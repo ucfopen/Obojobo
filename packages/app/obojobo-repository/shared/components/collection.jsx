577fcfa3a293fba0cb1773bb2f31cf7ddcfaba38
Brandon Stull <bstull@ucf.edu>
2020-04-06 15:39:18 -0400

    #684 Created new 'MenuControlButton' component to use as shared hamburger menu button in both module and collection icons.

--- packages/app/obojobo-repository/shared/components/collection.jsx
+++ packages/app/obojobo-repository/shared/components/collection.jsx
@@ -4,6 +4,7 @@ const React = require('react')
 const { useState } = require('react')
 const CollectionImage = require('./collection-image')
 const CollectionMenu = require('./collection-menu-hoc')
+const MenuControlButton = require('./menu-control-button')
 const short = require('short-uuid')
 
 const Collection = props => {
@@ -38,11 +39,7 @@ const Collection = props => {
 		mainRender = (
 			<button onClick={onToggleMenu}>
 				<CollectionImage id={props.id} />
-				<div className="repository--collection-icon--menu-control-button">
-					<svg height="32px" viewBox="0 0 32 32" width="32px" xmlns="http://www.w3.org/2000/svg">
-						<path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z" />
-					</svg>
-				</div>
+				<MenuControlButton className="repository--collection-icon--menu-control-button" />
 				<div className="repository--collection-icon--title">{props.title}</div>
 			</button>
 		)
