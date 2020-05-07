0a1a566edc786e910dfef5ba36eb6af4d9cac25c
Zachary Berry <zachberry@gmail.com>
2020-05-07 15:05:54 -0400

    * Removes moved or old code

--- packages/obonode/obojobo-chunks-numeric-assessment/NumericFeedback_OLD/viewer.js
+++
@@ -1,9 +0,0 @@
-import Common from 'obojobo-document-engine/src/scripts/common'
-import ViewerComponent from './viewer-component'
-import { NUMERIC_FEEDBACK_NODE } from '../constants'
-
-Common.Registry.registerModel(NUMERIC_FEEDBACK_NODE, {
-	adapter: null,
-	componentClass: ViewerComponent,
-	type: 'chunk'
-})
