0b0f07ef7ef4c1e9bcba36f28fb3242306da41c8
Toan Vu <vutoan1245@gmail.com>
2020-05-19 12:53:14 -0400

    Keep style buttons stay highlight when its corresponding text is selected

--- packages/app/obojobo-document-engine/src/scripts/oboeditor/components/toolbars/content-toolbar.js
+++ packages/app/obojobo-document-engine/src/scripts/oboeditor/components/toolbars/content-toolbar.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Editor } from 'slate'
+import isOrNot from 'obojobo-document-engine/src/scripts/common/util/isornot'
 
 import BasicMarks from '../marks/basic-marks'
 import LinkMark from '../marks/link-mark'
@@ -18,44 +20,53 @@ const contentMarks = [
 ]
 
 const unorderedList = [
-	{ bulletStyle: 'disc', display: '●'},
-	{ bulletStyle: 'circle', display: '○'},
-	{ bulletStyle: 'square', display: '■'}
+	{ bulletStyle: 'disc', display: '●' },
+	{ bulletStyle: 'circle', display: '○' },
+	{ bulletStyle: 'square', display: '■' }
 ]
 
 const orderedList = [
-	{ bulletStyle: 'decimal', display: '1.'},
-	{ bulletStyle: 'lower-alpha', display: 'a.'},
-	{ bulletStyle: 'lower-roman', display: 'i.'},
-	{ bulletStyle: 'upper-alpha', display: 'A.'},
-	{ bulletStyle: 'upper-roman', display: 'I.'}
+	{ bulletStyle: 'decimal', display: '1.' },
+	{ bulletStyle: 'lower-alpha', display: 'a.' },
+	{ bulletStyle: 'lower-roman', display: 'i.' },
+	{ bulletStyle: 'upper-alpha', display: 'A.' },
+	{ bulletStyle: 'upper-roman', display: 'I.' }
 ]
 
-const ContentToolbar = props => (
-	<div className={`visual-editor--content-toolbar`}>
-		<ParagraphStyles editor={props.editor}/>
-		{contentMarks.map(mark => {
-			const Icon = mark.icon
-			return (
-				<button
-					key={mark.name}
-					onClick={() => mark.action(props.editor)}
-					title={mark.name}>
-					<Icon />
-				</button>
-			)
-		})}
-		<ListDropper 
-			editor={props.editor} 
-			type='unordered' 
-			bullets={unorderedList} 
-			defaultStyle="disc"/>
-		<ListDropper 
-			editor={props.editor} 
-			type='ordered' 
-			bullets={orderedList} 
-			defaultStyle="decimal"/>
-	</div>
-)
+const ContentToolbar = props => {
+	const marks = Editor.marks(props.editor)
+	return (
+		<div className={`visual-editor--content-toolbar`}>
+			<ParagraphStyles editor={props.editor} />
+			{contentMarks.map(mark => {
+				const Icon = mark.icon
+				const isSelected = marks && mark && marks[mark.type]
+				const className = isOrNot(isSelected, 'selected')
 
+				return (
+					<button
+						className={className}
+						key={mark.name}
+						onClick={() => mark.action(props.editor)}
+						title={mark.name}
+					>
+						<Icon editor={props.editor} />
+					</button>
+				)
+			})}
+			<ListDropper
+				editor={props.editor}
+				type="unordered"
+				bullets={unorderedList}
+				defaultStyle="disc"
+			/>
+			<ListDropper
+				editor={props.editor}
+				type="ordered"
+				bullets={orderedList}
+				defaultStyle="decimal"
+			/>
+		</div>
+	)
+}
 export default ContentToolbar
