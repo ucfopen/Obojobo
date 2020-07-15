5c1260934b6e046e4ca02104393a8ea2640f0570
Mauricio <mauriciofigueiredo@knights.ucf.edu>
2020-07-15 14:13:20 -0400

    Wrote red-alert-status migration

--- packages/app/obojobo-express/server/migrations/20200713182052-create-red-alert-status.js
+++ packages/app/obojobo-express/server/migrations/20200713182052-create-red-alert-status.js
@@ -1,27 +1,57 @@
 'use strict'
 
-var dbm
-var type
-var seed
+let dbm
+let type
+let seed
 
 /**
- * We receive the dbmigrate dependency from dbmigrate initially.
- * This enables us to not have to rely on NODE_PATH.
- */
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
 exports.setup = function(options, seedLink) {
-	dbm = options.dbmigrate
-	type = dbm.dataType
-	seed = seedLink
+  dbm = options.dbmigrate
+  type = dbm.dataType
+  seed = seedLink
 }
 
 exports.up = function(db) {
-	return null
+	return db
+		.createTable('red_alert_status', {
+			id: {
+				type: 'UUID',
+				primaryKey: true,
+				defaultValue: new String('uuid_generate_v4()')
+      },
+      created_at: {
+				type: 'timestamp WITH TIME ZONE',
+				notNull: true,
+				defaultValue: new String('now()')
+			},
+			user_id: { type: 'bigint', notNull: true },
+      draft_id: { type: 'UUID', notNull: true },
+      is_enabled: { type: 'boolean', notNull: true }
+    })
+
+		.then(result => {
+			return db.addIndex('red_alert_status', 'ras_user_id_index', ['user_id'])
+		})
+		.then(result => {
+			return db.addIndex('red_alert_status', 'ras_draft_id_index', ['draft_id'])
+		})
+		.then(() =>
+			db.addIndex(
+				'red_alert_status',
+				'user_draft_unique',
+				['user_id', 'draft_id'],
+				true
+			)
+		)
 }
 
 exports.down = function(db) {
-	return null
+	return db.dropTable('red_alert_status')
 }
 
 exports._meta = {
-	version: 1
+  'version': 1
 }
