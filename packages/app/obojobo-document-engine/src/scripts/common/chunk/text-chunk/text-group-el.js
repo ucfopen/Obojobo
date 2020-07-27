7f0c0b9e2a4dfd14bc07ef49f67b823d3d01fc0a
Toan Vu <vutoan1245@gmail.com>
2020-07-27 13:45:25 -0400

    Change class name from 'text-group' to 'obo-text'

--- packages/app/obojobo-document-engine/src/scripts/common/chunk/text-chunk/text-group-el.js
+++ packages/app/obojobo-document-engine/src/scripts/common/chunk/text-chunk/text-group-el.js
@@ -51,7 +51,7 @@ const TextGroupEl = props => {
 	const hangingIndent = ('' + props.textItem.data.hangingIndent).toLowerCase()
 
 	const txtProps = {
-		className: 'text-group',
+		className: 'obo-text',
 		'data-group-index': props.groupIndex
 	}
 
