a47adfb5ca50f52e94fa0b0984cb7139637bcd21
iturgeon <iturgeon@gmail.com>
2020-10-01 23:27:38 -0400

    tests for more info box onsave state updates
    added to make sure that errors in onSave update state
    and that non-errors return to a non-error state

--- packages/app/obojobo-document-engine/__tests__/oboeditor/components/navigation/more-info-box.test.js
+++ packages/app/obojobo-document-engine/__tests__/oboeditor/components/navigation/more-info-box.test.js
@@ -741,4 +741,31 @@ describe('MoreInfoBox', () => {
 		})(mockProps)
 		expect(mockSetState).toHaveBeenCalledWith({ content: 'newMockValue' })
 	})
+
+	test('onSave sets and clears state.error', () => {
+		const component = mount(
+			<MoreInfoBox
+				id="mock-id"
+				content={{}}
+				saveId={jest
+					.fn()
+					.mockReturnValueOnce('error!')
+					.mockReturnValueOnce(null)}
+				saveContent={jest.fn().mockReturnValue(null)}
+				markUnsaved={jest.fn()}
+				contentDescription={[]}
+				isAssessment
+			/>
+		)
+
+		// saveId() will return an error, make sure its on state
+		component.instance().onSave()
+		const aftState = component.state()
+		expect(aftState).toHaveProperty('error', 'error!')
+
+		// saveId() returns null this time, make sure state gets updated
+		component.instance().onSave()
+		const aftState2 = component.state()
+		expect(aftState2).toHaveProperty('error', null)
+	})
 })
