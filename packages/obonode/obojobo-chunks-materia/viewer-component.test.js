f82901f2269695c69b86b5d6061f5621363d0857
iturgeon <iturgeon@gmail.com>
2020-10-07 17:04:35 -0400

    wip materia chunk viewer comp tests

--- packages/obonode/obojobo-chunks-materia/viewer-component.test.js
+++ packages/obonode/obojobo-chunks-materia/viewer-component.test.js
@@ -1,11 +1,18 @@
 jest.mock('react-dom')
-jest.mock('obojobo-chunks-iframe/viewer-component', () => require('obojobo-document-engine/__mocks__/mock-class-component').default)
-jest.mock('obojobo-document-engine/src/scripts/common/chunk/text-chunk/text-group-el', () => require('obojobo-document-engine/__mocks__/mock-class-component').default)
+jest.mock(
+	'obojobo-chunks-iframe/viewer-component',
+	() => require('obojobo-document-engine/__mocks__/mock-class-component').default
+)
+jest.mock(
+	'obojobo-document-engine/src/scripts/common/chunk/text-chunk/text-group-el',
+	() => require('obojobo-document-engine/__mocks__/mock-class-component').default
+)
 
 import Materia from './viewer-component'
 import OboModel from 'obojobo-document-engine/__mocks__/obo-model-mock'
 import React from 'react'
 import renderer from 'react-test-renderer'
+import TextGroupEl from 'obojobo-document-engine/src/scripts/common/chunk/text-chunk/text-group-el'
 
 require('./viewer') // used to register this oboModel
 
@@ -38,8 +45,7 @@ describe('Materia viewer component', () => {
 		}
 	})
 
-	afterEach(() => {
-	})
+	afterEach(() => {})
 
 	test('renders', () => {
 		expect.hasAssertions()
@@ -62,14 +68,25 @@ describe('Materia viewer component', () => {
 
 		const inst = component.getInstance()
 		// make sure it's listening on mount
-		expect(window.addEventListener).toHaveBeenCalledWith('message', inst.onPostMessageFromMateria, false)
-		expect(window.removeEventListener).not.toHaveBeenCalledWith('message', inst.onPostMessageFromMateria, false)
+		expect(window.addEventListener).toHaveBeenCalledWith(
+			'message',
+			inst.onPostMessageFromMateria,
+			false
+		)
+		expect(window.removeEventListener).not.toHaveBeenCalledWith(
+			'message',
+			inst.onPostMessageFromMateria,
+			false
+		)
 
 		// make sure it's not listening when unmounted
 		component.unmount()
-		expect(window.removeEventListener).toHaveBeenCalledWith('message', inst.onPostMessageFromMateria, false)
+		expect(window.removeEventListener).toHaveBeenCalledWith(
+			'message',
+			inst.onPostMessageFromMateria,
+			false
+		)
 	})
-
 
 	test('onPostMessageFromMateria without an iframe ref doesnt update state', () => {
 		expect.hasAssertions()
@@ -82,7 +99,7 @@ describe('Materia viewer component', () => {
 
 		const inst = component.getInstance()
 		inst.iframeRef = {}
-		const event = {source: '', data: JSON.stringify({score: 100}) }
+		const event = { source: '', data: JSON.stringify({ score: 100 }) }
 		inst.onPostMessageFromMateria(event)
 		expect(inst.state).toHaveProperty('score', null)
 	})
@@ -97,8 +114,8 @@ describe('Materia viewer component', () => {
 		const component = renderer.create(<Materia {...props} />)
 
 		const inst = component.getInstance()
-		inst.iframeRef = {current:{contentWindow: 'different-mock-window'}}
-		const event = {source: 'mock-window', data: JSON.stringify({score: 100}) }
+		inst.iframeRef = { current: { contentWindow: 'different-mock-window' } }
+		const event = { source: 'mock-window', data: JSON.stringify({ score: 100 }) }
 		inst.onPostMessageFromMateria(event)
 		expect(inst.state).toHaveProperty('score', null)
 	})
@@ -113,8 +130,8 @@ describe('Materia viewer component', () => {
 		const component = renderer.create(<Materia {...props} />)
 
 		const inst = component.getInstance()
-		inst.iframeRef = {current:{contentWindow: 'http://not-localhost/'}}
-		const event = {source: 'http://localhost/whatever', data: JSON.stringify({score: 100}) }
+		inst.iframeRef = { current: { contentWindow: 'http://not-localhost/' } }
+		const event = { source: 'http://localhost/whatever', data: JSON.stringify({ score: 100 }) }
 		inst.onPostMessageFromMateria(event)
 		expect(inst.state).toHaveProperty('score', null)
 	})
@@ -129,8 +146,12 @@ describe('Materia viewer component', () => {
 		const component = renderer.create(<Materia {...props} />)
 
 		const inst = component.getInstance()
-		inst.iframeRef = {current:{contentWindow: 'http://localhost/whatever'}}
-		const event = {origin: 'http://not-localhost' ,source: 'http://localhost/whatever', data: JSON.stringify({score: 100}) }
+		inst.iframeRef = { current: { contentWindow: 'http://localhost/whatever' } }
+		const event = {
+			origin: 'http://not-localhost',
+			source: 'http://localhost/whatever',
+			data: JSON.stringify({ score: 100 })
+		}
 		inst.onPostMessageFromMateria(event)
 		expect(inst.state).toHaveProperty('score', null)
 	})
@@ -145,8 +166,12 @@ describe('Materia viewer component', () => {
 		const component = renderer.create(<Materia {...props} />)
 
 		const inst = component.getInstance()
-		inst.iframeRef = {current:{contentWindow: 'http://localhost/whatever'}}
-		const event = {origin: 'http://localhost' ,source: 'http://localhost/whatever', data: {score: 100} }
+		inst.iframeRef = { current: { contentWindow: 'http://localhost/whatever' } }
+		const event = {
+			origin: 'http://localhost',
+			source: 'http://localhost/whatever',
+			data: { score: 100 }
+		}
 		inst.onPostMessageFromMateria(event)
 		expect(inst.state).toHaveProperty('score', null)
 	})
@@ -161,8 +186,12 @@ describe('Materia viewer component', () => {
 		const component = renderer.create(<Materia {...props} />)
 
 		const inst = component.getInstance()
-		inst.iframeRef = {current:{contentWindow: 'http://localhost/whatever'}}
-		const event = {origin: 'http://localhost' ,source: 'http://localhost/whatever', data: JSON.stringify({type: 'notmateriaScoreRecorded', score: 100}) }
+		inst.iframeRef = { current: { contentWindow: 'http://localhost/whatever' } }
+		const event = {
+			origin: 'http://localhost',
+			source: 'http://localhost/whatever',
+			data: JSON.stringify({ type: 'notmateriaScoreRecorded', score: 100 })
+		}
 		inst.onPostMessageFromMateria(event)
 		expect(inst.state).toHaveProperty('score', null)
 	})
@@ -177,12 +206,15 @@ describe('Materia viewer component', () => {
 		const component = renderer.create(<Materia {...props} />)
 
 		const inst = component.getInstance()
-		inst.iframeRef = {current:{contentWindow: 'http://localhost/whatever'}}
-		const event = {origin: 'http://localhost' ,source: 'http://localhost/whatever', data: JSON.stringify({type: 'materiaScoreRecorded', score: 100}) }
+		inst.iframeRef = { current: { contentWindow: 'http://localhost/whatever' } }
+		const event = {
+			origin: 'http://localhost',
+			source: 'http://localhost/whatever',
+			data: JSON.stringify({ type: 'materiaScoreRecorded', score: 100 })
+		}
 		inst.onPostMessageFromMateria(event)
 		expect(inst.state).toHaveProperty('score', 100)
 	})
-
 
 	test('onPostMessageFromMateria handles json parsing errors', () => {
 		expect.hasAssertions()
@@ -194,9 +226,14 @@ describe('Materia viewer component', () => {
 		const component = renderer.create(<Materia {...props} />)
 
 		const inst = component.getInstance()
-		inst.iframeRef = {current:{contentWindow: 'http://localhost/whatever'}}
-		const event = {origin: 'http://localhost' ,source: 'http://localhost/whatever', data: '!]&({}' }
+		inst.iframeRef = { current: { contentWindow: 'http://localhost/whatever' } }
+		const event = {
+			origin: 'http://localhost',
+			source: 'http://localhost/whatever',
+			data: '!]&({}'
+		}
 		jest.spyOn(console, 'error')
+		console.error.mockReturnValueOnce() // consume error output
 		inst.onPostMessageFromMateria(event)
 		expect(inst.state).toHaveProperty('score', null)
 		expect(console.error).toHaveBeenCalled()
@@ -211,7 +248,80 @@ describe('Materia viewer component', () => {
 
 		const component = renderer.create(<Materia {...props} />)
 		const inst = component.getInstance()
-		expect(inst.srcToLTILaunchUrl('visit-id', 'node-id')).toBe('http://localhost/materia-lti-launch?visitId=visit-id&nodeId=node-id')
-		expect(inst.srcToLTILaunchUrl('99-9999-999', 'fff-fff-fff')).toBe("http://localhost/materia-lti-launch?visitId=99-9999-999&nodeId=fff-fff-fff")
+		expect(inst.srcToLTILaunchUrl('visit-id', 'node-id')).toBe(
+			'http://localhost/materia-lti-launch?visitId=visit-id&nodeId=node-id'
+		)
+		expect(inst.srcToLTILaunchUrl('99-9999-999', 'fff-fff-fff')).toBe(
+			'http://localhost/materia-lti-launch?visitId=99-9999-999&nodeId=fff-fff-fff'
+		)
+	})
+
+	test('onShow changes state to open', () => {
+		expect.hasAssertions()
+		const props = {
+			model,
+			moduleData
+		}
+
+		const component = renderer.create(<Materia {...props} />)
+		const inst = component.getInstance()
+		expect(inst.state).toHaveProperty('open', false)
+		inst.onShow()
+		expect(inst.state).toHaveProperty('open', true)
+	})
+
+	test('renderCaptionOrScore renders text', () => {
+		expect.hasAssertions()
+		const props = {
+			model,
+			moduleData
+		}
+
+		const component = renderer.create(<Materia {...props} />)
+		const inst = component.getInstance()
+
+		// find the textGroupEL by a unique prop
+		// if it's found, it was rendered
+		expect(component.root.findAllByProps({ groupIndex: '0' })).toHaveLength(1)
+	})
+
+	test('renderCaptionOrScore captures errors', () => {
+		expect.hasAssertions()
+		const props = {
+			model,
+			moduleData
+		}
+
+		const component = renderer.create(<Materia {...props} />)
+
+		const inst = component.getInstance()
+		// set a value that'll cause a property of undefined error
+		inst.state.model.modelState.textGroup = null
+		// force it to redraw with the error
+		inst.setState({ model: inst.state.model })
+
+		// find the textGroupEL by a unique prop
+		// it shouldn't render one
+		expect(component.root.findAllByProps({ groupIndex: '0' })).toHaveLength(0)
+	})
+
+	test('renderTextCaption renders null when there is no textgroup text', () => {
+		expect.hasAssertions()
+		const props = {
+			model,
+			moduleData
+		}
+
+		const component = renderer.create(<Materia {...props} />)
+
+		const inst = component.getInstance()
+		// set a value that'll cause a property of undefined error
+		inst.state.model.modelState.textGroup.first.text = ''
+		// force it to redraw with the error
+		inst.setState({ model: inst.state.model })
+
+		// find the textGroupEL by a unique prop
+		// it shouldn't render one
+		expect(component.root.findAllByProps({ groupIndex: '0' })).toHaveLength(0)
 	})
 })
