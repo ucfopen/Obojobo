2033231b9bbb401328a9952d99b459b31ef38eb4
ctcuff <dev.ctcuff@gmail.com>
2020-10-22 11:52:59 -0400

    Module lock is checked before document is restored

--- packages/app/obojobo-repository/shared/components/version-history-dialog.jsx
+++ packages/app/obojobo-repository/shared/components/version-history-dialog.jsx
@@ -18,6 +18,8 @@ class VersionHistoryDialog extends React.Component {
 		this.state = {
 			isMenuOpen: true,
 			isConfirmDialogOpen: false,
+			isLockDialogOpen: false,
+			isErrorDialogOpen: false,
 			editorUrl: props.versionHistory.length ? this.editUrlForItem(0) : null,
 			selectedIndex: 0
 		}
@@ -27,6 +29,11 @@ class VersionHistoryDialog extends React.Component {
 		this.toggleMenu = this.toggleMenu.bind(this)
 		this.openConfirmDialog = this.openConfirmDialog.bind(this)
 		this.closeConfirmDialog = this.closeConfirmDialog.bind(this)
+		this.checkModuleLock = this.checkModuleLock.bind(this)
+		this.renderLockDialog = this.renderLockDialog.bind(this)
+		this.closeLockDialog = this.closeLockDialog.bind(this)
+		this.openErrorDialog = this.openErrorDialog.bind(this)
+		this.closeErrorDialog = this.closeErrorDialog.bind(this)
 
 		this.menuRef = React.createRef()
 	}
@@ -53,11 +60,14 @@ class VersionHistoryDialog extends React.Component {
 
 	restoreModule() {
 		this.closeConfirmDialog()
+		this.closeLockDialog()
+
 		if (this.state.selectedIndex === 0) {
 			// Prevent restoring a module that's already
 			// the latest version
 			return
 		}
+
 		const selectedRevision = this.props.versionHistory[this.state.selectedIndex]
 		this.props.restoreVersion(this.props.draftId, selectedRevision.id)
 		this.setState({
@@ -75,6 +85,45 @@ class VersionHistoryDialog extends React.Component {
 
 	closeConfirmDialog() {
 		this.setState({ isConfirmDialogOpen: false })
+	}
+
+	openLockDialog() {
+		this.setState({ isLockDialogOpen: true })
+	}
+
+	closeLockDialog() {
+		this.setState({ isLockDialogOpen: false })
+	}
+
+	openErrorDialog() {
+		this.setState({ isErrorDialogOpen: true })
+	}
+
+	closeErrorDialog() {
+		this.setState({ isErrorDialogOpen: false })
+	}
+
+	checkModuleLock() {
+		this.closeConfirmDialog()
+
+		// Check to see if anyone is editing this module before
+		// we try to restore it
+		this.props.checkModuleLock(this.props.draftId).then(res => {
+			const { payload, error } = res
+
+			if (error) {
+				this.openErrorDialog()
+				return
+			}
+
+			// payload will be undefined here if no one is
+			// currently editing this module
+			if (!payload) {
+				this.restoreModule()
+			} else {
+				this.openLockDialog()
+			}
+		})
 	}
 
 	renderConfirmDialog() {
@@ -99,7 +148,55 @@ class VersionHistoryDialog extends React.Component {
 					<Button className="secondary-button" onClick={this.closeConfirmDialog}>
 						Cancel
 					</Button>
+					<Button onClick={this.checkModuleLock}>Yes - Restore</Button>
+				</div>
+			</ReactModal>
+		)
+	}
+
+	renderLockDialog() {
+		if (!this.state.isLockDialogOpen) {
+			return null
+		}
+
+		return (
+			<ReactModal
+				isOpen={true}
+				onRequestClose={this.closeLockDialog}
+				className="repository--modal restore-confirm-dialog"
+				overlayClassName="repository--modal-overlay"
+			>
+				<h1 className="dialog-title">Warning</h1>
+				<div className="dialog-content">
+					This module is currently being edited by another user, are you sure you want to restore
+					it?
+				</div>
+				<div className="dialog-controls">
+					<Button className="secondary-button" onClick={this.closeLockDialog}>
+						Cancel
+					</Button>
 					<Button onClick={this.restoreModule}>Yes - Restore</Button>
+				</div>
+			</ReactModal>
+		)
+	}
+
+	renderErrorDialog() {
+		if (!this.state.isErrorDialogOpen) {
+			return
+		}
+
+		return (
+			<ReactModal
+				isOpen={true}
+				onRequestClose={this.closeErrorDialog}
+				className="repository--modal restore-confirm-dialog"
+				overlayClassName="repository--modal-overlay"
+			>
+				<h1 className="dialog-title">Error</h1>
+				<div className="dialog-content">An error occurred while restoring this version.</div>
+				<div className="dialog-controls">
+					<Button onClick={this.closeErrorDialog}>Close</Button>
 				</div>
 			</ReactModal>
 		)
@@ -153,6 +250,8 @@ class VersionHistoryDialog extends React.Component {
 		return (
 			<div className="version-history-dialog">
 				{this.renderConfirmDialog()}
+				{this.renderLockDialog()}
+				{this.renderErrorDialog()}
 				<div className="version-history-dialog--header">
 					<ModuleImage id={this.props.draftId} />
 					<div className="title">{this.props.title}</div>
