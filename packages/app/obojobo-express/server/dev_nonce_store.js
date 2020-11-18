f0c8c69d872de9e8396cd85c1d90b5b96d00ccb1
iturgeon <iturgeon@gmail.com>
2020-11-17 20:07:35 -0500

    update to lti in-memory nonce so it clears correctly

--- packages/app/obojobo-express/server/dev_nonce_store.js
+++ packages/app/obojobo-express/server/dev_nonce_store.js
@@ -1,7 +1,7 @@
 const NonceStore = require('ims-lti').Stores.NonceStore
 const logger = oboRequire('server/logger')
-
 const EXPIRE_IN_SEC = 5 * 60
+const MAX_COUNT = 10000
 
 class DevNonceStore extends NonceStore {
 	constructor() {
@@ -55,16 +55,25 @@ class DevNonceStore extends NonceStore {
 	}
 
 	setUsed(nonce, timestamp, next = () => {}) {
+		timestamp = parseInt(timestamp, 10)
 		this.used[nonce] = timestamp + EXPIRE_IN_SEC
 		next(null)
 	}
 
 	_clearExpiredNonces() {
 		const now = Math.round(Date.now() / 1000)
-		Object.keys(this.used).forEach(key => {
-			logger.info(`Clearing nonce memory for ${key}`)
-			if (this.used[key] <= now) delete this.used[key]
+		const nonceKeys = Object.keys(this.used)
+		let cleared = 0
+
+		// clear all expired nonces and any that are over our limit
+		nonceKeys.forEach((key, index) => {
+			if (this.used[key] <= now || index >= MAX_COUNT) {
+				cleared++
+				delete this.used[key]
+			}
 		})
+
+		logger.info(`${nonceKeys.length - cleared} lti nonce keys stored`)
 	}
 }
 
