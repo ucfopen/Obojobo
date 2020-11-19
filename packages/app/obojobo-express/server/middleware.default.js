7f86bf48cc5541da74e8a57ce0af39e6ed7e6fd0
iturgeon <iturgeon@gmail.com>
2020-11-19 16:43:38 -0500

    query timeouts added
    We had an outage where our AWS RDS database failed over to
    its backup server.  Normally one would expect the db driver to lose
    connection or at least somehow be aware of the outage.  It wasn't, and just
    sat waiting for every db query to resolve.  No errors logged, no automatic
    recovery.  The node proccesses needed to be terminated to re-establish
    connections.
    
    This commit adds query_timeouts to the configuration for pg-promise to ensure
    that they will eventually time out.  We set them just below chromes usual request timeout
    to maximize the time before timeout occurs.  This should give us a lot of headroom
    for unforseen long-running queries and still produce error logs we can use to indicate
    something has gone wrong.
    
    Important: adds a required env var: DB_TIMEOUT, suggested value 29000

--- packages/app/obojobo-express/server/middleware.default.js
+++ packages/app/obojobo-express/server/middleware.default.js
@@ -69,7 +69,8 @@ module.exports = app => {
 		res.missing()
 	})
 
-	// unknown error handler
+	// uncaught error handler
+	// you'll want to keep an eye out for 'Query read timeout' in your logs, it may indicate the db has gone away
 	app.use((err, req, res, next) => {
 		logger.error('Unknown error', err)
 		res.unexpected(err)
