40c2d484e5cab2faafa3fdbfe8de4dc675df3538
Zachary Berry <zachberry@gmail.com>
2020-12-09 14:53:17 -0500

    No verify commit. Restores split-parent which makes hitting enter add new lines again in Texts

--- packages/obonode/obojobo-chunks-text/changes/split-parent.js
+++ packages/obonode/obojobo-chunks-text/changes/split-parent.js
@@ -1,10 +1,34 @@
-import { Transforms } from 'slate'
+import { Editor, Transforms, Range } from 'slate'
+
+const TEXT_LINE_NODE = 'ObojoboDraft.Chunks.Text.TextLine'
 
 const splitParent = (entry, editor, event) => {
-	if (event.isDefaultPrevented()) return
+	if (event.isDefaultPrevented()) {
+		return
+	}
+
+	if (!Range.isCollapsed(editor.selection)) {
+		return
+	}
+
+	const [leaf] = Editor.leaf(editor, editor.selection)
+
+	if (leaf.text !== '') {
+		return
+	}
+
 	event.preventDefault()
 
-	Transforms.splitNodes(editor, { at: editor.selection })
+	const [, nodePath] = entry
+	const nodeRange = Editor.range(editor, nodePath)
+	const [[, linePath]] = Array.from(
+		Editor.nodes(editor, {
+			at: Range.intersection(editor.selection, nodeRange),
+			match: child => child.subtype === TEXT_LINE_NODE
+		})
+	)
+
+	Transforms.splitNodes(editor, { at: linePath, height: 1 })
 }
 
 export default splitParent
--- packages/obonode/obojobo-chunks-text/changes/split-parent.test.js
+++ packages/obonode/obojobo-chunks-text/changes/split-parent.test.js
@@ -1,5 +1,6 @@
 import { Transforms } from 'slate'
-jest.mock('slate')
+import { ReactEditor } from 'slate-react'
+jest.mock('slate-react')
 
 import splitParent from './split-parent'
 
@@ -7,23 +8,42 @@ const TEXT_NODE = 'ObojoboDraft.Chunks.Text'
 const TEXT_LINE_NODE = 'ObojoboDraft.Chunks.Text.TextLine'
 
 describe('Split Text Parent', () => {
-	test('splitParent does nothing if default event is prevented', () => {
+	test('splitParent does nothing if text is not empty', () => {
+		ReactEditor.findPath.mockReturnValueOnce([0])
+
 		const editor = {
-			children: [],
+			children: [
+				{
+					id: 'mockKey',
+					type: TEXT_NODE,
+					content: {},
+					children: [
+						{
+							type: TEXT_NODE,
+							subtype: TEXT_LINE_NODE,
+							content: { indent: 1 },
+							children: [{ text: 'mockText', b: true }]
+						}
+					]
+				}
+			],
 			selection: {
 				anchor: { path: [0, 0, 0], offset: 1 },
 				focus: { path: [0, 0, 0], offset: 1 }
 			},
 			isVoid: () => false
 		}
-		const event = { isDefaultPrevented: () => true, preventDefault: jest.fn() }
+		const event = { preventDefault: jest.fn(), isDefaultPrevented: () => false }
 
 		splitParent([editor.children[0], [0]], editor, event)
 
 		expect(event.preventDefault).not.toHaveBeenCalled()
 	})
 
-	test('splitParent calls splitNodes', () => {
+	test('splitParent does nothing if text is not empty', () => {
+		jest.spyOn(Transforms, 'splitNodes').mockReturnValueOnce(true)
+		ReactEditor.findPath.mockReturnValueOnce([0])
+
 		const editor = {
 			children: [
 				{
@@ -46,7 +66,7 @@ describe('Split Text Parent', () => {
 			},
 			isVoid: () => false
 		}
-		const event = { isDefaultPrevented: () => false, preventDefault: jest.fn() }
+		const event = { preventDefault: jest.fn(), isDefaultPrevented: () => false }
 
 		splitParent([editor.children[0], [0]], editor, event)
 
