c18e6d1b6b59e71600795d398662982fbaeafaab
iturgeon <iturgeon@gmail.com>
2021-03-11 11:33:18 -0500

    add dynamic loading of viewer chunks for tests

--- packages/app/obojobo-document-engine/__mocks__/_load-all-chunks.js
+++ packages/app/obojobo-document-engine/__mocks__/_load-all-chunks.js
@@ -1,8 +1,10 @@
-const { gatherClientScriptsFromModules } = require('obojobo-lib-utils')
-const entriesFromObojoboModules = gatherClientScriptsFromModules()
-
-chunkViewerScripts.forEach(script => {
+// searching for the viewer scripts is expensive, this will cache them
+if(!global.__viewerScriptCache){
+	const { gatherClientScriptsFromModules } = require('obojobo-lib-utils')
+	const entriesFromObojoboModules = gatherClientScriptsFromModules()
+	const chunkViewerScripts = entriesFromObojoboModules.viewer.filter(item => !item.includes('obojobo-document-engine'))
 	// skip viewer scripts from the doc-engine
-	if(item.includes('obojobo-document-engine')) return
-	require(script)
-});
+	global.__viewerScriptCache = chunkViewerScripts.filter(script => !script.includes('obojobo-document-engine'))
+}
+
+global.__viewerScriptCache.forEach(script => require(script))
