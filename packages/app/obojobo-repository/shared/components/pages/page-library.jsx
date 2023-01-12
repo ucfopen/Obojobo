0bbf5a6ecdc9bcdd91168505acd42db491a04ae2
Brandon Stull <bstull@ucf.edu>
2023-01-12 16:12:29 -0500

    #2000 Module library page now hydrates to enable live filtration. Module/collection filter functions were moved into their own file to be shared easily between components. Added a function to the collection backend model to enable looking up all public collections, whenever that becomes necessary.

--- packages/app/obojobo-repository/shared/components/pages/page-library.jsx
+++ packages/app/obojobo-repository/shared/components/pages/page-library.jsx
@@ -1,54 +1,76 @@
+require('./page-library.scss')
+
 const React = require('react')
 import LayoutDefault from '../layouts/default'
 import RepositoryNav from '../repository-nav'
 import RepositoryBanner from '../repository-banner'
 import Module from '../module'
 
+const Search = require('../search')
+
+const { filterModules } = require('../../util/filter-functions')
+
 const title = 'Module Library'
 
-const PageLibrary = props => (
-	<LayoutDefault
-		title={title}
-		className="repository--library"
-		appCSSUrl={props.appCSSUrl /* provided by resp.render() */}
-	>
-		<RepositoryNav
-			userId={props.currentUser.id}
-			userPerms={props.currentUser.perms}
-			avatarUrl={props.currentUser.avatarUrl}
-			displayName={`${props.currentUser.firstName} ${props.currentUser.lastName}`}
-			noticeCount={0}
-		/>
-		<RepositoryBanner className="default-bg" title={title} />
+const PageLibrary = props => {
+	const [filterString, setFilterString] = React.useState('')
 
-		<div className="repository--section-wrapper">
-			<section className="repository--main-content">
-				<p>Find modules for your course.</p>
-				{props.collections.map(collection => (
-					<span key={collection.id}>
-						<div className="repository--main-content--title">
-							<span>{collection.title}</span>
-						</div>
-						<div className="repository--item-list--collection">
-							<div className="repository--item-list--collection--item-wrapper">
-								<div className="repository--item-list--row">
-									<div className="repository--item-list--collection--item--multi-wrapper">
-										{collection.drafts.map(draft => (
-											<Module key={draft.draftId} {...draft}></Module>
-										))}
-									</div>
+	const filteredDisplay = props.collections.map(collection => {
+		const visibleModulesInCollection = filterModules(collection.drafts, filterString, false)
+		const modulesInCollectionRender = visibleModulesInCollection.map(draft => (
+			<Module key={draft.draftId} {...draft}></Module>
+		))
+
+		if (visibleModulesInCollection.length) {
+			return (
+				<span key={collection.id}>
+					<div className="repository--main-content--title">
+						<span>{collection.title}</span>
+					</div>
+					<div className="repository--item-list--collection">
+						<div className="repository--item-list--collection--item-wrapper">
+							<div className="repository--item-list--row">
+								<div className="repository--item-list--collection--item--multi-wrapper">
+									{modulesInCollectionRender}
 								</div>
 							</div>
 						</div>
-					</span>
-				))}
-			</section>
-		</div>
-	</LayoutDefault>
-)
+					</div>
+				</span>
+			)
+		}
+		return null
+	})
+
+	return (
+		<LayoutDefault
+			title={title}
+			className="repository--library"
+			appCSSUrl={props.appCSSUrl /* provided by resp.render() */}
+		>
+			<RepositoryNav
+				userId={props.currentUser.id}
+				userPerms={props.currentUser.perms}
+				avatarUrl={props.currentUser.avatarUrl}
+				displayName={`${props.currentUser.firstName} ${props.currentUser.lastName}`}
+				noticeCount={0}
+			/>
+			<RepositoryBanner className="default-bg" title={title} />
+
+			<div className="repository--section-wrapper">
+				<section className="repository--main-content">
+					<p>Find modules for your course.</p>
+					<Search value={filterString} placeholder="Filter Modules..." onChange={setFilterString} />
+					{filteredDisplay}
+				</section>
+			</div>
+		</LayoutDefault>
+	)
+}
 
 PageLibrary.defaultProps = {
 	collections: []
 }
 
-module.exports = PageLibrary
+// module.exports = PageLibrary
+export default PageLibrary
--- packages/app/obojobo-repository/shared/components/pages/page-library.test.js
+++ packages/app/obojobo-repository/shared/components/pages/page-library.test.js
@@ -3,6 +3,7 @@ import PageLibrary from './page-library'
 import { shallow } from 'enzyme'
 
 const Module = require('../module')
+const Search = require('../search')
 
 describe('PageLibrary', () => {
 	const mockCurrentUser = {
@@ -20,7 +21,7 @@ describe('PageLibrary', () => {
 		expect(mainContentChild.find('span').length).toBe(0)
 	})
 
-	test('renders collections correctly based on props', () => {
+	test('renders correctly when collections are provided but contain no drafts', () => {
 		const mockCollections = [
 			{
 				id: 'mockCollectionId',
@@ -31,6 +32,28 @@ describe('PageLibrary', () => {
 				id: 'mockCollectionId2',
 				title: 'mockCollectionTitle2',
 				drafts: []
+			}
+		]
+
+		const component = shallow(
+			<PageLibrary currentUser={mockCurrentUser} collections={mockCollections} />
+		)
+
+		const mainContentSpans = component.find('.repository--main-content > span')
+		expect(mainContentSpans.length).toBe(0)
+	})
+
+	test('renders correctly when collections are provided and contain drafts', () => {
+		const mockCollections = [
+			{
+				id: 'mockCollectionId',
+				title: 'mockCollectionTitle',
+				drafts: [{ draftId: 'mockDraftId', title: 'mockDraft' }]
+			},
+			{
+				id: 'mockCollectionId2',
+				title: 'mockCollectionTitle2',
+				drafts: [{ draftId: 'mockDraftId2', title: 'mockDraft2' }]
 			}
 		]
 
@@ -50,9 +73,9 @@ describe('PageLibrary', () => {
 				id: 'mockCollectionId',
 				title: 'mockCollectionTitle',
 				drafts: [
-					{ draftId: 'mockDraftId' },
-					{ draftId: 'mockDraftId2' },
-					{ draftId: 'mockDraftId3' }
+					{ draftId: 'mockDraftId', title: 'mockDraft' },
+					{ draftId: 'mockDraftId2', title: 'mockDraft2' },
+					{ draftId: 'mockDraftId3', title: 'mockDraft3' }
 				]
 			}
 		]
@@ -70,5 +93,47 @@ describe('PageLibrary', () => {
 		expect(moduleComponents.at(0).key()).toBe('mockDraftId')
 		expect(moduleComponents.at(1).key()).toBe('mockDraftId2')
 		expect(moduleComponents.at(2).key()).toBe('mockDraftId3')
+	})
+
+	test('renders collections and drafts correctly based on applied filters', () => {
+		const mockCollections = [
+			{
+				id: 'mockCollectionId',
+				title: 'mockCollectionTitle',
+				drafts: [{ draftId: 'mockDraftId', title: 'mockDraft1' }]
+			},
+			{
+				id: 'mockCollectionId2',
+				title: 'mockCollectionTitle2',
+				drafts: [{ draftId: 'mockDraftId2', title: 'mockDraft2' }]
+			}
+		]
+
+		const component = shallow(
+			<PageLibrary currentUser={mockCurrentUser} collections={mockCollections} />
+		)
+
+		// everything should be visible by default
+		let mainContentSpans = component.find('.repository--main-content > span')
+		expect(mainContentSpans.length).toBe(2)
+		expect(mainContentSpans.at(0).key()).toBe('mockCollectionId')
+		expect(mainContentSpans.at(1).key()).toBe('mockCollectionId2')
+
+		let firstCollectionModules = mainContentSpans.at(0).find(Module)
+		expect(firstCollectionModules.at(0).key()).toBe('mockDraftId')
+
+		const secondCollectionModules = mainContentSpans.at(1).find(Module)
+		expect(secondCollectionModules.at(0).key()).toBe('mockDraftId2')
+
+		// change the filter string to '2'
+		const searchComponent = component.find(Search)
+		searchComponent.props().onChange('2')
+		// since none of the modules in the first collection have '2' in the title, it should not render
+		mainContentSpans = component.find('.repository--main-content > span')
+		expect(mainContentSpans.length).toBe(1)
+		expect(mainContentSpans.at(0).key()).toBe('mockCollectionId2')
+
+		firstCollectionModules = mainContentSpans.at(0).find(Module)
+		expect(firstCollectionModules.at(0).key()).toBe('mockDraftId2')
 	})
 })
