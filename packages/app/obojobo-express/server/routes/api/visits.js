acf12dd9508b7f0e9aaca7af0b98d4bed3202e4b
RachelDau <112724541+RachelDau@users.noreply.github.com>
2023-12-11 12:48:01 -0500

    Visits code clean-up

--- packages/app/obojobo-express/server/routes/api/visits.js
+++ packages/app/obojobo-express/server/routes/api/visits.js
@@ -91,11 +91,11 @@ router
 					visitId: req.currentVisit.id
 				})
 			})
-
 			.then(() => {
 				logger.log(
 					`VISIT: Start visit success for visitId="${req.currentVisit.id}", draftId="${req.currentDocument.draftId}", userId="${req.currentUser.id}"`
 				)
+				
 				// Build lti data for return
 				const lti = { lis_outcome_service_url: null }
 				if (req.currentVisit.is_preview === false) {
