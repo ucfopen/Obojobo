c37cc24c390f3226bebaad9436f226625a9f56f0
Rachel Dauns <ra657495@ucf.edu>
2024-05-08 11:36:33 -0400

    notification unit tests for button click updated

--- packages/app/obojobo-repository/shared/components/notification.test.js
+++ packages/app/obojobo-repository/shared/components/notification.test.js
@@ -35,20 +35,25 @@ describe('Notification component', () => {
 		const notificationValue = [{ key: 1, text: 'Test Notification', title: 'Test Title' }]
 		document.cookie = `notifications=${JSON.stringify(notificationValue)}`
 
-		const component = create(<Notification onDataFromNotification={onDataFromNotification} />)
+		const reusableComponent = <Notification onDataFromNotification={onDataFromNotification} />
+		let component
+		act(() => {
+			component = create(reusableComponent)
+		})
+
 		const tree = component.toJSON()
 
 		expect(tree).toMatchSnapshot()
-		expect(document.cookie).toBe(`notifications=${JSON.stringify(notificationValue)}`)
+		expect(document.cookie).toBe(
+			`notifications=${encodeURIComponent(JSON.stringify(notificationValue))};`
+		)
 	})
 
-	test('handles click on exit button and updates state and cookie', () => {
+	test('hides the notification on exit button click', () => {
 		const onDataFromNotification = jest.fn()
-		const notificationValue = [
-			{ key: 1, text: 'Notification1', title: 'Title1' },
-			{ key: 2, text: 'Notification2', title: 'Title2' }
-		]
-		document.cookie = `notifications=${JSON.stringify(notificationValue)}`
+		document.cookie =
+			'notifications=' +
+			JSON.stringify([{ key: 1, text: 'Test Notification', title: 'Test Title' }])
 
 		const reusableComponent = <Notification onDataFromNotification={onDataFromNotification} />
 		let component
@@ -59,28 +64,22 @@ describe('Notification component', () => {
 		let tree = component.toJSON()
 		expect(tree).toMatchSnapshot()
 
-		// Simulate click on exit button for the first notification
-		const key = 0
 		const exitButtons = component.root.findAllByProps({ className: 'notification-exit-button' })
 
-		if (exitButtons[key]) {
-			act(() => {
-				exitButtons[key].props.onClick()
-			})
+		act(() => {
+			exitButtons[0].props.onClick()
+		})
 
-			tree = component.toJSON()
-			expect(tree).toMatchSnapshot()
-			expect(document.cookie).toBe(
-				`notifications=${encodeURIComponent(JSON.stringify(notificationValue))};`
-			)
-		}
+		tree = component.toJSON()
+		expect(tree).toMatchSnapshot()
 	})
-
-	test('hides the notification on exit button click', () => {
+	test('handles click on exit button and updates state and cookie', () => {
 		const onDataFromNotification = jest.fn()
-		document.cookie =
-			'notifications=' +
-			JSON.stringify([{ key: 1, text: 'Test Notification', title: 'Test Title' }])
+		const notificationValue = [
+			{ key: 1, text: 'Notification1', title: 'Title1' },
+			{ key: 2, text: 'Notification2', title: 'Title2' }
+		]
+		document.cookie = `notifications=${JSON.stringify(notificationValue)}`
 
 		const elementToExit = document.getElementsByClassName('notification-exit-button')[0]
 		const reusableComponent = <Notification onDataFromNotification={onDataFromNotification} />
@@ -99,11 +98,15 @@ describe('Notification component', () => {
 			exitButtons[key].props.onClick()
 		})
 
+		const newNotificationValue = [{ key: 2, text: 'Notification2', title: 'Title2' }]
 		tree = component.toJSON()
 		expect(tree).toMatchSnapshot()
-
+		expect(document.cookie).toBe(
+			`notifications=${encodeURIComponent(JSON.stringify(newNotificationValue))};`
+		)
 		expect(elementToExit).toBe(undefined)
 	})
+
 	test('renders null when there are no notifications but document.cookie is not null', () => {
 		const onDataFromNotification = jest.fn()
 		const reusableComponent = <Notification onDataFromNotification={onDataFromNotification} />
